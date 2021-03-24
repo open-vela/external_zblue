490e590800e1cb126f5f03732f40a9f3ae25ed9e
chao.an <anchao@xiaomi.com>
2021-03-24 18:27:22 +0800

    port/zblue/shell: remove the static section of shell command
    Signed-off-by: chao.an <anchao@xiaomi.com>

--- port/include/shell/shell.h
+++ port/include/shell/shell.h
@@ -166,10 +166,7 @@ struct shell_static_entry {
 			       mandatory, optional)			   \
 	const struct shell_static_entry UTIL_CAT(_shell_, syntax) = \
 	SHELL_CMD_ARG(syntax, subcmd, help, handler, mandatory, optional); \
-	const struct shell_cmd_entry UTIL_CAT(shell_cmd_, syntax)   \
-	__attribute__ ((section("."					   \
-			STRINGIFY(UTIL_CAT(shell_root_cmd_, syntax)))))	   \
-	__attribute__((used)) = {					   \
+	const struct shell_cmd_entry UTIL_CAT(shell_cmd_, syntax) = {	   \
 		.is_dynamic = false,					   \
 		.u = {.entry = &UTIL_CAT(_shell_, syntax)}		   \
 	}
