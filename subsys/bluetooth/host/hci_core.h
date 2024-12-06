19fc8f6a701c53d38b099b2a18e68fd0ca2ddbdc
chengkai <chengkai@xiaomi.com>
2024-12-06 10:53:07 +0800

    zblue: add rssi interface
    bug: v/47789
    
    Signed-off-by: chengkai <chengkai@xiaomi.com>

--- subsys/bluetooth/host/hci_core.h
+++ subsys/bluetooth/host/hci_core.h
@@ -401,6 +401,7 @@ int bt_le_set_data_len(struct bt_conn *conn, uint16_t tx_octets, uint16_t tx_tim
 int bt_le_set_phy(struct bt_conn *conn, uint8_t all_phys,
 		  uint8_t pref_tx_phy, uint8_t pref_rx_phy, uint8_t phy_opts);
 uint8_t bt_get_phy(uint8_t hci_phy);
+int bt_le_read_conn_rssi(struct bt_conn *conn, int8_t *rssi);
 
 int bt_le_scan_update(bool fast_scan);
 
