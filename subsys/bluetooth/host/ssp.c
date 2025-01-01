9ade3fc5f9ac23ec67aadf5207c825514c3a8ca1
Haishen Zhang <zhanghaishen@xiaomi.com>
2025-01-01 21:13:42 +0800

    Fix: compiling warning
    bug: v/51417
    
    1. Fix compiling warning. Use strlcpy instead of strncpy.
    
    Signed-off-by: Haishen Zhang <zhanghaishen@xiaomi.com>

--- subsys/bluetooth/host/ssp.c
+++ subsys/bluetooth/host/ssp.c
@@ -72,7 +72,7 @@ static int pin_code_reply(struct bt_conn *conn, const char *pin, uint8_t len)
 
 	bt_addr_copy(&cp->bdaddr, &conn->br.dst);
 	cp->pin_len = len;
-	strncpy((char *)cp->pin_code, pin, sizeof(cp->pin_code));
+	strlcpy((char *)cp->pin_code, pin, sizeof(cp->pin_code));
 
 	return bt_hci_cmd_send_sync(BT_HCI_OP_PIN_CODE_REPLY, buf, NULL);
 }
