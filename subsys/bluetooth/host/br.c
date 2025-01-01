d26420afeeb0d85d7c5162745b76fbba1dde341b
Haishen Zhang <zhanghaishen@xiaomi.com>
2025-01-01 21:13:42 +0800

    Fix: compiling warning
    bug: v/51417
    
    1. Fix compiling warning. Use strlcpy instead of strncpy.
    
    Signed-off-by: Haishen Zhang <zhanghaishen@xiaomi.com>

--- subsys/bluetooth/host/br.c
+++ subsys/bluetooth/host/br.c
@@ -1301,7 +1301,7 @@ int bt_br_write_local_name(const char *name)
 	}
 
 	name_cp = net_buf_add(buf, sizeof(*name_cp));
-	strncpy((char *)name_cp->local_name, name,
+	strlcpy((char *)name_cp->local_name, name,
 		sizeof(name_cp->local_name));
 
 	return bt_hci_cmd_send_sync(BT_HCI_OP_WRITE_LOCAL_NAME, buf, NULL);
