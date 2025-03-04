2e4a9a542bff76adcfc921e2754f6ac5493ba9fe
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2025-03-04 21:04:56 +0100

    bluetooth: host: ecc: Change log level debug
    VELAPLATFO-78094
    
    This commit changes log level for already registered callback from
    warning to debug as this is cause unnecessary noise and doesn't indicate
    actual issue. Caller gets a certain error code for this case which can
    be handled properly.
    
    Change-Id: Ic84b151220f1ae4f39bab82eda1a0da09f7167b8
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>

--- subsys/bluetooth/host/ecc.c
+++ subsys/bluetooth/host/ecc.c
@@ -246,7 +246,7 @@ int bt_pub_key_gen(struct bt_dev *hdev, struct bt_pub_key_cb *new_cb)
 
 	SYS_SLIST_FOR_EACH_CONTAINER(&hdev->pub_key_cb_slist, cb, node) {
 		if (cb == new_cb) {
-			LOG_WRN("Callback already registered");
+			LOG_DBG("Callback already registered");
 			return -EALREADY;
 		}
 	}
