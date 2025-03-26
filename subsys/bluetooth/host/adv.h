862b5c7fb98077a0b35fd8c2a75d7cf8fa886269
fangzhenwei <fangzhenwei@xiaomi.com>
2025-03-26 10:18:49 +0800

    Bluetooth: GAP: Adv: multi-controller support
    Signed-off-by: fangzhenwei <fangzhenwei@xiaomi.com>

--- subsys/bluetooth/host/adv.h
+++ subsys/bluetooth/host/adv.h
@@ -5,19 +5,19 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-void bt_le_adv_resume(void);
+void bt_le_adv_resume(struct bt_dev *hdev);
 
-struct bt_le_ext_adv *bt_le_adv_lookup_legacy(void);
+struct bt_le_ext_adv *bt_le_adv_lookup_legacy(struct bt_dev *hdev);
 
-void bt_le_adv_delete_legacy(void);
+void bt_le_adv_delete_legacy(struct bt_dev *hdev);
 int bt_le_adv_set_enable(struct bt_le_ext_adv *adv, bool enable);
 
-void bt_le_ext_adv_foreach(void (*func)(struct bt_le_ext_adv *adv, void *data),
-			   void *data);
+void bt_le_ext_adv_foreach(struct bt_dev *hdev,
+			   void (*func)(struct bt_le_ext_adv *adv, void *data), void *data);
 
 int bt_le_adv_set_enable_ext(struct bt_le_ext_adv *adv,
 			 bool enable,
 			 const struct bt_le_ext_adv_start_param *param);
 int bt_le_adv_set_enable_legacy(struct bt_le_ext_adv *adv, bool enable);
 int bt_le_lim_adv_cancel_timeout(struct bt_le_ext_adv *adv);
-void bt_adv_reset_adv_pool(void);
+void bt_adv_reset_adv_pool(struct bt_dev *hdev);
