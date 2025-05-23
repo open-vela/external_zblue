35489616cdb46d6d71ed74902e09e11590f59b5e
Lyle Zhu <lyle.zhu@nxp.com>
2025-05-23 11:37:45 +0800

    Bluetooth: Classic: Fix LTK cannot be derived issue
    VELAPLATFO-76826
    
    The LTK cannot be derived by LK when LK is not weaker than the old LTK.
    
    Improve the function `smp_br_pairing_allowed()` to avoid the LTK be
    overwrote when old LTK has MITM protection but new LK has not MITM
    protection.
    
    Change-Id: I50a48ce6535e0ba06ac91a6995d5d3fc63c2bbf7
    Signed-off-by: Lyle Zhu <lyle.zhu@nxp.com>

--- subsys/bluetooth/host/smp.c
+++ subsys/bluetooth/host/smp.c
@@ -1256,7 +1256,7 @@ static bool smp_br_pairing_allowed(struct bt_smp_br *smp)
 	bt_addr_le_t addr;
 	struct bt_conn *conn;
 	struct bt_keys_link_key *key;
-	bool le_bonded;
+	struct bt_keys *le_keys;
 
 	if (!smp->chan.chan.conn) {
 		return false;
@@ -1266,7 +1266,7 @@ static bool smp_br_pairing_allowed(struct bt_smp_br *smp)
 
 	addr.type = BT_ADDR_LE_PUBLIC;
 	bt_addr_copy(&addr.a, &conn->br.dst);
-	le_bonded = bt_le_bond_exists(BT_ID_DEFAULT, &addr);
+	le_keys = bt_keys_find_addr(BT_ID_DEFAULT, &addr);
 
 	key = bt_keys_find_link_key(&conn->br.dst);
 	if (!key) {
@@ -1280,7 +1280,9 @@ static bool smp_br_pairing_allowed(struct bt_smp_br *smp)
 	 * or MITM protection, then neither device shall generate an LE LTK using cross-transport
 	 * key derivation from a BR/EDR link key.
 	 */
-	if (le_bonded && !(key->flags & BT_LINK_KEY_AUTHENTICATED)) {
+	if ((le_keys != NULL) && ((le_keys->flags & BT_KEYS_AUTHENTICATED) != 0) &&
+	    ((key->flags & BT_LINK_KEY_AUTHENTICATED) == 0)) {
+		LOG_WRN("Stronger LTK (MITM) cannot be overwrote by weaker LK");
 		return false;
 	}
 
