884ad5f151b5ec3766975701c5c43dab8d11f26e
huangyulong3 <huangyulong3@xiaomi.com>
2025-08-01 17:00:37 +0800

    zblue: a2dp conn not released, causing null pointer crash.
    Root cause: When the a2dp connection is disconnected, if the conn stored in `br_chan` has already been released, calling `raise_data_ready` will reference conn, causing a crash.
    
    Signed-off-by: huangyulong3 <huangyulong3@xiaomi.com>

--- subsys/bluetooth/host/classic/l2cap_br.c
+++ subsys/bluetooth/host/classic/l2cap_br.c
@@ -310,6 +310,11 @@ int bt_l2cap_br_send_cb(struct bt_conn *conn, uint16_t cid, struct net_buf *buf,
 	struct bt_l2cap_chan *ch = bt_l2cap_br_lookup_tx_cid(conn, cid);
 	struct bt_l2cap_br_chan *br_chan = CONTAINER_OF(ch, struct bt_l2cap_br_chan, chan);
 
+	if (!ch || !ch->conn) {
+		LOG_WRN("Don't find l2cap ch(%p) or conn(%p).", ch, ch ? ch->conn : 0);
+		return -ESHUTDOWN;
+	}
+
 	LOG_DBG("chan %p buf %p len %zu", br_chan, buf, buf->len);
 
 	hdr = net_buf_push(buf, sizeof(*hdr));
