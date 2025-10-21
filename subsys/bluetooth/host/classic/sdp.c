18e4f5cb5be60177a52eaecfa323487a88066e1c
Mark Wang <yichang.wang@nxp.com>
2025-10-21 20:20:44 +0800

    bluetooth: sdp: add avdtp protocol
    VELAPLATFO-78146
    
    get the avdtp version through the bt_sdp_get_proto_param
    
    Change-Id: If666540107749952c1c522a9a5bd7c9f88775332
    Signed-off-by: Mark Wang <yichang.wang@nxp.com>

--- subsys/bluetooth/host/classic/sdp.c
+++ subsys/bluetooth/host/classic/sdp.c
@@ -2849,7 +2849,8 @@ int bt_sdp_get_proto_param(const struct net_buf *buf, enum bt_sdp_proto proto,
 	struct bt_sdp_uuid_desc pd;
 	int res;
 
-	if (proto != BT_SDP_PROTO_RFCOMM && proto != BT_SDP_PROTO_L2CAP) {
+	if (proto != BT_SDP_PROTO_RFCOMM && proto != BT_SDP_PROTO_L2CAP &&
+	    proto != BT_SDP_PROTO_AVDTP) {
 		LOG_ERR("Invalid protocol specifier");
 		return -EINVAL;
 	}
@@ -2876,7 +2877,8 @@ int bt_sdp_get_addl_proto_param(const struct net_buf *buf, enum bt_sdp_proto pro
 	struct bt_sdp_uuid_desc pd;
 	int res;
 
-	if (proto != BT_SDP_PROTO_RFCOMM && proto != BT_SDP_PROTO_L2CAP) {
+	if (proto != BT_SDP_PROTO_RFCOMM && proto != BT_SDP_PROTO_L2CAP &&
+	    proto != BT_SDP_PROTO_AVDTP) {
 		LOG_ERR("Invalid protocol specifier");
 		return -EINVAL;
 	}
