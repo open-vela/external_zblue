185df72a1e1cf87eec85ae12c3a147afe04e62dd
Lu Jia <jialu@xiaomi.com>
2025-11-20 13:18:18 +0800

    avrcp: Add the port (PSM) used by AVRCP to _net_buf_pool_list for dedicated network buffer allocation.
    VELAPLATFO-78622
    
    Change-Id: I6ba0c01c1c01de5b565e5e27766860e92b93050f
    Signed-off-by: jialu <jialu@xiaomi.com>

--- port/sections/defines.c
+++ port/sections/defines.c
@@ -167,6 +167,11 @@ extern struct net_buf_pool acl_tx_pool;
 extern struct net_buf_pool bt_a2dp_tx_pool; // for openvela sal
 extern struct net_buf_pool avdtp_pool;
 extern struct net_buf_pool avdtp_frag_pool;
+extern struct net_buf_pool avrcp_vd_tx_pool;
+extern struct net_buf_pool avrcp_vd_rx_pool;
+extern struct net_buf_pool avctp_ctrl_rx_pool;
+extern struct net_buf_pool avctp_ctrl_tx_pool;
+extern struct net_buf_pool bt_avrcp_tx_pool;
 
 struct net_buf_pool *_net_buf_pool_list[] = {
 #if defined(CONFIG_BT_HCI)
@@ -225,6 +230,13 @@ struct net_buf_pool *_net_buf_pool_list[] = {
 #if defined(CONFIG_BT_A2DP_SOURCE)
 	&bt_a2dp_tx_pool,
 #endif /* CONFIG_BT_A2DP_SOURCE */
+#if defined(CONFIG_BT_AVRCP)
+	&avrcp_vd_tx_pool,
+	&avrcp_vd_rx_pool,
+	&avctp_ctrl_rx_pool,
+	&avctp_ctrl_tx_pool,
+	&bt_avrcp_tx_pool,
+#endif /* CONFIG_BT_AVRCP */
 #if defined(CONFIG_BT_RFCOMM)
 	// &dummy_pool,
 #endif /* CONFIG_BT_RFCOMM */
