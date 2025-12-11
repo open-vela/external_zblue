56f8cdb2323c3966e72e6ecb091d9f08956210d9
liuxiang18 <liuxiang18@xiaomi.com>
2025-12-11 16:39:29 +0800

    Revert "Bluetooth: Classic: GAP: improve classic bond info"
    This reverts commit 6c3715894dd9a12a86a1a01ae5cecc677fff72d7.
    
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

--- include/zephyr/bluetooth/classic/classic.h
+++ include/zephyr/bluetooth/classic/classic.h
@@ -407,62 +407,6 @@ static inline int bt_br_write_ext_inq_response(uint8_t fec_required)
 }
 #endif
 
-/** Information about a br/edr bond with a remote device. */
-struct bt_bond_info_br {
-	bt_addr_t addr;
-	uint8_t   key_type;
-	uint8_t   key[16];
-};
-
-/**
- * @brief Callback for iterating over all BR/EDR bond information.
- *
- * @param info Bond information.
- * @param user_data Data passed to the iterator.
- */
-void bt_foreach_bond_br_mc(uint8_t dev_id, void (*func)(const struct bt_bond_info_br *info, void *user_data),
-			void *user_data);
-#ifdef CONFIG_BT_ORIGINAL_API
-static inline void bt_foreach_bond_br(void (*func)(const struct bt_bond_info_br *info, void *user_data),
-			void *user_data)
-{
-	bt_foreach_bond_br_mc(0, func, user_data);
-}
-#endif
-
-/**
- * @brief Set BR/EDR bond information.
- *
- * @param info Bond information.
- *
- * @return Zero on success or error code otherwise, positive in case
- * of protocol error or negative (POSIX) in case of stack internal error.
- */
-int bt_set_bond_info_br_mc(uint8_t dev_id, const struct bt_bond_info_br *info);
-#ifdef CONFIG_BT_ORIGINAL_API
-static inline int bt_set_bond_info_br(const struct bt_bond_info_br *info)
-{
-	return bt_set_bond_info_br_mc(0, info);
-}
-#endif
-
-/**
- * @brief Get BR/EDR bond information.
- *
- * @param bdaddr Address of the remote device.
- * @param info Bond information.
- *
- * @return Zero on success or error code otherwise, positive in case
- * of protocol error or negative (POSIX) in case of stack internal error.
- */
-int bt_get_bond_info_br_mc(uint8_t dev_id, const bt_addr_t* bdaddr, struct bt_bond_info_br *info);
-#ifdef CONFIG_BT_ORIGINAL_API
-static inline int bt_get_bond_info_br(const bt_addr_t* bdaddr, struct bt_bond_info_br *info)
-{
-	return bt_get_bond_info_br_mc(0, bdaddr, info);
-}
-#endif
-
 /**
  * @brief Request remote device name callback.
  *
