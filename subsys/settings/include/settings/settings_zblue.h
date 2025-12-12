aa4f708fc4997782606a208e18a5d0961d02db53
liuxiang18 <liuxiang18@xiaomi.com>
2025-12-12 12:02:48 +0800

    settings: add settings_zblue module.
    bug: v/79393
    
    Implement the settings_zblue module, include:
    1. Define the bt_settings_zblue_cb callback function
    2. Implement the bt_setting_cb_register function
    3. Implement the settings_zblue_itf hook function
    4. add config SETTINGS_ZBLUE.
    
    Signed-off-by: liuxiang18 <liuxiang18@xiaomi.com>

---
+++ subsys/settings/include/settings/settings_zblue.h
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2025 liuxiang
+ * Copyright (c) 2025 Xiaomi Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __SETTINGS_ZBLUE_H_
+#define __SETTINGS_ZBLUE_H_
+
+#include <zephyr/bluetooth/addr.h>
+#include <zephyr/settings/settings.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* In the zblue backend
+ *
+ * Application integration requirements:
+ * The application must register the callback function bt_settings_zblue_cb to receive:
+ *   - keys notification
+ *   - keys load
+ *
+ * About the XX_load callback of bt_settings_zblue_cb:
+ *
+ * The application must explicitly call bt_settings_load() API to trigger the loading of the
+ * setting. This will invoke the settings subsystem (via handlers registered with
+ * BT_SETTINGS_DEFINE) to process the value through the corresponding XX_load callback.
+ */
+
+struct settings_zblue {
+    struct settings_store cf_store;
+};
+
+struct settings_zblue_save_fn_arg {
+    bt_addr_le_t addr;
+    const char* value;
+};
+
+struct bt_settings_zblue_cb {
+#if defined(CONFIG_BT_CLASSIC)
+    /** @brief notify that bredr pairing link key generated.
+     *
+     *  The callback notifies the application that the link key has been
+     *  generated during the pairing procedure.
+     *
+     *  @param dev_id Device identifier number.
+     *  @param addr Remote address.
+     *  @param key_value Data streams requiring storage.
+     *  @param value value length.
+     */
+    int (*linkkey_notify)(uint8_t dev_id, bt_addr_le_t* addr, const char* key_value, uint8_t value_len);
+
+    /** @brief load BREDR link key.
+     *
+     *  The callback is responsible for retrieving the link_key data
+     *  from the application.
+     *
+     *  @param addr Remote address.
+     *  @param key_value Target memory address to which the private key needs to be copied.
+     *  @param value_len key_value length.
+     */
+    int (*linkkey_load)(bt_addr_le_t* addr, uint8_t* key_value, uint8_t value_len);
+#endif
+
+    /** @brief notify that BLE pairing LTK generated.
+     *
+     *  The callback notifies the application that the LTK has been
+     *  generated during the pairing procedure.
+     *
+     *  @param dev_id Device identifier number.
+     *  @param id identifier number.
+     *  @param addr Remote LE identity address.
+     *  @param key_value Data streams requiring storage.
+     *  @param value_len key_value length.
+     */
+    int (*ltk_notify)(uint8_t dev_id, uint8_t id, bt_addr_le_t* addr, const char* key_value, uint8_t value_len);
+
+    /** @brief load BLE LTK.
+     *
+     *  The callback is responsible for retrieving the LTK data
+     *  from the application.
+     *
+     *  @param addr Remote identity address.
+     *  @param key_value Target memory address to which the private key needs to be copied.
+     *  @param value_len key_value length.
+     */
+    int (*ltk_load)(bt_addr_le_t* addr, uint8_t* key_value, uint8_t value_len);
+
+    /** @internal Internally used field for list handling */
+    sys_snode_t _node;
+};
+
+/** @brief Register Bluetooth settings callbacks.
+ *
+ *  Register callbacks to handle Bluetooth settings operations,
+ *  such as key notifications and key loading.
+ *
+ *  @param cb Callback struct. Must point to memory that remains valid.
+ *
+ * @retval 0 Success.
+ * @retval -EEXIST if @p cb was already registered.
+ */
+int bt_setting_cb_register(struct bt_settings_zblue_cb* cb);
+
+/* register zblue to be a source of settings */
+int settings_zblue_src(struct settings_zblue* cf);
+
+/* register zblue to be the destination of settings */
+int settings_zblue_dst(struct settings_zblue* cf);
+
+/** @brief Load Bluetooth settings for a device.
+ *
+ * Loads Bluetooth settings for the specified device and key. This function is typically
+ * called to retrieve and apply stored Bluetooth configuration data (such as keys or addresses)
+ * for a given device.
+ *
+ * @param dev_id   Device identifier number.
+ * @param id       identifier number.
+ * @param key      Key string identifying the setting to load.
+ * @param addr     Remote identity address.
+ *
+ * @return Zero on success, or a negative error code on failure.
+ */
+int bt_settings_load(uint8_t dev_id, uint8_t id, const char* key, bt_addr_le_t* addr);
+
+/* Initialize a zblue backend. */
+int settings_zblue_backend_init(struct settings_zblue* cf);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SETTINGS_ZBLUE_H_ */
---
+++ subsys/settings/src/settings_zblue.c
@@ -0,0 +1,150 @@
+/*
+ * Copyright (c) 2025 Liuxiang
+ * Copyright (c) 2019 Xiaomi Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <string.h>
+
+#include <zephyr/settings/settings.h>
+
+#include "settings/settings_zblue.h"
+#include <conn_internal.h>
+#include <iso_internal.h>
+
+#include <hci_core.h>
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/logging/log.h>
+
+#include "settings.h"
+
+LOG_MODULE_DECLARE(settings, CONFIG_SETTINGS_LOG_LEVEL);
+
+static int settings_zblue_load(struct settings_store* cs,
+    const struct settings_load_arg* arg);
+static int settings_zblue_save(struct settings_store* cs, const char* name,
+    const char* value, size_t val_len);
+
+sys_slist_t bt_settings_cbs;
+
+static struct settings_store_itf settings_zblue_itf = {
+    .csi_load = settings_zblue_load,
+    .csi_save = settings_zblue_save,
+};
+
+static struct settings_zblue default_settings_zblue;
+
+int settings_zblue_src(struct settings_zblue* cf)
+{
+    cf->cf_store.cs_itf = &settings_zblue_itf;
+    settings_src_register(&cf->cf_store);
+
+    return 0;
+}
+
+int settings_zblue_dst(struct settings_zblue* cf)
+{
+    cf->cf_store.cs_itf = &settings_zblue_itf;
+    settings_dst_register(&cf->cf_store);
+
+    return 0;
+}
+
+static ssize_t settings_zblue_read(void* back_end, void* data, size_t len)
+{
+    struct bt_settings_zblue_cb *listener, *next;
+    char* name = (char*)back_end;
+    bt_addr_le_t addr;
+    int res = 0;
+
+    return res;
+}
+
+static int settings_zblue_load(struct settings_store* cs,
+    const struct settings_load_arg* arg)
+{
+    LOG_DBG("%s", __func__);
+    return 0;
+}
+
+int bt_settings_load(uint8_t dev_id, uint8_t id, const char* key, bt_addr_le_t* addr)
+{
+    int err, len = 0;
+    char id_str[4];
+    char dev_id_str[4];
+    char key_str[BT_SETTINGS_KEY_MAX];
+
+    if (addr) {
+        if (id) {
+            u8_to_dec(id_str, sizeof(id_str), id);
+        }
+
+        u8_to_dec(dev_id_str, sizeof(dev_id_str), dev_id);
+        bt_settings_encode_key(key_str, sizeof(key_str), key, addr, (id ? id_str : NULL), dev_id_str);
+    } else {
+        err = snprintk(key_str, sizeof(key_str), "bt/%s/%d", key, dev_id);
+        if (err < 0) {
+            return -EINVAL;
+        }
+    }
+
+    settings_call_set_handler(
+        key_str, len /* unused */,
+        settings_zblue_read /* read_cb */, key_str /* read_cb_arg */,
+        NULL);
+
+    return 0;
+}
+
+static void parse_settings_key(const char* name, uint8_t* dev_id, uint8_t* id, bt_addr_le_t* addr)
+{
+    const char *id_next, *dev_next;
+
+    /* parse addr */
+    bt_settings_decode_key(name, addr);
+
+    /* parse dev_id(multi-controller) */
+    settings_name_next(name, &dev_next);
+    *dev_id = strtoul(dev_next, NULL, 10);
+
+    /* parse id(optional) */
+    settings_name_next(dev_next, &id_next);
+    if (id_next)
+        *id = strtoul(id_next, NULL, 10);
+    else
+        *id = BT_ID_DEFAULT;
+}
+
+static int settings_zblue_save(struct settings_store* cs, const char* name,
+    const char* value, size_t val_len)
+{
+    struct bt_settings_zblue_cb *listener, *next;
+    bt_addr_le_t addr;
+    uint8_t dev_id, id;
+    int err = 0;
+
+    LOG_DBG("%s", __func__);
+
+    return err;
+}
+
+int bt_setting_cb_register(struct bt_settings_zblue_cb* cb)
+{
+
+    if (sys_slist_find(&bt_settings_cbs, &cb->_node, NULL)) {
+        return -EEXIST;
+    }
+
+    sys_slist_append(&bt_settings_cbs, &cb->_node);
+
+    return 0;
+}
+
+int settings_backend_init(void)
+{
+    settings_zblue_src(&default_settings_zblue);
+    settings_zblue_dst(&default_settings_zblue);
+    return 0;
+}
