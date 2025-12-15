galak/zephyr

samples: openamp: Update sample to OpenAMP v2018.10 release
The v2018.10 release of OpenAMP reworks the API set and splits the
remoteproc vs rpmsg interfaces so one can use rpmsg without remoteproc.

This helps drastically reduce the code footprint utilized by OpenAMP.
The remote see around 4k reduction in code size.

Signed-off-by: Kumar Gala <kumar.gala@linaro.org>

samples/subsys/ipc/openamp/common.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ * Copyright (c) 2018 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef COMMON_H__
+#define COMMON_H__
+
+#define SHM_START_ADDR		0x04000400
+#define SHM_SIZE		0x7c00
+#define SHM_DEVICE_NAME		"sramx.shm"
+
+#define VRING_COUNT		2
+#define VRING_RX_ADDRESS	0x04007800
+#define VRING_TX_ADDRESS	0x04007C00
+#define VRING_ALIGNMENT		4
+#define VRING_SIZE		16
+
+#define RSC_TABLE_ADDRESS	0x04000000
+
+#endif

open-vela/external_zblue

samples/subsys/ipc/openamp/common.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright (c) 2018 Nordic Semiconductor ASA
+ * Copyright (c) 2018 Linaro Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef COMMON_H__
+#define COMMON_H__
+
+#define SHM_START_ADDR		0x04000400
+#define SHM_SIZE		0x7c00
+#define SHM_DEVICE_NAME		"sramx.shm"
+
+#define VRING_COUNT		2
+#define VRING_RX_ADDRESS	0x04007800
+#define VRING_TX_ADDRESS	0x04007C00
+#define VRING_ALIGNMENT		4
+#define VRING_SIZE		16
+
+#define VDEV_STATUS_ADDR	0x04000000
+
+#endif