zephyrproject-rtos/zephyr

arch: xtensa: Get CPU clock frequency from DTS
The SoC initialization code used system clock frequency
as a CPU clock frequency. This commit corrects that by
obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

arch/xtensa/include/xtensa_rtos.h
@@ -46,10 +46,8 @@
   #define XT_BOARD 1
 #endif
 
-#ifdef CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
-  #undef XT_CLOCK_FREQ
-  #define XT_CLOCK_FREQ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
-#endif
+#undef  XT_CLOCK_FREQ
+#define XT_CLOCK_FREQ DT_CPUS_CPU_0_CLOCK_FREQUENCY
 
 #ifndef XT_TIMER_INDEX
   #if defined configXT_TIMER_INDEX

arch/xtensa/include/xtensa_timer.h
@@ -115,7 +115,7 @@
  * anyway!).
  */
 #if defined(XT_SIMULATOR) && !defined(XT_CLOCK_FREQ)
-#define XT_CLOCK_FREQ       CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define XT_CLOCK_FREQ	DT_CPUS_CPU_0_CLOCK_FREQUENCY
 #endif
 
 #if !defined(XT_CLOCK_FREQ) && !defined(XT_BOARD)

open-vela/external_zblue

arch/xtensa/include/xtensa_rtos.h
@@ -46,10 +46,8 @@
   #define XT_BOARD 1
 #endif
 
-#ifdef CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
-  #undef XT_CLOCK_FREQ
-  #define XT_CLOCK_FREQ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
-#endif
+#undef  XT_CLOCK_FREQ
+#define XT_CLOCK_FREQ DT_CPU_CLOCK_FREQUENCY
 
 #ifndef XT_TIMER_INDEX
   #if defined configXT_TIMER_INDEX

arch/xtensa/include/xtensa_timer.h
@@ -115,7 +115,7 @@
  * anyway!).
  */
 #if defined(XT_SIMULATOR) && !defined(XT_CLOCK_FREQ)
-#define XT_CLOCK_FREQ       CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define XT_CLOCK_FREQ	DT_CPU_CLOCK_FREQUENCY
 #endif
 
 #if !defined(XT_CLOCK_FREQ) && !defined(XT_BOARD)