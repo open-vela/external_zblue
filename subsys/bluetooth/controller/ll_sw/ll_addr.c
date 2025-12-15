zephyrproject-rtos/zephyr

Bluetooth: controller: split: Allow set rand addr when using ext cmds
Allow use of HCI LE Set Random Address cmd when advertising
is enabled using Extended Advertising commands.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/ll_addr.c
@@ -51,9 +51,16 @@ u8_t *ll_addr_get(u8_t addr_type, u8_t *bdaddr)
 
 u32_t ll_addr_set(u8_t addr_type, u8_t const *const bdaddr)
 {
-	if (IS_ENABLED(CONFIG_BT_BROADCASTER) &&
-	    ull_adv_is_enabled(0)) {
-		return BT_HCI_ERR_CMD_DISALLOWED;
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER)) {
+		u32_t status = ull_adv_is_enabled(0);
+
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+		if ((status & 5U) == 1U) {
+#else /* !CONFIG_BT_CTLR_ADV_EXT */
+		if (status) {
+#endif /* !CONFIG_BT_CTLR_ADV_EXT */
+			return BT_HCI_ERR_CMD_DISALLOWED;
+		}
 	}
 
 	if (IS_ENABLED(CONFIG_BT_OBSERVER) &&

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -122,8 +122,6 @@ u8_t ll_adv_params_set(u16_t interval, u8_t adv_type,
 	 * evt_prop bits.
 	 */
 
-	adv->lll.phy_p = BIT(0);
-
 	/* extended adv param set */
 	if (adv_type == PDU_ADV_TYPE_EXT_IND) {
 		/* legacy */
@@ -137,8 +135,7 @@ u8_t ll_adv_params_set(u16_t interval, u8_t adv_type,
 				adv_type = 0x01;
 			}
 
-			/* Mark the adv set as created */
-			adv->is_created = 1;
+			adv->lll.phy_p = BIT(0);
 		} else {
 			/* - Connectable and scannable not allowed;
 			 * - High duty cycle directed connectable not allowed
@@ -153,6 +150,14 @@ u8_t ll_adv_params_set(u16_t interval, u8_t adv_type,
 
 			adv->lll.phy_p = phy_p;
 		}
+
+		/* Mark the adv set as created by extended advertising cmd */
+		adv->is_created = 3U;
+	} else {
+		adv->lll.phy_p = BIT(0);
+
+		/* Mark the adv set as created by legacy advertising cmd */
+		adv->is_created = 1U;
 	}
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
@@ -353,9 +358,6 @@ u8_t ll_adv_params_set(u16_t interval, u8_t adv_type,
 		/* NOTE: TargetA, filled at enable and RPA timeout */
 
 		/* NOTE: AdvA, filled at enable and RPA timeout */
-
-		/* Mark the adv set as created */
-		adv->is_created = 1;
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 	} else if (pdu->len == 0) {
@@ -1147,7 +1149,11 @@ u32_t ull_adv_is_enabled(u8_t handle)
 		return 0;
 	}
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+	return BIT(0) | ((u32_t)adv->is_created << 1);
+#else /* !CONFIG_BT_CTLR_ADV_EXT */
 	return BIT(0);
+#endif /* !CONFIG_BT_CTLR_ADV_EXT */
 }
 
 u32_t ull_adv_filter_pol_get(u8_t handle)

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -18,7 +18,7 @@ struct ll_adv_set {
 	u32_t interval;
 	u8_t  rnd_addr[BDADDR_SIZE];
 	u8_t  sid:4;
-	u8_t  is_created:1;
+	u8_t  is_created:2;
 #else /* !CONFIG_BT_CTLR_ADV_EXT */
 	u16_t interval;
 #endif /* !CONFIG_BT_CTLR_ADV_EXT */

open-vela/external_zblue

subsys/bluetooth/controller/ll_sw/ll_addr.c
@@ -51,9 +51,16 @@ uint8_t *ll_addr_get(uint8_t addr_type, uint8_t *bdaddr)
 
 uint32_t ll_addr_set(uint8_t addr_type, uint8_t const *const bdaddr)
 {
-	if (IS_ENABLED(CONFIG_BT_BROADCASTER) &&
-	    ull_adv_is_enabled(0)) {
-		return BT_HCI_ERR_CMD_DISALLOWED;
+	if (IS_ENABLED(CONFIG_BT_BROADCASTER)) {
+		uint32_t status = ull_adv_is_enabled(0);
+
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+		if ((status & 5U) == 1U) {
+#else /* !CONFIG_BT_CTLR_ADV_EXT */
+		if (status) {
+#endif /* !CONFIG_BT_CTLR_ADV_EXT */
+			return BT_HCI_ERR_CMD_DISALLOWED;
+		}
 	}
 
 	if (IS_ENABLED(CONFIG_BT_OBSERVER) &&

subsys/bluetooth/controller/ll_sw/ull_adv.c
@@ -122,8 +122,6 @@ uint8_t ll_adv_params_set(uint16_t interval, uint8_t adv_type,
 	 * evt_prop bits.
 	 */
 
-	adv->lll.phy_p = BIT(0);
-
 	/* extended adv param set */
 	if (adv_type == PDU_ADV_TYPE_EXT_IND) {
 		/* legacy */
@@ -137,8 +135,7 @@ uint8_t ll_adv_params_set(uint16_t interval, uint8_t adv_type,
 				adv_type = 0x01;
 			}
 
-			/* Mark the adv set as created */
-			adv->is_created = 1;
+			adv->lll.phy_p = BIT(0);
 		} else {
 			/* - Connectable and scannable not allowed;
 			 * - High duty cycle directed connectable not allowed
@@ -153,6 +150,14 @@ uint8_t ll_adv_params_set(uint16_t interval, uint8_t adv_type,
 
 			adv->lll.phy_p = phy_p;
 		}
+
+		/* Mark the adv set as created by extended advertising cmd */
+		adv->is_created = 3U;
+	} else {
+		adv->lll.phy_p = BIT(0);
+
+		/* Mark the adv set as created by legacy advertising cmd */
+		adv->is_created = 1U;
 	}
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
@@ -353,9 +358,6 @@ uint8_t ll_adv_params_set(uint16_t interval, uint8_t adv_type,
 		/* NOTE: TargetA, filled at enable and RPA timeout */
 
 		/* NOTE: AdvA, filled at enable and RPA timeout */
-
-		/* Mark the adv set as created */
-		adv->is_created = 1;
 #endif /* CONFIG_BT_CTLR_ADV_EXT */
 
 	} else if (pdu->len == 0) {
@@ -1150,7 +1152,11 @@ uint32_t ull_adv_is_enabled(uint8_t handle)
 		return 0;
 	}
 
+#if defined(CONFIG_BT_CTLR_ADV_EXT)
+	return BIT(0) | ((uint32_t)adv->is_created << 1);
+#else /* !CONFIG_BT_CTLR_ADV_EXT */
 	return BIT(0);
+#endif /* !CONFIG_BT_CTLR_ADV_EXT */
 }
 
 uint32_t ull_adv_filter_pol_get(uint8_t handle)

subsys/bluetooth/controller/ll_sw/ull_adv_types.h
@@ -18,7 +18,7 @@ struct ll_adv_set {
 	uint32_t interval;
 	uint8_t  rnd_addr[BDADDR_SIZE];
 	uint8_t  sid:4;
-	uint8_t  is_created:1;
+	uint8_t  is_created:2;
 #else /* !CONFIG_BT_CTLR_ADV_EXT */
 	uint16_t interval;
 #endif /* !CONFIG_BT_CTLR_ADV_EXT */