lindemer/zephyr

arch: riscv: add memory protection support
The IRQ handler has had a major changes to manage syscall, reschedule
and interrupt from user thread and stack guard.

Add userspace support:
- Use a global variable to know if the current execution is user or
  machine. The location of this variable is read only for all user
  thread and read/write for kernel thread.
- Memory shared is supported.
- Use dynamic allocation to optimize PMP slot usage. If the area size
  is a power of 2, only one PMP slot is used, else 2 are used.

Add stack guard support:
- Use MPRV bit to force PMP rules to machine mode execution.
- IRQ stack have a locked stack guard to avoid re-write PMP
  configuration registers for each interruption and then win some
  cycle.
- The IRQ stack is used as "temporary" stack at the beginning of IRQ
  handler to save current ESF. That avoid to trigger write fault on
  thread stack during store ESF which that call IRQ handler to
  infinity.
- A stack guard is also setup for privileged stack of a user thread.

Thread:
- A PMP setup is specific to each thread. PMP setup are saved in each
  thread structure to improve reschedule performance.

Signed-off-by: Alexandre Mergnat <amergnat@baylibre.com>
Reviewed-by: Nicolas Royer <nroyer@baylibre.com>

arch/riscv/include/core_pmp.h
@@ -23,6 +23,16 @@
 #define FROM_PMP_ADDR(addr)	((addr) << 2)
 #define TO_NAPOT_RANGE(size)	(((size) - 1) >> 1)
 
+#ifdef CONFIG_PMP_STACK_GUARD
+
+#define PMP_GUARD_ALIGN_AND_SIZE CONFIG_PMP_STACK_GUARD_MIN_SIZE
+
+#else
+
+#define PMP_GUARD_ALIGN_AND_SIZE 0
+
+#endif /* CONFIG_PMP_STACK_GUARD */
+
 #ifdef CONFIG_RISCV_PMP
 
 /*
@@ -50,5 +60,80 @@ void pmp_clear_config(void);
  */
 void pmp_print(unsigned int index);
 #endif /* CONFIG_RISCV_PMP */
+
+#if defined(CONFIG_USERSPACE)
+
+/*
+ * @brief Configure RISCV user thread access to the stack
+ *
+ * Determine and save allow access setup in thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void init_user_accesses(struct k_thread *thread);
+
+/*
+ * @brief Apply RISCV user thread access to the stack
+ *
+ * Write user access setup saved in this thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void configure_user_allowed_stack(struct k_thread *thread);
+
+/*
+ * @brief Add a new RISCV stack access
+ *
+ * Add a new memory permission area in the existing
+ * pmp setup of the thread.
+ *
+ * @param thread Thread info data pointer.
+ * @param addr   Start address of the memory area.
+ * @param size   Size of the memory area.
+ * @param flags  Pemissions: PMP_R, PMP_W, PMP_X, PMP_L
+ */
+void pmp_add_dynamic(struct k_thread *thread,
+			ulong_t addr,
+			ulong_t size,
+			unsigned char flags);
+#endif /* CONFIG_USERSPACE */
+
+#ifdef CONFIG_PMP_STACK_GUARD
+
+/*
+ * @brief Configure RISCV stack guard for interrupt stack
+ *
+ * Write PMP registers to prevent RWX access from all privilege mode.
+ */
+void configure_interrupt_stack_guard(void);
+
+/*
+ * @brief Configure RISCV stack guard
+ *
+ * Determine and save stack guard setup in thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void init_stack_guard(struct k_thread *thread);
+
+/*
+ * @brief Apply RISCV stack guard
+ *
+ * Write stack guard setup saved in this thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void configure_stack_guard(struct k_thread *thread);
+#endif /* CONFIG_PMP_STACK_GUARD */
+
+#if defined(CONFIG_PMP_STACK_GUARD) || defined(CONFIG_USERSPACE)
+
+/*
+ * @brief Initialize thread PMP setup value to 0
+ *
+ * @param thread Thread info data pointer.
+ */
+void pmp_init_thread(struct k_thread *thread);
+#endif /* CONFIG_PMP_STACK_GUARD || CONFIG_USERSPACE */
 
 #endif /* CORE_PMP_H_ */

open-vela/external_zblue

arch/riscv/include/core_pmp.h
@@ -26,6 +26,16 @@
 #define TO_PMP_NAPOT(addr, size)	TO_PMP_ADDR(addr | \
 					TO_NAPOT_RANGE(size))
 
+#ifdef CONFIG_PMP_STACK_GUARD
+
+#define PMP_GUARD_ALIGN_AND_SIZE CONFIG_PMP_STACK_GUARD_MIN_SIZE
+
+#else
+
+#define PMP_GUARD_ALIGN_AND_SIZE 0
+
+#endif /* CONFIG_PMP_STACK_GUARD */
+
 #ifdef CONFIG_RISCV_PMP
 
 /*
@@ -53,5 +63,80 @@ void z_riscv_pmp_clear_config(void);
  */
 void z_riscv_pmp_print(unsigned int index);
 #endif /* CONFIG_RISCV_PMP */
+
+#if defined(CONFIG_USERSPACE)
+
+/*
+ * @brief Configure RISCV user thread access to the stack
+ *
+ * Determine and save allow access setup in thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void z_riscv_init_user_accesses(struct k_thread *thread);
+
+/*
+ * @brief Apply RISCV user thread access to the stack
+ *
+ * Write user access setup saved in this thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void z_riscv_configure_user_allowed_stack(struct k_thread *thread);
+
+/*
+ * @brief Add a new RISCV stack access
+ *
+ * Add a new memory permission area in the existing
+ * pmp setup of the thread.
+ *
+ * @param thread Thread info data pointer.
+ * @param addr   Start address of the memory area.
+ * @param size   Size of the memory area.
+ * @param flags  Pemissions: PMP_R, PMP_W, PMP_X, PMP_L
+ */
+void z_riscv_pmp_add_dynamic(struct k_thread *thread,
+			ulong_t addr,
+			ulong_t size,
+			unsigned char flags);
+#endif /* CONFIG_USERSPACE */
+
+#ifdef CONFIG_PMP_STACK_GUARD
+
+/*
+ * @brief Configure RISCV stack guard for interrupt stack
+ *
+ * Write PMP registers to prevent RWX access from all privilege mode.
+ */
+void z_riscv_configure_interrupt_stack_guard(void);
+
+/*
+ * @brief Configure RISCV stack guard
+ *
+ * Determine and save stack guard setup in thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void z_riscv_init_stack_guard(struct k_thread *thread);
+
+/*
+ * @brief Apply RISCV stack guard
+ *
+ * Write stack guard setup saved in this thread structure.
+ *
+ * @param thread Thread info data pointer.
+ */
+void z_riscv_configure_stack_guard(struct k_thread *thread);
+#endif /* CONFIG_PMP_STACK_GUARD */
+
+#if defined(CONFIG_PMP_STACK_GUARD) || defined(CONFIG_USERSPACE)
+
+/*
+ * @brief Initialize thread PMP setup value to 0
+ *
+ * @param thread Thread info data pointer.
+ */
+void z_riscv_pmp_init_thread(struct k_thread *thread);
+#endif /* CONFIG_PMP_STACK_GUARD || CONFIG_USERSPACE */
 
 #endif /* CORE_PMP_H_ */