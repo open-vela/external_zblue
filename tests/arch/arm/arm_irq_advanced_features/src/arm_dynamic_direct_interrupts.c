zephyrproject-rtos/zephyr

tests: arch: arm: add test-suite for dynamic direct IRQs
We add a test-suite for the newly introduced feature of
ARM Dynamic Direct Interrupts.

Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>

tests/arch/arm/arm_irq_advanced_features/src/arm_dynamic_direct_interrupts.c
@@ -1,0 +1,86 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+#include <arch/cpu.h>
+#include <arch/arm/cortex_m/cmsis.h>
+
+/* Offset for the Direct interrupt used in this test. */
+#define DIRECT_ISR_OFFSET (CONFIG_NUM_IRQS - 1)
+
+static volatile int test_flag;
+
+void arm_direct_isr_handler_0(void *args)
+{
+        ARG_UNUSED(args);
+
+        test_flag = 1;
+}
+
+void arm_direct_isr_handler_1(void *args)
+{
+        ARG_UNUSED(args);
+
+        test_flag = 2;
+}
+
+void test_arm_dynamic_direct_interrupts(void)
+{
+        int post_flag = 0;
+
+        /* Place the dynamic interrupt dispatcher (with no rescheduling)
+         * in the ROM ISR table.
+         */
+         ARM_IRQ_DIRECT_DYNAMIC_CONNECT_NO_RESCHEDULE(DIRECT_ISR_OFFSET, 0, 0);
+
+        /* Attach the ISR handler at run time. */
+        arch_irq_connect_dynamic(DIRECT_ISR_OFFSET, 0 /* highest priority */,
+                arm_direct_isr_handler_0,
+                NULL,
+                0);
+
+        /* Enable and pend the interrupt */
+        irq_enable(DIRECT_ISR_OFFSET);
+        NVIC_SetPendingIRQ(DIRECT_ISR_OFFSET);
+
+        /*
+         * Instruction barriers to make sure the NVIC IRQ is
+         * set to pending state before 'test_flag' is checked.
+         */
+         __DSB();
+         __ISB();
+
+         /* Confirm test flag is set by the dynamic direct ISR handler. */
+         post_flag = test_flag;
+         zassert_true(post_flag == 1, "Test flag not set by ISR0\n");
+
+         post_flag = 0;
+         irq_disable(DIRECT_ISR_OFFSET);
+
+        /* Attach an alternative ISR handler at run-time. */
+        arch_irq_connect_dynamic(DIRECT_ISR_OFFSET, 0 /* highest priority */,
+                arm_direct_isr_handler_1,
+                NULL,
+                0);
+
+        /* Enable and pend the interrupt */
+        irq_enable(DIRECT_ISR_OFFSET);
+        NVIC_SetPendingIRQ(DIRECT_ISR_OFFSET);
+
+        /*
+         * Instruction barriers to make sure the NVIC IRQ is
+         * set to pending state before 'test_flag' is checked.
+         */
+        __DSB();
+        __ISB();
+
+        /* Confirm test flag is set by the dynamic direct ISR handler. */
+        post_flag = test_flag;
+        zassert_true(post_flag == 2, "Test flag not set by ISR1\n");
+}
+/**
+ * @}
+ */

open-vela/external_zblue

tests/arch/arm/arm_irq_advanced_features/src/arm_dynamic_direct_interrupts.c
@@ -1,0 +1,86 @@
+/*
+ * Copyright (c) 2019 Nordic Semiconductor ASA.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+#include <arch/cpu.h>
+#include <arch/arm/cortex_m/cmsis.h>
+
+/* Offset for the Direct interrupt used in this test. */
+#define DIRECT_ISR_OFFSET (CONFIG_NUM_IRQS - 1)
+
+static volatile int test_flag;
+
+void arm_direct_isr_handler_0(void *args)
+{
+	ARG_UNUSED(args);
+
+	test_flag = 1;
+}
+
+void arm_direct_isr_handler_1(void *args)
+{
+	ARG_UNUSED(args);
+
+	test_flag = 2;
+}
+
+void test_arm_dynamic_direct_interrupts(void)
+{
+	int post_flag = 0;
+
+	/* Place the dynamic interrupt dispatcher (with no rescheduling)
+	 * in the ROM ISR table.
+	 */
+	ARM_IRQ_DIRECT_DYNAMIC_CONNECT(DIRECT_ISR_OFFSET, 0, 0, no_reschedule);
+
+	/* Attach the ISR handler at run time. */
+	irq_connect_dynamic(DIRECT_ISR_OFFSET, 0 /* highest priority */,
+		arm_direct_isr_handler_0,
+		NULL,
+		0);
+
+	/* Enable and pend the interrupt */
+	irq_enable(DIRECT_ISR_OFFSET);
+	NVIC_SetPendingIRQ(DIRECT_ISR_OFFSET);
+
+	/*
+	 * Instruction barriers to make sure the NVIC IRQ is
+	 * set to pending state before 'test_flag' is checked.
+	 */
+	__DSB();
+	__ISB();
+
+	/* Confirm test flag is set by the dynamic direct ISR handler. */
+	post_flag = test_flag;
+	zassert_true(post_flag == 1, "Test flag not set by ISR0\n");
+
+	post_flag = 0;
+	irq_disable(DIRECT_ISR_OFFSET);
+
+	/* Attach an alternative ISR handler at run-time. */
+	irq_connect_dynamic(DIRECT_ISR_OFFSET, 0 /* highest priority */,
+		arm_direct_isr_handler_1,
+		NULL,
+		0);
+
+	/* Enable and pend the interrupt */
+	irq_enable(DIRECT_ISR_OFFSET);
+	NVIC_SetPendingIRQ(DIRECT_ISR_OFFSET);
+
+	/*
+	 * Instruction barriers to make sure the NVIC IRQ is
+	 * set to pending state before 'test_flag' is checked.
+	 */
+	__DSB();
+	__ISB();
+
+	/* Confirm test flag is set by the dynamic direct ISR handler. */
+	post_flag = test_flag;
+	zassert_true(post_flag == 2, "Test flag not set by ISR1\n");
+}
+/**
+ * @}
+ */