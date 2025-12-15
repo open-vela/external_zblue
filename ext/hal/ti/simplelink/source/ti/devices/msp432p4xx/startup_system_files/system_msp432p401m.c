zephyrproject-rtos/zephyr

soc: msp432p4xx: Get CPU clock frequency from DTS
The SoC initialization code used system clock frequency
as a CPU clock frequency. This commit corrects that by
obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

ext/hal/ti/simplelink/source/ti/devices/msp432p4xx/startup_system_files/system_msp432p401m.c
@@ -44,6 +44,7 @@
 
 #include <stdint.h>
 #include <ti/devices/msp432p4xx/inc/msp.h>
+#include <generated_dts_board.h>
 
 /*--------------------- Configuration Instructions ----------------------------
    1. If you prefer to halt the Watchdog Timer, set __HALT_WDT to 1:
@@ -68,7 +69,7 @@
 //     <12000000> 12 MHz
 //     <24000000> 24 MHz
 //     <48000000> 48 MHz
-#define  __SYSTEM_CLOCK    CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define  __SYSTEM_CLOCK    DT_CPUS_CPU_0_CLOCK_FREQUENCY
 
 /*--------------------- Power Regulator Configuration -----------------------*/
 //  Power Regulator Mode

open-vela/external_zblue

ext/hal/ti/simplelink/source/ti/devices/msp432p4xx/startup_system_files/system_msp432p401m.c
@@ -44,6 +44,7 @@
 
 #include <stdint.h>
 #include <ti/devices/msp432p4xx/inc/msp.h>
+#include <generated_dts_board.h>
 
 /*--------------------- Configuration Instructions ----------------------------
    1. If you prefer to halt the Watchdog Timer, set __HALT_WDT to 1:
@@ -68,7 +69,7 @@
 //     <12000000> 12 MHz
 //     <24000000> 24 MHz
 //     <48000000> 48 MHz
-#define  __SYSTEM_CLOCK    CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define  __SYSTEM_CLOCK    DT_ARM_CORTEX_M4F_0_CLOCK_FREQUENCY
 
 /*--------------------- Power Regulator Configuration -----------------------*/
 //  Power Regulator Mode