zephyrproject-rtos/zephyr

drivers: uart_msp432p4xx: Get clock frequency from DTS
The uart_msp432p4xx driver used system clock frequency
as a base for baudrate calculation. This commit corrects
that by obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

soc/arm/ti_simplelink/msp432p4xx/dts_fixup.h
@@ -12,6 +12,7 @@
 
 #define DT_UART_MSP432P4XX_NAME            DT_TI_MSP432P4XX_UART_40001000_LABEL
 #define DT_UART_MSP432P4XX_BASE_ADDRESS    DT_TI_MSP432P4XX_UART_40001000_BASE_ADDRESS
+#define DT_UART_MSP432P4XX_CLOCK_FREQUENCY DT_TI_MSP432P4XX_UART_40001000_CLOCK_FREQUENCY
 #define DT_UART_MSP432P4XX_BAUD_RATE       DT_TI_MSP432P4XX_UART_40001000_CURRENT_SPEED
 
 /* End of SoC Level DTS fixup file */

open-vela/external_zblue

soc/arm/ti_simplelink/msp432p4xx/dts_fixup.h
@@ -12,6 +12,7 @@
 
 #define DT_UART_MSP432P4XX_NAME            DT_TI_MSP432P4XX_UART_40001000_LABEL
 #define DT_UART_MSP432P4XX_BASE_ADDRESS    DT_TI_MSP432P4XX_UART_40001000_BASE_ADDRESS
+#define DT_UART_MSP432P4XX_CLOCK_FREQUENCY DT_TI_MSP432P4XX_UART_40001000_CLOCKS_CLOCK_FREQUENCY
 #define DT_UART_MSP432P4XX_BAUD_RATE       DT_TI_MSP432P4XX_UART_40001000_CURRENT_SPEED
 
 /* End of SoC Level DTS fixup file */