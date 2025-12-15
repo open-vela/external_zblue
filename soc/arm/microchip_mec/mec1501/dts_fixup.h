zephyrproject-rtos/zephyr

driver/counter: Add support for Microchip's XEC basic timer devices
Such basic timer is found on MEC150x for instance.
Since instances have dedicated data, let's define specifice instance
based on unique DT base address definition.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

soc/arm/microchip_mec/mec1501/dts_fixup.h
@@ -94,3 +94,42 @@
 #define DT_GPIO_XEC_GPIO240_276_IRQ_PRIORITY	DT_MICROCHIP_XEC_GPIO_40081280_IRQ_0_PRIORITY
 #define DT_GPIO_XEC_GPIO240_276_LABEL		DT_MICROCHIP_XEC_GPIO_40081280_LABEL
 #define DT_GPIO_XEC_GPIO240_276_SIZE		DT_MICROCHIP_XEC_GPIO_40081280_SIZE
+
+#ifdef DT_MICROCHIP_XEC_TIMER_40000C00_BASE_ADDRESS
+
+#define DT_COUNTER_MCHP_XEC_0			1
+#define DT_COUNTER_MCHP_XEC_0_BASE_ADDR		DT_MICROCHIP_XEC_TIMER_40000C00_BASE_ADDRESS
+#define DT_COUNTER_MCHP_XEC_0_IRQ		DT_MICROCHIP_XEC_TIMER_40000C00_IRQ_0
+#define DT_COUNTER_MCHP_XEC_0_IRQ_PRIORITY	DT_MICROCHIP_XEC_TIMER_40000C00_IRQ_0_PRIORITY
+#define DT_COUNTEX_MCHP_XEC_0_MAX_VALUE		DT_MICROCHIP_XEC_TIMER_40000C00_MAX_VALUE_0
+#define DT_COUNTER_MCHP_XEC_0_CLOCK_FREQUENCY	DT_MICROCHIP_XEC_TIMER_40000C00_CLOCK_FREQUENCY
+#define DT_COUNTER_MCHP_XEC_0_PRESCALER		DT_MICROCHIP_XEC_TIMER_40000C00_PRESCALER
+#define DT_COUNTER_MCHP_XEC_0_LABEL		DT_MICROCHIP_XEC_TIMER_40000C00_LABEL
+
+#endif /* DT_MICROCHIP_XEC_TIMER_40000C00_BASE_ADDRESS */
+
+#ifdef DT_MICROCHIP_XEC_TIMER_40000C20_BASE_ADDRESS
+
+#define DT_COUNTER_MCHP_XEC_1			1
+#define DT_COUNTER_MCHP_XEC_1_BASE_ADDR		DT_MICROCHIP_XEC_TIMER_40000C20_BASE_ADDRESS
+#define DT_COUNTER_MCHP_XEC_1_IRQ		DT_MICROCHIP_XEC_TIMER_40000C20_IRQ_0
+#define DT_COUNTER_MCHP_XEC_1_IRQ_PRIORITY	DT_MICROCHIP_XEC_TIMER_40000C20_IRQ_0_PRIORITY
+#define DT_COUNTEX_MCHP_XEC_1_MAX_VALUE		DT_MICROCHIP_XEC_TIMER_40000C20_MAX_VALUE_0
+#define DT_COUNTER_MCHP_XEC_1_CLOCK_FREQUENCY	DT_MICROCHIP_XEC_TIMER_40000C20_CLOCK_FREQUENCY
+#define DT_COUNTER_MCHP_XEC_1_PRESCALER		DT_MICROCHIP_XEC_TIMER_40000C20_PRESCALER
+#define DT_COUNTER_MCHP_XEC_1_LABEL		DT_MICROCHIP_XEC_TIMER_40000C20_LABEL
+
+#endif /* DT_MICROCHIP_XEC_TIMER_40000C20_BASE_ADDRESS */
+
+#ifdef DT_MICROCHIP_XEC_TIMER_40000CA0_BASE_ADDRESS
+
+#define DT_COUNTER_MCHP_XEC_2			1
+#define DT_COUNTER_MCHP_XEC_2_BASE_ADDR		DT_MICROCHIP_XEC_TIMER_40000CA0_BASE_ADDRESS
+#define DT_COUNTER_MCHP_XEC_2_IRQ		DT_MICROCHIP_XEC_TIMER_40000CA0_IRQ_0
+#define DT_COUNTER_MCHP_XEC_2_IRQ_PRIORITY	DT_MICROCHIP_XEC_TIMER_40000CA0_IRQ_0_PRIORITY
+#define DT_COUNTEX_MCHP_XEC_2_MAX_VALUE		DT_MICROCHIP_XEC_TIMER_40000CA0_MAX_VALUE_0
+#define DT_COUNTER_MCHP_XEC_2_CLOCK_FREQUENCY	DT_MICROCHIP_XEC_TIMER_40000CA0_CLOCK_FREQUENCY
+#define DT_COUNTER_MCHP_XEC_2_PRESCALER		DT_MICROCHIP_XEC_TIMER_40000CA0_PRESCALER
+#define DT_COUNTER_MCHP_XEC_2_LABEL		DT_MICROCHIP_XEC_TIMER_40000CA0_LABEL
+
+#endif /* DT_MICROCHIP_XEC_TIMER_40000CA0_BASE_ADDRESS */

open-vela/external_zblue

Signed-off-by: Daniel Leung <daniel.leung@intel.com>

soc/arm/microchip_mec/mec1501/dts_fixup.h
@@ -95,4 +95,43 @@
 #define DT_GPIO_XEC_GPIO240_276_LABEL		DT_MICROCHIP_XEC_GPIO_40081280_LABEL
 #define DT_GPIO_XEC_GPIO240_276_SIZE		DT_MICROCHIP_XEC_GPIO_40081280_SIZE
 
+#ifdef DT_MICROCHIP_XEC_TIMER_40000C00_BASE_ADDRESS
+
+#define DT_COUNTER_MCHP_XEC_0			1
+#define DT_COUNTER_MCHP_XEC_0_BASE_ADDR		DT_MICROCHIP_XEC_TIMER_40000C00_BASE_ADDRESS
+#define DT_COUNTER_MCHP_XEC_0_IRQ		DT_MICROCHIP_XEC_TIMER_40000C00_IRQ_0
+#define DT_COUNTER_MCHP_XEC_0_IRQ_PRIORITY	DT_MICROCHIP_XEC_TIMER_40000C00_IRQ_0_PRIORITY
+#define DT_COUNTEX_MCHP_XEC_0_MAX_VALUE		DT_MICROCHIP_XEC_TIMER_40000C00_MAX_VALUE_0
+#define DT_COUNTER_MCHP_XEC_0_CLOCK_FREQUENCY	DT_MICROCHIP_XEC_TIMER_40000C00_CLOCK_FREQUENCY
+#define DT_COUNTER_MCHP_XEC_0_PRESCALER		DT_MICROCHIP_XEC_TIMER_40000C00_PRESCALER
+#define DT_COUNTER_MCHP_XEC_0_LABEL		DT_MICROCHIP_XEC_TIMER_40000C00_LABEL
+
+#endif /* DT_MICROCHIP_XEC_TIMER_40000C00_BASE_ADDRESS */
+
+#ifdef DT_MICROCHIP_XEC_TIMER_40000C20_BASE_ADDRESS
+
+#define DT_COUNTER_MCHP_XEC_1			1
+#define DT_COUNTER_MCHP_XEC_1_BASE_ADDR		DT_MICROCHIP_XEC_TIMER_40000C20_BASE_ADDRESS
+#define DT_COUNTER_MCHP_XEC_1_IRQ		DT_MICROCHIP_XEC_TIMER_40000C20_IRQ_0
+#define DT_COUNTER_MCHP_XEC_1_IRQ_PRIORITY	DT_MICROCHIP_XEC_TIMER_40000C20_IRQ_0_PRIORITY
+#define DT_COUNTEX_MCHP_XEC_1_MAX_VALUE		DT_MICROCHIP_XEC_TIMER_40000C20_MAX_VALUE_0
+#define DT_COUNTER_MCHP_XEC_1_CLOCK_FREQUENCY	DT_MICROCHIP_XEC_TIMER_40000C20_CLOCK_FREQUENCY
+#define DT_COUNTER_MCHP_XEC_1_PRESCALER		DT_MICROCHIP_XEC_TIMER_40000C20_PRESCALER
+#define DT_COUNTER_MCHP_XEC_1_LABEL		DT_MICROCHIP_XEC_TIMER_40000C20_LABEL
+
+#endif /* DT_MICROCHIP_XEC_TIMER_40000C20_BASE_ADDRESS */
+
+#ifdef DT_MICROCHIP_XEC_TIMER_40000CA0_BASE_ADDRESS
+
+#define DT_COUNTER_MCHP_XEC_3			1
+#define DT_COUNTER_MCHP_XEC_3_BASE_ADDR		DT_MICROCHIP_XEC_TIMER_40000CA0_BASE_ADDRESS
+#define DT_COUNTER_MCHP_XEC_3_IRQ		DT_MICROCHIP_XEC_TIMER_40000CA0_IRQ_0
+#define DT_COUNTER_MCHP_XEC_3_IRQ_PRIORITY	DT_MICROCHIP_XEC_TIMER_40000CA0_IRQ_0_PRIORITY
+#define DT_COUNTEX_MCHP_XEC_3_MAX_VALUE		DT_MICROCHIP_XEC_TIMER_40000CA0_MAX_VALUE_0
+#define DT_COUNTER_MCHP_XEC_3_CLOCK_FREQUENCY	DT_MICROCHIP_XEC_TIMER_40000CA0_CLOCK_FREQUENCY
+#define DT_COUNTER_MCHP_XEC_3_PRESCALER		DT_MICROCHIP_XEC_TIMER_40000CA0_PRESCALER
+#define DT_COUNTER_MCHP_XEC_3_LABEL		DT_MICROCHIP_XEC_TIMER_40000CA0_LABEL
+
+#endif /* DT_MICROCHIP_XEC_TIMER_40000CA0_BASE_ADDRESS */
+
 #define DT_WDT_0_NAME				DT_INST_0_MICROCHIP_XEC_WATCHDOG_LABEL