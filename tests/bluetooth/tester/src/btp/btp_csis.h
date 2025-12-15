zephyrproject-rtos/zephyr

bluetooth: tester: Add CSIS btp command
This commit adds CSIS command to change type of SIRK.
This is needed for CSIS/SR/SP/BV-05-C test case.

Signed-off-by: Piotr Narajowski <piotr.narajowski@codecoup.pl>

tests/bluetooth/tester/src/btp/btp_csis.h
@@ -28,3 +28,8 @@ struct btp_csis_get_member_rsi_cmd {
 struct btp_csis_get_member_rsi_rp {
 	uint8_t rsi[BT_CSIP_RSI_SIZE];
 } __packed;
+
+#define BTP_CSIS_ENC_SIRK_TYPE			0x04
+struct btp_csis_set_sirk_type_cmd {
+	uint8_t type;
+} __packed;

open-vela/external_zblue

tests/bluetooth/tester/src/btp/btp_csis.h
@@ -28,3 +28,8 @@ struct btp_csis_get_member_rsi_cmd {
 struct btp_csis_get_member_rsi_rp {
 	uint8_t rsi[BT_CSIP_RSI_SIZE];
 } __packed;
+
+#define BTP_CSIS_ENC_SIRK_TYPE			0x04
+struct btp_csis_set_sirk_type_cmd {
+	uint8_t encrypted;
+} __packed;