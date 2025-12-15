hillLang/zephyr

kernel/sched: Fix k_yield() in SMP
This was always doing a remove/add of the _current thread to the run
queue, which is wrong because in SMP _current isn't in the queue to
remove.  But it went undetected until the recent dlist changes.

Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

kernel/sched.c
@@ -867,12 +867,16 @@ void _impl_k_yield(void)
 
 	if (!_is_idle(_current)) {
 		LOCKED(&sched_lock) {
-			_priq_run_remove(&_kernel.ready_q.runq, _current);
-			_priq_run_add(&_kernel.ready_q.runq, _current);
+			if (!IS_ENABLED(CONFIG_SMP) ||
+			    _is_thread_queued(_current)) {
+				_priq_run_remove(&_kernel.ready_q.runq,
+						 _current);
+				_priq_run_add(&_kernel.ready_q.runq,
+					      _current);
+			}
 			update_cache(1);
 		}
 	}
-
 	_Swap_unlocked();
 }
 

open-vela/external_zblue

kernel/sched.c
@@ -886,12 +886,16 @@ void z_impl_k_yield(void)
 
 	if (!is_idle(_current)) {
 		LOCKED(&sched_spinlock) {
-			_priq_run_remove(&_kernel.ready_q.runq, _current);
-			_priq_run_add(&_kernel.ready_q.runq, _current);
+			if (!IS_ENABLED(CONFIG_SMP) ||
+			    z_is_thread_queued(_current)) {
+				_priq_run_remove(&_kernel.ready_q.runq,
+						 _current);
+				_priq_run_add(&_kernel.ready_q.runq,
+					      _current);
+			}
 			update_cache(1);
 		}
 	}
-
 	z_swap_unlocked();
 }
 