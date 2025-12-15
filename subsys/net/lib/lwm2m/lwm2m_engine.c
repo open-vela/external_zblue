rlubos/zephyr

net: lwm2m: Add support for object versioning
Each object now have to specify the object version it implements.
Based on this information the LwM2M engine can decide whether it's
needed to report the object version during Registration/Discovery
operations or not.

Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

subsys/net/lib/lwm2m/lwm2m_engine.c
@@ -1094,8 +1094,11 @@ int lwm2m_register_payload_handler(struct lwm2m_message *msg)
 			continue;
 		}
 
-		/* Only report <OBJ_ID> when no instance available */
-		if (obj->instance_count == 0U) {
+		/* Only report <OBJ_ID> when no instance available or it's
+		 * needed to report object version.
+		 */
+		if (obj->instance_count == 0U ||
+		    lwm2m_engine_report_obj_version(obj)) {
 			struct lwm2m_obj_path path = {
 				.obj_id = obj->obj_id,
 				.level = LWM2M_PATH_LEVEL_OBJECT,
@@ -1106,7 +1109,9 @@ int lwm2m_register_payload_handler(struct lwm2m_message *msg)
 				return ret;
 			}
 
-			continue;
+			if (obj->instance_count == 0U) {
+				continue;
+			}
 		}
 
 		SYS_SLIST_FOR_EACH_CONTAINER(&engine_obj_inst_list,
@@ -3094,11 +3099,13 @@ int lwm2m_discover_handler(struct lwm2m_message *msg, bool is_bootstrap)
 		}
 
 		/* For bootstrap discover, only report object ID when no
-		 * instance is available.
+		 * instance is available or it's needed to report object
+		 * version.
 		 * For device management discovery, only report object ID with
 		 * attributes if object ID (alone) was provided.
 		 */
-		if ((is_bootstrap && obj->instance_count == 0U) ||
+		if ((is_bootstrap && (obj->instance_count == 0U ||
+				      lwm2m_engine_report_obj_version(obj))) ||
 		    (!is_bootstrap && msg->path.level == LWM2M_PATH_LEVEL_OBJECT)) {
 			struct lwm2m_obj_path path = {
 				.obj_id = obj->obj_id,
@@ -3112,7 +3119,7 @@ int lwm2m_discover_handler(struct lwm2m_message *msg, bool is_bootstrap)
 
 			reported = true;
 
-			if (is_bootstrap) {
+			if (obj->instance_count == 0U) {
 				continue;
 			}
 		}
@@ -3263,6 +3270,16 @@ struct lwm2m_engine_res *lwm2m_engine_get_res(
 	}
 
 	return res;
+}
+
+bool lwm2m_engine_report_obj_version(const struct lwm2m_engine_obj *obj)
+{
+	if (obj->is_core) {
+		return obj->version_major != LWM2M_PROTOCOL_VERSION_MAJOR ||
+		       obj->version_minor != LWM2M_PROTOCOL_VERSION_MINOR;
+	}
+
+	return obj->version_major != 1 || obj->version_minor != 0;
 }
 
 static int do_write_op(struct lwm2m_message *msg,

subsys/net/lib/lwm2m/lwm2m_engine.h
@@ -10,7 +10,12 @@
 
 #include "lwm2m_object.h"
 
-#define LWM2M_PROTOCOL_VERSION "1.0"
+#define LWM2M_PROTOCOL_VERSION_MAJOR 1
+#define LWM2M_PROTOCOL_VERSION_MINOR 0
+
+#define LWM2M_PROTOCOL_VERSION_STRING STRINGIFY(LWM2M_PROTOCOL_VERSION_MAJOR) \
+				      "." \
+				      STRINGIFY(LWM2M_PROTOCOL_VERSION_MINOR)
 
 /* LWM2M / CoAP Content-Formats */
 #define LWM2M_FORMAT_PLAIN_TEXT		0
@@ -71,6 +76,8 @@ struct lwm2m_engine_obj_inst *lwm2m_engine_get_obj_inst(
 					const struct lwm2m_obj_path *path);
 struct lwm2m_engine_res *lwm2m_engine_get_res(
 					const struct lwm2m_obj_path *path);
+
+bool lwm2m_engine_report_obj_version(const struct lwm2m_engine_obj *obj);
 
 /* LwM2M context functions */
 int lwm2m_engine_context_close(struct lwm2m_ctx *client_ctx);

subsys/net/lib/lwm2m/lwm2m_rw_link_format.c
@@ -14,7 +14,7 @@ LOG_MODULE_REGISTER(net_lwm2m_link_format, CONFIG_LWM2M_LOG_LEVEL);
 
 #define CORELINK_BUF_SIZE 24
 
-#define ENABLER_VERSION "lwm2m=\"" LWM2M_PROTOCOL_VERSION "\""
+#define ENABLER_VERSION "lwm2m=\"" LWM2M_PROTOCOL_VERSION_STRING "\""
 
 /*
  * TODO: to implement a way for clients to specify alternate path
@@ -103,6 +103,26 @@ static int put_corelink_separator(struct lwm2m_output_context *out)
 	}
 
 	return (int)sizeof(comma);
+}
+
+static int put_corelink_version(struct lwm2m_output_context *out,
+				const struct lwm2m_engine_obj *obj,
+				uint8_t *buf, uint16_t buflen)
+{
+	int ret, len;
+
+	len = snprintk(buf, buflen, ";ver=%u.%u", obj->version_major,
+		       obj->version_minor);
+	if (len < 0 || len >= buflen) {
+		return -ENOMEM;
+	}
+
+	ret = buf_append(CPKT_BUF_WRITE(out->out_cpkt), buf, len);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return len;
 }
 
 static int put_corelink_dimension(struct lwm2m_output_context *out,
@@ -264,6 +284,7 @@ static int put_obj_corelink(struct lwm2m_output_context *out,
 			    struct link_format_out_formatter_data *fd)
 {
 	char obj_buf[CORELINK_BUF_SIZE];
+	struct lwm2m_engine_obj *obj;
 	int len = 0;
 	int ret;
 
@@ -279,16 +300,24 @@ static int put_obj_corelink(struct lwm2m_output_context *out,
 		return ret;
 	}
 
+	obj = lwm2m_engine_get_obj(path);
+	if (obj == NULL) {
+		return -EINVAL;
+	}
+
+	if (lwm2m_engine_report_obj_version(obj)) {
+		ret = put_corelink_version(out, obj, obj_buf, sizeof(obj_buf));
+		if (ret < 0) {
+			return ret;
+		}
+
+		len += ret;
+	}
+
 	if (fd->mode == LINK_FORMAT_MODE_DISCOVERY) {
 		/* Report object attributes only in device management mode
 		 * (5.4.2).
 		 */
-		struct lwm2m_engine_obj *obj = lwm2m_engine_get_obj(path);
-
-		if (obj == NULL) {
-			return -EINVAL;
-		}
-
 		ret = put_corelink_attributes(out, obj, obj_buf,
 					      sizeof(obj_buf));
 		if (ret < 0) {

open-vela/external_zblue

subsys/net/lib/lwm2m/lwm2m_engine.c
@@ -1094,8 +1094,11 @@ int lwm2m_register_payload_handler(struct lwm2m_message *msg)
 			continue;
 		}
 
-		/* Only report <OBJ_ID> when no instance available */
-		if (obj->instance_count == 0U) {
+		/* Only report <OBJ_ID> when no instance available or it's
+		 * needed to report object version.
+		 */
+		if (obj->instance_count == 0U ||
+		    lwm2m_engine_shall_report_obj_version(obj)) {
 			struct lwm2m_obj_path path = {
 				.obj_id = obj->obj_id,
 				.level = LWM2M_PATH_LEVEL_OBJECT,
@@ -1106,7 +1109,9 @@ int lwm2m_register_payload_handler(struct lwm2m_message *msg)
 				return ret;
 			}
 
-			continue;
+			if (obj->instance_count == 0U) {
+				continue;
+			}
 		}
 
 		SYS_SLIST_FOR_EACH_CONTAINER(&engine_obj_inst_list,
@@ -3263,11 +3268,13 @@ int lwm2m_discover_handler(struct lwm2m_message *msg, bool is_bootstrap)
 		}
 
 		/* For bootstrap discover, only report object ID when no
-		 * instance is available.
+		 * instance is available or it's needed to report object
+		 * version.
 		 * For device management discovery, only report object ID with
 		 * attributes if object ID (alone) was provided.
 		 */
-		if ((is_bootstrap && obj->instance_count == 0U) ||
+		if ((is_bootstrap && (obj->instance_count == 0U ||
+				      lwm2m_engine_shall_report_obj_version(obj))) ||
 		    (!is_bootstrap && msg->path.level == LWM2M_PATH_LEVEL_OBJECT)) {
 			struct lwm2m_obj_path path = {
 				.obj_id = obj->obj_id,
@@ -3281,7 +3288,7 @@ int lwm2m_discover_handler(struct lwm2m_message *msg, bool is_bootstrap)
 
 			reported = true;
 
-			if (is_bootstrap) {
+			if (obj->instance_count == 0U) {
 				continue;
 			}
 		}
@@ -3432,6 +3439,16 @@ struct lwm2m_engine_res *lwm2m_engine_get_res(
 	}
 
 	return res;
+}
+
+bool lwm2m_engine_shall_report_obj_version(const struct lwm2m_engine_obj *obj)
+{
+	if (obj->is_core) {
+		return obj->version_major != LWM2M_PROTOCOL_VERSION_MAJOR ||
+		       obj->version_minor != LWM2M_PROTOCOL_VERSION_MINOR;
+	}
+
+	return obj->version_major != 1 || obj->version_minor != 0;
 }
 
 static int do_write_op(struct lwm2m_message *msg,

subsys/net/lib/lwm2m/lwm2m_engine.h
@@ -10,7 +10,12 @@
 
 #include "lwm2m_object.h"
 
-#define LWM2M_PROTOCOL_VERSION "1.0"
+#define LWM2M_PROTOCOL_VERSION_MAJOR 1
+#define LWM2M_PROTOCOL_VERSION_MINOR 0
+
+#define LWM2M_PROTOCOL_VERSION_STRING STRINGIFY(LWM2M_PROTOCOL_VERSION_MAJOR) \
+				      "." \
+				      STRINGIFY(LWM2M_PROTOCOL_VERSION_MINOR)
 
 /* LWM2M / CoAP Content-Formats */
 #define LWM2M_FORMAT_PLAIN_TEXT		0
@@ -71,6 +76,8 @@ struct lwm2m_engine_obj_inst *lwm2m_engine_get_obj_inst(
 					const struct lwm2m_obj_path *path);
 struct lwm2m_engine_res *lwm2m_engine_get_res(
 					const struct lwm2m_obj_path *path);
+
+bool lwm2m_engine_shall_report_obj_version(const struct lwm2m_engine_obj *obj);
 
 /* LwM2M context functions */
 int lwm2m_engine_context_close(struct lwm2m_ctx *client_ctx);

subsys/net/lib/lwm2m/lwm2m_rw_link_format.c
@@ -14,7 +14,7 @@ LOG_MODULE_REGISTER(net_lwm2m_link_format, CONFIG_LWM2M_LOG_LEVEL);
 
 #define CORELINK_BUF_SIZE 24
 
-#define ENABLER_VERSION "lwm2m=\"" LWM2M_PROTOCOL_VERSION "\""
+#define ENABLER_VERSION "lwm2m=\"" LWM2M_PROTOCOL_VERSION_STRING "\""
 
 /*
  * TODO: to implement a way for clients to specify alternate path
@@ -103,6 +103,26 @@ static int put_corelink_separator(struct lwm2m_output_context *out)
 	}
 
 	return (int)sizeof(comma);
+}
+
+static int put_corelink_version(struct lwm2m_output_context *out,
+				const struct lwm2m_engine_obj *obj,
+				uint8_t *buf, uint16_t buflen)
+{
+	int ret, len;
+
+	len = snprintk(buf, buflen, ";ver=%u.%u", obj->version_major,
+		       obj->version_minor);
+	if (len < 0 || len >= buflen) {
+		return -ENOMEM;
+	}
+
+	ret = buf_append(CPKT_BUF_WRITE(out->out_cpkt), buf, len);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return len;
 }
 
 static int put_corelink_dimension(struct lwm2m_output_context *out,
@@ -264,6 +284,7 @@ static int put_obj_corelink(struct lwm2m_output_context *out,
 			    struct link_format_out_formatter_data *fd)
 {
 	char obj_buf[CORELINK_BUF_SIZE];
+	struct lwm2m_engine_obj *obj;
 	int len = 0;
 	int ret;
 
@@ -279,16 +300,24 @@ static int put_obj_corelink(struct lwm2m_output_context *out,
 		return ret;
 	}
 
+	obj = lwm2m_engine_get_obj(path);
+	if (obj == NULL) {
+		return -EINVAL;
+	}
+
+	if (lwm2m_engine_shall_report_obj_version(obj)) {
+		ret = put_corelink_version(out, obj, obj_buf, sizeof(obj_buf));
+		if (ret < 0) {
+			return ret;
+		}
+
+		len += ret;
+	}
+
 	if (fd->mode == LINK_FORMAT_MODE_DISCOVERY) {
 		/* Report object attributes only in device management mode
 		 * (5.4.2).
 		 */
-		struct lwm2m_engine_obj *obj = lwm2m_engine_get_obj(path);
-
-		if (obj == NULL) {
-			return -EINVAL;
-		}
-
 		ret = put_corelink_attributes(out, obj, obj_buf,
 					      sizeof(obj_buf));
 		if (ret < 0) {