mbolivar/zephyr

cmake: add helpers for setting board runners
This helps by letting us add checks for when the runner has already
been set. There is documentation saying you can set
-DBOARD_DEBUG_RUNNER at the command line and have it take effect,
which turns out not to be true for a large number of boards.
A status message helps the user debug.

(We'll address the existing in-tree boards in the next patch.)

Signed-off-by: Marti Bolivar <marti.bolivar@nordicsemi.no>

boards/arm/frdm_kl25z/board.cmake
@@ -3,11 +3,11 @@
 set_ifndef(OPENSDA_FW daplink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifndef(jlink)
+  board_set_flasher_ifndef(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifndef(pyocd)
+  board_set_flasher_ifndef(pyocd)
 endif()
 
 board_runner_args(jlink "--device=MKL25Z128xxx4")

boards/arm/frdm_kw41z/board.cmake
@@ -3,11 +3,11 @@
 set_ifndef(OPENSDA_FW daplink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifndef(jlink)
+  board_set_flasher_ifndef(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifndef(pyocd)
+  board_set_flasher_ifndef(pyocd)
 endif()
 
 board_runner_args(jlink "--device=MKW41Z512xxx4")

boards/arm/hexiwear_kw40z/board.cmake
@@ -3,11 +3,11 @@
 set_ifndef(OPENSDA_FW jlink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifndef(jlink)
+  board_set_flasher_ifndef(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifndef(pyocd)
+  board_set_flasher_ifndef(pyocd)
 endif()
 
 board_runner_args(jlink "--device=MKW40Z160xxx4")

boards/arm/mimxrt1020_evk/board.cmake
@@ -7,11 +7,11 @@
 set_ifndef(OPENSDA_FW jlink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifndef(jlink)
+  board_set_flasher_ifndef(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifndef(pyocd)
+  board_set_flasher_ifndef(pyocd)
 endif()
 
 board_runner_args(pyocd "--target=mimxrt1020")

boards/common/bossac.board.cmake
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER bossac)
+board_set_flasher_ifndef(bossac)
 board_finalize_runner_args(bossac "--bossac=${BOSSAC}")

boards/common/dfu-util.board.cmake
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER dfu-util)
+board_set_flasher_ifndef(dfu-util)
 board_finalize_runner_args(dfu-util) # No default arguments to provide.

boards/common/jlink.board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER jlink)
-set_ifndef(BOARD_DEBUG_RUNNER jlink)
+board_set_flasher_ifndef(jlink)
+board_set_debugger_ifndef(jlink)
 board_finalize_runner_args(jlink "--dt-flash=y")

boards/common/openocd.board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER openocd)
-set_ifndef(BOARD_DEBUG_RUNNER openocd)
+board_set_flasher_ifndef(openocd)
+board_set_debugger_ifndef(openocd)
 
 # "load_image" or "flash write_image erase"?
 if(CONFIG_X86 OR CONFIG_ARC)

open-vela/external_zblue

boards/arm/frdm_kl25z/board.cmake
@@ -3,11 +3,11 @@
 set_ifndef(OPENSDA_FW daplink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifnset(jlink)
+  board_set_flasher_ifnset(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifnset(pyocd)
+  board_set_flasher_ifnset(pyocd)
 endif()
 
 board_runner_args(jlink "--device=MKL25Z128xxx4")

boards/arm/frdm_kw41z/board.cmake
@@ -3,11 +3,11 @@
 set_ifndef(OPENSDA_FW daplink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifnset(jlink)
+  board_set_flasher_ifnset(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifnset(pyocd)
+  board_set_flasher_ifnset(pyocd)
 endif()
 
 board_runner_args(jlink "--device=MKW41Z512xxx4")

boards/arm/hexiwear_kw40z/board.cmake
@@ -3,11 +3,11 @@
 set_ifndef(OPENSDA_FW jlink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifnset(jlink)
+  board_set_flasher_ifnset(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifnset(pyocd)
+  board_set_flasher_ifnset(pyocd)
 endif()
 
 board_runner_args(jlink "--device=MKW40Z160xxx4")

boards/arm/mimxrt1020_evk/board.cmake
@@ -7,11 +7,11 @@
 set_ifndef(OPENSDA_FW jlink)
 
 if(OPENSDA_FW STREQUAL jlink)
-  set_ifndef(BOARD_DEBUG_RUNNER jlink)
-  set_ifndef(BOARD_FLASH_RUNNER jlink)
+  board_set_debugger_ifnset(jlink)
+  board_set_flasher_ifnset(jlink)
 elseif(OPENSDA_FW STREQUAL daplink)
-  set_ifndef(BOARD_DEBUG_RUNNER pyocd)
-  set_ifndef(BOARD_FLASH_RUNNER pyocd)
+  board_set_debugger_ifnset(pyocd)
+  board_set_flasher_ifnset(pyocd)
 endif()
 
 board_runner_args(pyocd "--target=mimxrt1020")

boards/common/bossac.board.cmake
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER bossac)
+board_set_flasher_ifnset(bossac)
 board_finalize_runner_args(bossac "--bossac=${BOSSAC}")

boards/common/dfu-util.board.cmake
@@ -1,4 +1,4 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER dfu-util)
+board_set_flasher_ifnset(dfu-util)
 board_finalize_runner_args(dfu-util) # No default arguments to provide.

boards/common/jlink.board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER jlink)
-set_ifndef(BOARD_DEBUG_RUNNER jlink)
+board_set_flasher_ifnset(jlink)
+board_set_debugger_ifnset(jlink)
 board_finalize_runner_args(jlink "--dt-flash=y")

boards/common/openocd.board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-set_ifndef(BOARD_FLASH_RUNNER openocd)
-set_ifndef(BOARD_DEBUG_RUNNER openocd)
+board_set_flasher_ifnset(openocd)
+board_set_debugger_ifnset(openocd)
 
 # "load_image" or "flash write_image erase"?
 if(CONFIG_X86 OR CONFIG_ARC)