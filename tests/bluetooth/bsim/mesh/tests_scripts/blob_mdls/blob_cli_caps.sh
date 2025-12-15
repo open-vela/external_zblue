Balaklaka/zephyr

Tests: Bluetooth: Mesh: BLOB cli ret caps suspend
Add bsim test for suspending retrieve capabilities procedure of the BLOB
client model.

Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_caps.sh
@@ -15,3 +15,7 @@ RunTest mesh_blob \
 # The test instance sequence must stay as it is due to addressing scheme
 RunTest mesh_blob \
 	blob_cli_caps_no_rsp blob_srv_caps_no_rsp blob_srv_caps_no_rsp
+
+# The test instance seqence must stay as it is due to addressing scheme
+RunTest mesh_blob \
+	blob_cli_caps_cancelled blob_srv_caps_standard

open-vela/external_zblue

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_caps.sh
@@ -18,3 +18,8 @@ RunTest blob_caps \
 conf=prj_mesh1d1_conf
 RunTest blob_caps \
 	blob_cli_caps_no_rsp blob_srv_caps_no_rsp blob_srv_caps_no_rsp
+
+# The test instance seqence must stay as it is due to addressing scheme
+conf=prj_mesh1d1_conf
+RunTest blob_caps \
+	blob_cli_caps_cancelled blob_srv_caps_standard