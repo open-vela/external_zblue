zephyrproject-rtos/zephyr

drivers: uart_cc32xx: Get clock frequency from DTS
The uart_cc32xx driver used system clock frequency
as a base for baudrate calculation. This commit corrects
that by obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

drivers/serial/uart_cc32xx.c
@@ -38,7 +38,7 @@ static void uart_cc32xx_isr(void *arg);
 
 static const struct uart_device_config uart_cc32xx_dev_cfg_0 = {
 	.base = (void *)DT_TI_CC32XX_UART_4000C000_BASE_ADDRESS,
-	.sys_clk_freq = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC,
+	.sys_clk_freq = DT_TI_CC32XX_UART_4000C000_CLOCK_FREQUENCY,
 };
 
 static struct uart_cc32xx_dev_data_t uart_cc32xx_dev_data_0 = {

open-vela/external_zblue

drivers/serial/uart_cc32xx.c
@@ -38,7 +38,7 @@ static void uart_cc32xx_isr(void *arg);
 
 static const struct uart_device_config uart_cc32xx_dev_cfg_0 = {
 	.base = (void *)DT_TI_CC32XX_UART_4000C000_BASE_ADDRESS,
-	.sys_clk_freq = CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC,
+	.sys_clk_freq = DT_TI_CC32XX_UART_4000C000_CLOCKS_CLOCK_FREQUENCY,
 };
 
 static struct uart_cc32xx_dev_data_t uart_cc32xx_dev_data_0 = {