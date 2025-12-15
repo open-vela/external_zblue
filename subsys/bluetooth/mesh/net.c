Balaklaka/zephyr

Bluetooth: Mesh: Do not relay message rejected by Transport layer
Mesh Profile Specification Errata 11341

3.6.4.3 Message error procedure
When the Upper Transport layer receives a message that is not
understood, then the message shall be ignored.
A message that is not understood includes messages that met one or more
conditions listed below:
• The Transport Control message opcode is unknown by the receiving node.
• The Transport Control message size for the Transport Control opcode
is incorrect.
• The Transport Control message parameters contain values that are
Prohibited

Signed-off-by: Michał Narajowski <michal.narajowski@codecoup.pl>

subsys/bluetooth/mesh/net.c
@@ -830,6 +830,7 @@ void bt_mesh_net_recv(struct net_buf_simple *data, int8_t rssi,
 	NET_BUF_SIMPLE_DEFINE(buf, BT_MESH_NET_MAX_PDU_LEN);
 	struct bt_mesh_net_rx rx = { .ctx.recv_rssi = rssi };
 	struct net_buf_simple_state state;
+	int err;
 
 	BT_DBG("rssi %d net_if %u", rssi, net_if);
 
@@ -858,18 +859,23 @@ void bt_mesh_net_recv(struct net_buf_simple *data, int8_t rssi,
 		}
 	}
 
-	/* The transport layer has indicated that it has rejected the message,
-	 * but would like to see it again if it is received in the future.
-	 * This can happen if a message is received when the device is in
-	 * Low Power mode, but the message was not encrypted with the friend
-	 * credentials. Remove it from the message cache so that we accept
-	 * it again in the future.
-	 */
-	if (bt_mesh_trans_recv(&buf, &rx) == -EAGAIN) {
+	err = bt_mesh_trans_recv(&buf, &rx);
+	if (err == -EAGAIN) {
+		/* The transport layer has indicated that it has rejected the message,
+		* but would like to see it again if it is received in the future.
+		* This can happen if a message is received when the device is in
+		* Low Power mode, but the message was not encrypted with the friend
+		* credentials. Remove it from the message cache so that we accept
+		* it again in the future.
+		*/
 		BT_WARN("Removing rejected message from Network Message Cache");
 		/* Rewind the next index now that we're not using this entry */
 		msg_cache[--msg_cache_next].src = BT_MESH_ADDR_UNASSIGNED;
 		dup_cache[--dup_cache_next] = 0;
+		return;
+	} else if (err == -EBADMSG) {
+		BT_DBG("Not relaying message rejected by the Transport layer");
+		return;
 	}
 
 	/* Relay if this was a group/virtual address, or if the destination

open-vela/external_zblue

subsys/bluetooth/mesh/net.c
@@ -827,6 +827,7 @@ void bt_mesh_net_recv(struct net_buf_simple *data, int8_t rssi,
 	NET_BUF_SIMPLE_DEFINE(buf, BT_MESH_NET_MAX_PDU_LEN);
 	struct bt_mesh_net_rx rx = { .ctx.recv_rssi = rssi };
 	struct net_buf_simple_state state;
+	int err;
 
 	LOG_DBG("rssi %d net_if %u", rssi, net_if);
 
@@ -855,18 +856,23 @@ void bt_mesh_net_recv(struct net_buf_simple *data, int8_t rssi,
 		}
 	}
 
-	/* The transport layer has indicated that it has rejected the message,
-	 * but would like to see it again if it is received in the future.
-	 * This can happen if a message is received when the device is in
-	 * Low Power mode, but the message was not encrypted with the friend
-	 * credentials. Remove it from the message cache so that we accept
-	 * it again in the future.
-	 */
-	if (bt_mesh_trans_recv(&buf, &rx) == -EAGAIN) {
+	err = bt_mesh_trans_recv(&buf, &rx);
+	if (err == -EAGAIN) {
+		/* The transport layer has indicated that it has rejected the message,
+		 * but would like to see it again if it is received in the future.
+		 * This can happen if a message is received when the device is in
+		 * Low Power mode, but the message was not encrypted with the friend
+		 * credentials. Remove it from the message cache so that we accept
+		 * it again in the future.
+		 */
 		LOG_WRN("Removing rejected message from Network Message Cache");
 		/* Rewind the next index now that we're not using this entry */
 		msg_cache[--msg_cache_next].src = BT_MESH_ADDR_UNASSIGNED;
 		dup_cache[--dup_cache_next] = 0;
+		return;
+	} else if (err == -EBADMSG) {
+		LOG_DBG("Not relaying message rejected by the Transport layer");
+		return;
 	}
 
 	/* Relay if this was a group/virtual address, or if the destination