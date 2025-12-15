jori-nordic/zephyr

Bluetooth: host: poll on CTLR buffers instead of host TX queue
When there are no buffers, it doesn't make sense to repeatedly try to send
the host TX queue.

Signed-off-by: Jonathan Rico <jonathan.rico@nordicsemi.no>

subsys/bluetooth/host/conn.c
@@ -513,7 +513,7 @@ static int send_frag(struct bt_conn *conn, struct net_buf *buf, uint8_t flags,
 	LOG_HEXDUMP_DBG(buf->data, buf->len, "send_frag");
 
 	/* Check if the controller can accept ACL packets */
-	if(k_sem_take(bt_conn_get_pkts(conn), K_MSEC(100))) {
+	if(k_sem_take(bt_conn_get_pkts(conn), K_NO_WAIT)) {
 		/* not `goto fail`, we don't want to free the tx context: in the
 		 * case where it is the original buffer, it will contain the
 		 * callback ptr.
@@ -752,10 +752,31 @@ static int conn_prepare_events(struct bt_conn *conn,
 
 	BT_DBG("Adding conn %p to poll list", conn);
 
-	k_poll_event_init(&events[0],
-			K_POLL_TYPE_FIFO_DATA_AVAILABLE,
-			K_POLL_MODE_NOTIFY_ONLY,
-			&conn->tx_queue);
+	/* Here's an idea:
+	 * - put a K_NO_WAIT for the semaphore acquisition
+	 * - remove the queue from the k_poll
+	 * - add the NCP event to the k_poll
+	 * -> this way this process_tx won't get triggered all the gdamn time
+	 * */
+
+	bool buffer_available = k_sem_count_get(bt_conn_get_pkts(conn)) > 0;
+	if (buffer_available) {
+		BT_DBG("wait on host fifo");
+		/* Wait until there is more data to send. */
+		k_poll_event_init(&events[0],
+				K_POLL_TYPE_FIFO_DATA_AVAILABLE,
+				K_POLL_MODE_NOTIFY_ONLY,
+				&conn->tx_queue);
+	} else {
+		BT_DBG("wait on ctlr buffers");
+		/* Only resume sending when the controller has buffer space
+		 * available for this connection.
+		 */
+		k_poll_event_init(&events[0],
+				K_POLL_TYPE_SEM_AVAILABLE,
+				K_POLL_MODE_NOTIFY_ONLY,
+				bt_conn_get_pkts(conn));
+	}
 	events[0].tag = BT_EVENT_CONN_TX_QUEUE;
 
 	return 0;

open-vela/external_zblue

subsys/bluetooth/host/conn.c
@@ -513,7 +513,7 @@ static int send_frag(struct bt_conn *conn, struct net_buf *buf, uint8_t flags,
 	int err = 0;
 
 	/* Check if the controller can accept ACL packets */
-	if (k_sem_take(bt_conn_get_pkts(conn), K_MSEC(100))) {
+	if (k_sem_take(bt_conn_get_pkts(conn), K_NO_WAIT)) {
 		/* not `goto fail`, we don't want to free the tx context: in the
 		 * case where it is the original buffer, it will contain the
 		 * callback ptr.
@@ -776,10 +776,26 @@ static int conn_prepare_events(struct bt_conn *conn,
 
 	LOG_DBG("Adding conn %p to poll list", conn);
 
-	k_poll_event_init(&events[0],
-			K_POLL_TYPE_FIFO_DATA_AVAILABLE,
-			K_POLL_MODE_NOTIFY_ONLY,
-			&conn->tx_queue);
+	bool buffers_available = k_sem_count_get(bt_conn_get_pkts(conn)) > 0;
+	bool packets_waiting = !k_fifo_is_empty(&conn->tx_queue);
+
+	if (packets_waiting && !buffers_available) {
+		/* Only resume sending when the controller has buffer space
+		 * available for this connection.
+		 */
+		LOG_DBG("wait on ctlr buffers");
+		k_poll_event_init(&events[0],
+				  K_POLL_TYPE_SEM_AVAILABLE,
+				  K_POLL_MODE_NOTIFY_ONLY,
+				  bt_conn_get_pkts(conn));
+	} else {
+		/* Wait until there is more data to send. */
+		LOG_DBG("wait on host fifo");
+		k_poll_event_init(&events[0],
+				  K_POLL_TYPE_FIFO_DATA_AVAILABLE,
+				  K_POLL_MODE_NOTIFY_ONLY,
+				  &conn->tx_queue);
+	}
 	events[0].tag = BT_EVENT_CONN_TX_QUEUE;
 
 	return 0;