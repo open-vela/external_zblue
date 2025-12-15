mbolivar-nordic/zephyr

cmake: Zephyr module error on module name current
In CMake, ZEPHYR_CURRENT_MODULE_DIR is used during processing of module
CMakeLists.txt files.
If a module is named `current` then it's corresponding
`ZEPHYR_<module_name>_MODULE_DIR` would become ZEPHYR_CURRENT_MODULE_DIR
and thus colliding with internal Zephyr CMake variable.

Using module name `current` is therefore forbidden.
Print an error to users when a module named `current` is used in
Zephyr.

Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

cmake/zephyr_module.cmake
@@ -75,8 +75,14 @@ if(WEST OR ZEPHYR_MODULES)
       list(APPEND ZEPHYR_MODULE_NAMES ${module_name})
 
       string(TOUPPER ${module_name} MODULE_NAME_UPPER)
-      set(ZEPHYR_${MODULE_NAME_UPPER}_MODULE_DIR ${module_path})
-      set(ZEPHYR_${MODULE_NAME_UPPER}_CMAKE_DIR ${cmake_path})
+      if(NOT ${MODULE_NAME_UPPER} STREQUAL CURRENT)
+        set(ZEPHYR_${MODULE_NAME_UPPER}_MODULE_DIR ${cmake_path})
+        set(ZEPHYR_${MODULE_NAME_UPPER}_CMAKE_DIR ${cmake_path})
+      else()
+        message(FATAL_ERROR "Found Zephyr module named: ${module_name}\n\
+${MODULE_NAME_UPPER} is a restricted name for Zephyr modules as it is used for \
+\${ZEPHYR_${MODULE_NAME_UPPER}_MODULE_DIR} CMake variable.")
+      endif()
     endforeach()
   endif()
 else()

open-vela/external_zblue

cmake/zephyr_module.cmake
@@ -76,8 +76,14 @@ if(WEST OR ZEPHYR_MODULES)
       list(APPEND ZEPHYR_MODULE_NAMES ${module_name})
 
       string(TOUPPER ${module_name} MODULE_NAME_UPPER)
-      set(ZEPHYR_${MODULE_NAME_UPPER}_MODULE_DIR ${module_path})
-      set(ZEPHYR_${MODULE_NAME_UPPER}_CMAKE_DIR ${cmake_path})
+      if(NOT ${MODULE_NAME_UPPER} STREQUAL CURRENT)
+        set(ZEPHYR_${MODULE_NAME_UPPER}_MODULE_DIR ${module_path})
+        set(ZEPHYR_${MODULE_NAME_UPPER}_CMAKE_DIR ${cmake_path})
+      else()
+        message(FATAL_ERROR "Found Zephyr module named: ${module_name}\n\
+${MODULE_NAME_UPPER} is a restricted name for Zephyr modules as it is used for \
+\${ZEPHYR_${MODULE_NAME_UPPER}_MODULE_DIR} CMake variable.")
+      endif()
     endforeach()
   endif()
 else()