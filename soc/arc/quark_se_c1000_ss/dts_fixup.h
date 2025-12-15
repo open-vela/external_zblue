zephyrproject-rtos/zephyr

drivers: uart_qmsi: Get clock frequency from DTS
The uart_qmsi driver used system clock frequency
as a base for baudrate calculation. This commit corrects
that by obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

soc/arc/quark_se_c1000_ss/dts_fixup.h
@@ -2,12 +2,14 @@
 
 /* SoC level DTS fixup file */
 
-#define DT_UART_QMSI_0_BAUDRATE	DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
+#define DT_UART_QMSI_0_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002000_CLOCK_FREQUENCY
+#define DT_UART_QMSI_0_BAUDRATE		DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
 #define DT_UART_QMSI_0_NAME		DT_INTEL_QMSI_UART_B0002000_LABEL
 #define DT_UART_QMSI_0_IRQ		DT_INTEL_QMSI_UART_B0002000_IRQ_0
 #define CONFIG_UART_QMSI_0_IRQ_PRI	DT_INTEL_QMSI_UART_B0002000_IRQ_0_PRIORITY
 
-#define DT_UART_QMSI_1_BAUDRATE	DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
+#define DT_UART_QMSI_1_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002400_CLOCK_FREQUENCY
+#define DT_UART_QMSI_1_BAUDRATE		DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
 #define DT_UART_QMSI_1_NAME		DT_INTEL_QMSI_UART_B0002400_LABEL
 #define DT_UART_QMSI_1_IRQ		DT_INTEL_QMSI_UART_B0002400_IRQ_0
 #define CONFIG_UART_QMSI_1_IRQ_PRI	DT_INTEL_QMSI_UART_B0002400_IRQ_0_PRIORITY

soc/x86/intel_quark/quark_se/dts_fixup.h
@@ -2,13 +2,15 @@
 
 /* SoC level DTS fixup file */
 
-#define DT_UART_QMSI_0_BAUDRATE	DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
+#define DT_UART_QMSI_0_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002000_CLOCK_FREQUENCY
+#define DT_UART_QMSI_0_BAUDRATE		DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
 #define DT_UART_QMSI_0_NAME		DT_INTEL_QMSI_UART_B0002000_LABEL
 #define DT_UART_QMSI_0_IRQ		DT_INTEL_QMSI_UART_B0002000_IRQ_0
 #define CONFIG_UART_QMSI_0_IRQ_PRI	DT_INTEL_QMSI_UART_B0002000_IRQ_0_PRIORITY
 #define DT_UART_QMSI_0_IRQ_FLAGS	DT_INTEL_QMSI_UART_B0002000_IRQ_0_SENSE
 
-#define DT_UART_QMSI_1_BAUDRATE	DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
+#define DT_UART_QMSI_1_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002400_CLOCK_FREQUENCY
+#define DT_UART_QMSI_1_BAUDRATE		DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
 #define DT_UART_QMSI_1_NAME		DT_INTEL_QMSI_UART_B0002400_LABEL
 #define DT_UART_QMSI_1_IRQ		DT_INTEL_QMSI_UART_B0002400_IRQ_0
 #define CONFIG_UART_QMSI_1_IRQ_PRI	DT_INTEL_QMSI_UART_B0002400_IRQ_0_PRIORITY

open-vela/external_zblue

soc/arc/quark_se_c1000_ss/dts_fixup.h
@@ -2,12 +2,14 @@
 
 /* SoC level DTS fixup file */
 
-#define DT_UART_QMSI_0_BAUDRATE	DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
+#define DT_UART_QMSI_0_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002000_CLOCKS_CLOCK_FREQUENCY
+#define DT_UART_QMSI_0_BAUDRATE		DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
 #define DT_UART_QMSI_0_NAME		DT_INTEL_QMSI_UART_B0002000_LABEL
 #define DT_UART_QMSI_0_IRQ		DT_INTEL_QMSI_UART_B0002000_IRQ_0
 #define DT_UART_QMSI_0_IRQ_PRI	DT_INTEL_QMSI_UART_B0002000_IRQ_0_PRIORITY
 
-#define DT_UART_QMSI_1_BAUDRATE	DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
+#define DT_UART_QMSI_1_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002400_CLOCKS_CLOCK_FREQUENCY
+#define DT_UART_QMSI_1_BAUDRATE		DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
 #define DT_UART_QMSI_1_NAME		DT_INTEL_QMSI_UART_B0002400_LABEL
 #define DT_UART_QMSI_1_IRQ		DT_INTEL_QMSI_UART_B0002400_IRQ_0
 #define DT_UART_QMSI_1_IRQ_PRI	DT_INTEL_QMSI_UART_B0002400_IRQ_0_PRIORITY

soc/x86/intel_quark/quark_se/dts_fixup.h
@@ -2,13 +2,15 @@
 
 /* SoC level DTS fixup file */
 
-#define DT_UART_QMSI_0_BAUDRATE	DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
+#define DT_UART_QMSI_0_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002000_CLOCKS_CLOCK_FREQUENCY
+#define DT_UART_QMSI_0_BAUDRATE		DT_INTEL_QMSI_UART_B0002000_CURRENT_SPEED
 #define DT_UART_QMSI_0_NAME		DT_INTEL_QMSI_UART_B0002000_LABEL
 #define DT_UART_QMSI_0_IRQ		DT_INTEL_QMSI_UART_B0002000_IRQ_0
 #define DT_UART_QMSI_0_IRQ_PRI	DT_INTEL_QMSI_UART_B0002000_IRQ_0_PRIORITY
 #define DT_UART_QMSI_0_IRQ_FLAGS	DT_INTEL_QMSI_UART_B0002000_IRQ_0_SENSE
 
-#define DT_UART_QMSI_1_BAUDRATE	DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
+#define DT_UART_QMSI_1_CLOCK_FREQUENCY	DT_INTEL_QMSI_UART_B0002400_CLOCKS_CLOCK_FREQUENCY
+#define DT_UART_QMSI_1_BAUDRATE		DT_INTEL_QMSI_UART_B0002400_CURRENT_SPEED
 #define DT_UART_QMSI_1_NAME		DT_INTEL_QMSI_UART_B0002400_LABEL
 #define DT_UART_QMSI_1_IRQ		DT_INTEL_QMSI_UART_B0002400_IRQ_0
 #define DT_UART_QMSI_1_IRQ_PRI	DT_INTEL_QMSI_UART_B0002400_IRQ_0_PRIORITY