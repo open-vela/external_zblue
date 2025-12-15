zephyrproject-rtos/zephyr

soc: samd2x: Get CPU clock frequency from DTS
The SoC initialization code used system clock frequency
as a CPU clock frequency. This commit corrects that by
obtaining the needed value from DTS.

Signed-off-by: Piotr Zięcik <piotr.ziecik@nordicsemi.no>

soc/arm/atmel_sam0/samd20/soc.h
@@ -54,7 +54,7 @@
 #endif /* _ASMLANGUAGE */
 
 /** Processor Clock (HCLK) Frequency */
-#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ DT_CPUS_CPU_0_CLOCK_FREQUENCY
 /** Master Clock (MCK) Frequency */
 #define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
 #define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768

soc/arm/atmel_sam0/samd21/soc.h
@@ -48,7 +48,7 @@
 #endif /* _ASMLANGUAGE */
 
 /** Processor Clock (HCLK) Frequency */
-#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ DT_CPUS_CPU_0_CLOCK_FREQUENCY
 /** Master Clock (MCK) Frequency */
 #define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
 #define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768

open-vela/external_zblue

soc/arm/atmel_sam0/samd20/soc.h
@@ -54,7 +54,7 @@
 #endif /* _ASMLANGUAGE */
 
 /** Processor Clock (HCLK) Frequency */
-#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ DT_ARM_CORTEX_M0PLUS_0_CLOCK_FREQUENCY
 /** Master Clock (MCK) Frequency */
 #define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
 #define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768

soc/arm/atmel_sam0/samd21/soc.h
@@ -48,7 +48,7 @@
 #endif /* _ASMLANGUAGE */
 
 /** Processor Clock (HCLK) Frequency */
-#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ CONFIG_SYS_CLOCK_HW_CYCLES_PER_SEC
+#define SOC_ATMEL_SAM0_HCLK_FREQ_HZ DT_ARM_CORTEX_M0PLUS_0_CLOCK_FREQUENCY
 /** Master Clock (MCK) Frequency */
 #define SOC_ATMEL_SAM0_MCK_FREQ_HZ SOC_ATMEL_SAM0_HCLK_FREQ_HZ
 #define SOC_ATMEL_SAM0_XOSC32K_FREQ_HZ 32768