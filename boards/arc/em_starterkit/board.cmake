mbolivar/zephyr

boards: allow cmake-time overrides of all runners
Convert all board_set_xxer(foo) calls to board_set_xxer_ifndef(foo),
which allows the user to make their own decision at CMake time.

Signed-off-by: Marti Bolivar <marti.bolivar@nordicsemi.no>

boards/arc/em_starterkit/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # TODO: can this board just use the usual openocd runner?
-board_set_flasher(em-starterkit)
-board_set_debugger(em-starterkit)
+board_set_flasher_ifndef(em-starterkit)
+board_set_debugger_ifndef(em-starterkit)
 board_finalize_runner_args(em-starterkit)

boards/arm/mps2_an385/board.cmake
@@ -10,4 +10,4 @@ set(QEMU_FLAGS_${ARCH}
   -vga none
   )
 
-board_set_debugger(qemu)
+board_set_debugger_ifndef(qemu)

boards/arm/nrf52_sparkfun/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher(pyocd.sh)
-board_set_debugger(pyocd.sh)
+board_set_flasher_ifndef(pyocd.sh)
+board_set_debugger_ifndef(pyocd.sh)
 
 set(PYOCD_TARGET nrf52)
 

boards/arm/v2m_beetle/board.cmake
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_debugger(openocd)
+board_set_debugger_ifndef(openocd)

boards/common/esp32.board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher(esp32)
+board_set_flasher_ifndef(esp32)
 
 if(NOT DEFINED ESP_IDF_PATH)
   if(DEFINED ENV{ESP_IDF_PATH})

boards/nios2/qemu_nios2/board.cmake
@@ -9,4 +9,4 @@ set(QEMU_FLAGS_${ARCH}
   -nographic
   )
 
-board_set_debugger(qemu)
+board_set_debugger_ifndef(qemu)

boards/riscv32/qemu_riscv32/board.cmake
@@ -9,4 +9,4 @@ set(QEMU_FLAGS_${ARCH}
   -machine sifive_e
   )
 
-board_set_debugger(qemu)
+board_set_debugger_ifndef(qemu)

open-vela/external_zblue

boards/arc/em_starterkit/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # TODO: can this board just use the usual openocd runner?
-board_set_flasher(em-starterkit)
-board_set_debugger(em-starterkit)
+board_set_flasher_ifnset(em-starterkit)
+board_set_debugger_ifnset(em-starterkit)
 board_finalize_runner_args(em-starterkit)

boards/arm/mps2_an385/board.cmake
@@ -10,4 +10,4 @@ set(QEMU_FLAGS_${ARCH}
   -vga none
   )
 
-board_set_debugger(qemu)
+board_set_debugger_ifnset(qemu)

boards/arm/nrf52_sparkfun/board.cmake
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher(pyocd.sh)
-board_set_debugger(pyocd.sh)
+board_set_flasher_ifnset(pyocd.sh)
+board_set_debugger_ifnset(pyocd.sh)
 
 set(PYOCD_TARGET nrf52)
 

boards/arm/v2m_beetle/board.cmake
@@ -1,3 +1,3 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_debugger(openocd)
+board_set_debugger_ifnset(openocd)

boards/common/esp32.board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_set_flasher(esp32)
+board_set_flasher_ifnset(esp32)
 
 if(NOT DEFINED ESP_IDF_PATH)
   if(DEFINED ENV{ESP_IDF_PATH})

boards/nios2/qemu_nios2/board.cmake
@@ -9,4 +9,4 @@ set(QEMU_FLAGS_${ARCH}
   -nographic
   )
 
-board_set_debugger(qemu)
+board_set_debugger_ifnset(qemu)

boards/riscv32/qemu_riscv32/board.cmake
@@ -9,4 +9,4 @@ set(QEMU_FLAGS_${ARCH}
   -machine sifive_e
   )
 
-board_set_debugger(qemu)
+board_set_debugger_ifnset(qemu)