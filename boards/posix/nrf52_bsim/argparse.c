aescolar/zephyr

nrf52_bsim: bstest options: Prepend options with mcu number
To allow knowing for which MCU are the options,
prepend them with the MCU number.
The primary/preferred MCU will also keep an alias to the
old options (without the cpu<n>_ prefix)

Signed-off-by: Alberto Escolar Piedras <alberto.escolar.piedras@nordicsemi.no>

boards/posix/nrf52_bsim/argparse.c
@@ -14,7 +14,7 @@
 #include "nsi_tracing.h"
 #include "nsi_main.h"
 
-static const char exe_name[] = "nrf52_bsim options:";
+static const char exe_name[] = "nrf_bsim options:";
 
 static char *testid;
 
@@ -33,7 +33,7 @@ static void nrfbsim_register_args(void)
 {
 	static bs_args_struct_t args_struct_toadd[] = {
 		{
-		.option = "testid",
+		.option = "cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testid",
 		.name = "testid",
 		.type = 's',
 		.dest = (void *)&testid,
@@ -42,11 +42,28 @@ static void nrfbsim_register_args(void)
 		},
 		{
 		.is_switch = true,
-		.option = "testslist",
+		.option = "cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testslist",
 		.type = 'b',
 		.call_when_found = cmd_testlist_found,
 		.descript = "Print information about the available bs application tests"
 		},
+#if (CONFIG_NATIVE_SIMULATOR_MCU_N == CONFIG_NATIVE_SIMULATOR_PRIMARY_MCU_N)
+		{
+		.option = "testid",
+		.name = "testid",
+		.type = 's',
+		.dest = (void *)&testid,
+		.call_when_found = cmd_testid_found,
+		.descript = "Alias of cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testid"
+		},
+		{
+		.is_switch = true,
+		.option = "testslist",
+		.type = 'b',
+		.call_when_found = cmd_testlist_found,
+		.descript = "Alias of cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testslist"
+		},
+#endif
 		ARG_TABLE_ENDMARKER
 	};
 

open-vela/external_zblue

boards/posix/nrf52_bsim/argparse.c
@@ -14,7 +14,7 @@
 #include "nsi_tracing.h"
 #include "nsi_main.h"
 
-static const char exe_name[] = "nrf52_bsim options:";
+static const char exe_name[] = "nrf_bsim options:";
 
 static char *testid;
 
@@ -33,7 +33,7 @@ static void nrfbsim_register_args(void)
 {
 	static bs_args_struct_t args_struct_toadd[] = {
 		{
-		.option = "testid",
+		.option = "cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testid",
 		.name = "testid",
 		.type = 's',
 		.dest = (void *)&testid,
@@ -42,11 +42,28 @@ static void nrfbsim_register_args(void)
 		},
 		{
 		.is_switch = true,
-		.option = "testslist",
+		.option = "cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testslist",
 		.type = 'b',
 		.call_when_found = cmd_testlist_found,
 		.descript = "Print information about the available bs application tests"
 		},
+#if (CONFIG_NATIVE_SIMULATOR_MCU_N == CONFIG_NATIVE_SIMULATOR_PRIMARY_MCU_INDEX)
+		{
+		.option = "testid",
+		.name = "testid",
+		.type = 's',
+		.dest = (void *)&testid,
+		.call_when_found = cmd_testid_found,
+		.descript = "Alias of cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testid"
+		},
+		{
+		.is_switch = true,
+		.option = "testslist",
+		.type = 'b',
+		.call_when_found = cmd_testlist_found,
+		.descript = "Alias of cpu" STRINGIFY(CONFIG_NATIVE_SIMULATOR_MCU_N) "_testslist"
+		},
+#endif
 		ARG_TABLE_ENDMARKER
 	};
 