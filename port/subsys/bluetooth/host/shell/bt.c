1993559e0eb8f1d5f9014d12233e6cb591d66827
zhongzhijie1 <zhongzhijie1@xiaomi.com>
2026-01-05 12:27:54 +0800

    zblue: rename scan type enums to avoid BT_LE_SCAN_TYPE_* collision
    VELAPLATFO-82081
    
    Framework exports ble_scan_type_t with BT_LE_SCAN_TYPE_PASSIVE/ACTIVE.
    Zephyr bluetooth.h defined the same enumerators, causing redeclaration errors
    when both headers are included.
    
    Rename Zephyr scan type enumerators to Z_BT_LE_SCAN_TYPE_{PASSIVE,ACTIVE}
    and update all internal references.
    
    Change-Id: I9cfadccc61b5c43b21a8a763a8b22f83045ab2f1
    Signed-off-by: zhongzhijie1 <zhongzhijie1@xiaomi.com>

--- port/subsys/bluetooth/host/shell/bt.c
+++ port/subsys/bluetooth/host/shell/bt.c
@@ -1656,7 +1656,7 @@ static int cmd_active_scan_on(const struct shell *sh, uint8_t dev_id, uint32_t o
 {
 	int err;
 	struct bt_le_scan_param param = {
-			.type       = BT_LE_SCAN_TYPE_ACTIVE,
+			.type       = Z_(BT_LE_SCAN_TYPE_ACTIVE),
 			.options    = BT_LE_SCAN_OPT_NONE,
 			.interval   = BT_GAP_SCAN_FAST_INTERVAL,
 			.window     = BT_GAP_SCAN_FAST_WINDOW,
@@ -1685,7 +1685,7 @@ static int cmd_passive_scan_on(const struct shell *sh, uint8_t dev_id, uint32_t 
 			       uint16_t timeout)
 {
 	struct bt_le_scan_param param = {
-			.type       = BT_LE_SCAN_TYPE_PASSIVE,
+			.type       = Z_(BT_LE_SCAN_TYPE_PASSIVE),
 			.options    = BT_LE_SCAN_OPT_NONE,
 			.interval   = 0x10,
 			.window     = 0x10,
@@ -3441,7 +3441,7 @@ static int cmd_auto_conn(const struct shell *sh, size_t argc, char *argv[])
 static int cmd_connect_le_name(const struct shell *sh, size_t argc, char *argv[])
 {
 	const struct bt_le_scan_param param = {
-		.type       = BT_LE_SCAN_TYPE_ACTIVE,
+		.type       = Z_(BT_LE_SCAN_TYPE_ACTIVE),
 		.options    = BT_LE_SCAN_OPT_NONE,
 		.interval   = BT_GAP_SCAN_FAST_INTERVAL,
 		.window     = BT_GAP_SCAN_FAST_WINDOW,
--- tests/bsim/bluetooth/host/misc/disconnect/dut/src/main.c
+++ tests/bsim/bluetooth/host/misc/disconnect/dut/src/main.c
@@ -121,7 +121,7 @@ static void connect(void)
 {
 	int err;
 	struct bt_le_scan_param scan_param = {
-		.type = BT_LE_SCAN_TYPE_ACTIVE,
+		.type = Z_(BT_LE_SCAN_TYPE_ACTIVE),
 		.options = BT_LE_SCAN_OPT_NONE,
 		.interval = BT_GAP_SCAN_FAST_INTERVAL,
 		.window = BT_GAP_SCAN_FAST_WINDOW,
