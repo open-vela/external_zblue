9089dd15da8a17d23302203dd305c88f6fcf8de4
Lu Jia <jialu@xiaomi.com>
2026-01-23 15:42:57 +0800

    Fix the failed binding problem
    bug: v/84588
    
    Before pairing, the link key will be deleted. If the controller returns an error, subsequent connection processes will not proceed, leading to a bonding failure.
    
    Signed-off-by: jialu <jialu@xiaomi.com>

--- subsys/bluetooth/host/conn.c
+++ subsys/bluetooth/host/conn.c
@@ -2844,10 +2844,7 @@ struct bt_conn *bt_conn_pair_br_mc(uint8_t dev_id, bt_addr_t *bdaddr, bt_securit
 	}
 
 	/* Tell controller to delete the link key if it has one stored */
-	err = bt_br_delete_stored_link_key(hdev, bdaddr, 1);
-	if (err) {
-		return NULL;
-	}
+	bt_br_delete_stored_link_key(hdev, bdaddr, 0);
 
 	if (!BT_FEAT_SSP(hdev->features)) {
 		/* pin type */
