Balaklaka/zephyr

Tests: Bluetooth: Mesh: BLOB cli broadcast unicast
Add bsim test using unicast addr for broadcast API of the BLOB client
model.

Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_broadcast.sh
@@ -9,3 +9,9 @@ RunTest mesh_blob \
 
 RunTest mesh_blob \
 	blob_cli_broadcast_trans
+
+RunTest mesh_blob \
+	blob_cli_broadcast_unicast_seq
+
+RunTest mesh_blob \
+	blob_cli_broadcast_unicast

open-vela/external_zblue

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_broadcast.sh
@@ -11,3 +11,11 @@ RunTest blob_broadcast \
 conf=prj_mesh1d1_conf
 RunTest blob_broadcast \
 	blob_cli_broadcast_trans
+
+conf=prj_mesh1d1_conf
+RunTest blob_broadcast \
+	blob_cli_broadcast_unicast_seq
+
+conf=prj_mesh1d1_conf
+RunTest blob_broadcast \
+	blob_cli_broadcast_unicast