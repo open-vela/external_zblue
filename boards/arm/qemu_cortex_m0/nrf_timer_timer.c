nashif/zephyr

clock: remove z_ from semi-public APIs
The clock/timer APIs are not application facing APIs, however, similar
to arch_ and a few other APIs they are available to implement drivers
and add support for new hardware and are documented and available to be
used outside of the clock/kernel subsystems.

Remove the leading z_ and provide them as clock_* APIs for someone
writing a new timer driver to use.

Signed-off-by: Anas Nashif <anas.nashif@intel.com>

boards/arm/qemu_cortex_m0/nrf_timer_timer.c
@@ -157,10 +157,10 @@ void timer0_nrf_isr(void *arg)
 		set_absolute_ticks(last_count + CYC_PER_TICK);
 	}
 
-	z_clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ? dticks : (dticks > 0));
+	clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ? dticks : (dticks > 0));
 }
 
-int z_clock_driver_init(const struct device *device)
+int clock_driver_init(const struct device *device)
 {
 	ARG_UNUSED(device);
 
@@ -185,7 +185,7 @@ int z_clock_driver_init(const struct device *device)
 	return 0;
 }
 
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 	uint32_t cyc;
@@ -234,7 +234,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	NVIC_ClearPendingIRQ(TIMER0_IRQn);
 }
 
-uint32_t z_clock_elapsed(void)
+uint32_t clock_elapsed(void)
 {
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
 		return 0;

drivers/timer/ite_it8xxx2_timer.c
@@ -201,10 +201,10 @@ static void timer_isr(const void *unused)
 				- accumulated_cycle_count) / CYC_PER_TICK;
 	accumulated_cycle_count += dticks * CYC_PER_TICK;
 	k_spin_unlock(&lock, key);
-	z_clock_announce(dticks);
+	clock_announce(dticks);
 }
 
-int z_clock_driver_init(const struct device *device)
+int clock_driver_init(const struct device *device)
 {
 	timer_init_combine(CTIMER_HW_TIMER_INDEX, TRUE);
 	timer_init(CTIMER_HW_TIMER_INDEX, ET_PSR_32K, TRUE, FALSE, 0);
@@ -215,7 +215,7 @@ int z_clock_driver_init(const struct device *device)
 	return 0;
 }
 
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 
@@ -227,7 +227,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	k_spin_unlock(&lock, key);
 }
 
-uint32_t z_clock_elapsed(void)
+uint32_t clock_elapsed(void)
 {
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
 		return 0;

drivers/timer/native_posix_timer.c
@@ -44,7 +44,7 @@ static void np_timer_isr(const void *arg)
 	int32_t elapsed_ticks = (now - last_tick_time)/tick_period;
 
 	last_tick_time += elapsed_ticks*tick_period;
-	z_clock_announce(elapsed_ticks);
+	clock_announce(elapsed_ticks);
 }
 
 /**
@@ -60,7 +60,7 @@ void np_timer_isr_test_hook(const void *arg)
  *
  * Enable the hw timer, setting its tick period, and setup its interrupt
  */
-int z_clock_driver_init(const struct device *device)
+int clock_driver_init(const struct device *device)
 {
 	ARG_UNUSED(device);
 
@@ -79,7 +79,7 @@ int z_clock_driver_init(const struct device *device)
  * @brief Set system clock timeout
  *
  * Informs the system clock driver that the next needed call to
- * z_clock_announce() will not be until the specified number of ticks
+ * clock_announce() will not be until the specified number of ticks
  * from the the current time have elapsed.
  *
  * See system_timer.h for more information
@@ -88,7 +88,7 @@ int z_clock_driver_init(const struct device *device)
  * @param idle Hint to the driver that the system is about to enter
  *        the idle state immediately after setting the timeout
  */
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 
@@ -96,7 +96,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	uint64_t silent_ticks;
 
 	/* Note that we treat INT_MAX literally as anyhow the maximum amount of
-	 * ticks we can report with z_clock_announce() is INT_MAX
+	 * ticks we can report with clock_announce() is INT_MAX
 	 */
 	if (ticks == K_TICKS_FOREVER) {
 		silent_ticks = INT64_MAX;
@@ -110,14 +110,14 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 }
 
 /**
- * @brief Ticks elapsed since last z_clock_announce() call
+ * @brief Ticks elapsed since last clock_announce() call
  *
  * Queries the clock driver for the current time elapsed since the
- * last call to z_clock_announce() was made.  The kernel will call
+ * last call to clock_announce() was made.  The kernel will call
  * this with appropriate locking, the driver needs only provide an
  * instantaneous answer.
  */
-uint32_t z_clock_elapsed(void)
+uint32_t clock_elapsed(void)
 {
 	return (hwm_get_time() - last_tick_time)/tick_period;
 }

drivers/timer/nrf_rtc_timer.c
@@ -242,7 +242,7 @@ static void sys_clock_timeout_handler(uint32_t chan,
 					  sys_clock_timeout_handler, NULL);
 	}
 
-	z_clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ?
+	clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ?
 						dticks : (dticks > 0));
 }
 
@@ -299,7 +299,7 @@ void z_nrf_rtc_timer_chan_free(uint32_t chan)
 	atomic_or(&alloc_mask, BIT(chan));
 }
 
-int z_clock_driver_init(const struct device *device)
+int clock_driver_init(const struct device *device)
 {
 	ARG_UNUSED(device);
 	static const enum nrf_lfclk_start_mode mode =
@@ -339,7 +339,7 @@ int z_clock_driver_init(const struct device *device)
 	return 0;
 }
 
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 	uint32_t cyc;
@@ -380,7 +380,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	compare_set(0, cyc, sys_clock_timeout_handler, NULL);
 }
 
-uint32_t z_clock_elapsed(void)
+uint32_t clock_elapsed(void)
 {
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
 		return 0;

kernel/sched.c
@@ -351,7 +351,7 @@ void z_reset_time_slice(void)
 	 * FUTURE z_time_slice() call.
 	 */
 	if (slice_time != 0) {
-		_current_cpu->slice_ticks = slice_time + z_clock_elapsed();
+		_current_cpu->slice_ticks = slice_time + clock_elapsed();
 		z_set_timeout_expiry(slice_time, false);
 	}
 }

open-vela/external_zblue

boards/arm/qemu_cortex_m0/nrf_timer_timer.c
@@ -157,10 +157,10 @@ void timer0_nrf_isr(void *arg)
 		set_absolute_ticks(last_count + CYC_PER_TICK);
 	}
 
-	z_clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ? dticks : (dticks > 0));
+	sys_clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ? dticks : (dticks > 0));
 }
 
-int z_clock_driver_init(const struct device *device)
+int sys_clock_driver_init(const struct device *device)
 {
 	ARG_UNUSED(device);
 
@@ -185,7 +185,7 @@ int z_clock_driver_init(const struct device *device)
 	return 0;
 }
 
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void sys_clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 	uint32_t cyc;
@@ -234,7 +234,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	NVIC_ClearPendingIRQ(TIMER0_IRQn);
 }
 
-uint32_t z_clock_elapsed(void)
+uint32_t sys_clock_elapsed(void)
 {
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
 		return 0;

drivers/timer/ite_it8xxx2_timer.c
@@ -201,10 +201,10 @@ static void timer_isr(const void *unused)
 				- accumulated_cycle_count) / CYC_PER_TICK;
 	accumulated_cycle_count += dticks * CYC_PER_TICK;
 	k_spin_unlock(&lock, key);
-	z_clock_announce(dticks);
+	sys_clock_announce(dticks);
 }
 
-int z_clock_driver_init(const struct device *device)
+int sys_clock_driver_init(const struct device *device)
 {
 	timer_init_combine(CTIMER_HW_TIMER_INDEX, TRUE);
 	timer_init(CTIMER_HW_TIMER_INDEX, ET_PSR_32K, TRUE, FALSE, 0);
@@ -215,7 +215,7 @@ int z_clock_driver_init(const struct device *device)
 	return 0;
 }
 
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void sys_clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 
@@ -227,7 +227,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	k_spin_unlock(&lock, key);
 }
 
-uint32_t z_clock_elapsed(void)
+uint32_t sys_clock_elapsed(void)
 {
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
 		return 0;

drivers/timer/native_posix_timer.c
@@ -44,7 +44,7 @@ static void np_timer_isr(const void *arg)
 	int32_t elapsed_ticks = (now - last_tick_time)/tick_period;
 
 	last_tick_time += elapsed_ticks*tick_period;
-	z_clock_announce(elapsed_ticks);
+	sys_clock_announce(elapsed_ticks);
 }
 
 /**
@@ -60,7 +60,7 @@ void np_timer_isr_test_hook(const void *arg)
  *
  * Enable the hw timer, setting its tick period, and setup its interrupt
  */
-int z_clock_driver_init(const struct device *device)
+int sys_clock_driver_init(const struct device *device)
 {
 	ARG_UNUSED(device);
 
@@ -79,7 +79,7 @@ int z_clock_driver_init(const struct device *device)
  * @brief Set system clock timeout
  *
  * Informs the system clock driver that the next needed call to
- * z_clock_announce() will not be until the specified number of ticks
+ * sys_clock_announce() will not be until the specified number of ticks
  * from the the current time have elapsed.
  *
  * See system_timer.h for more information
@@ -88,7 +88,7 @@ int z_clock_driver_init(const struct device *device)
  * @param idle Hint to the driver that the system is about to enter
  *        the idle state immediately after setting the timeout
  */
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void sys_clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 
@@ -96,7 +96,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	uint64_t silent_ticks;
 
 	/* Note that we treat INT_MAX literally as anyhow the maximum amount of
-	 * ticks we can report with z_clock_announce() is INT_MAX
+	 * ticks we can report with sys_clock_announce() is INT_MAX
 	 */
 	if (ticks == K_TICKS_FOREVER) {
 		silent_ticks = INT64_MAX;
@@ -110,14 +110,14 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 }
 
 /**
- * @brief Ticks elapsed since last z_clock_announce() call
+ * @brief Ticks elapsed since last sys_clock_announce() call
  *
  * Queries the clock driver for the current time elapsed since the
- * last call to z_clock_announce() was made.  The kernel will call
+ * last call to sys_clock_announce() was made.  The kernel will call
  * this with appropriate locking, the driver needs only provide an
  * instantaneous answer.
  */
-uint32_t z_clock_elapsed(void)
+uint32_t sys_clock_elapsed(void)
 {
 	return (hwm_get_time() - last_tick_time)/tick_period;
 }

drivers/timer/nrf_rtc_timer.c
@@ -242,7 +242,7 @@ static void sys_clock_timeout_handler(uint32_t chan,
 					  sys_clock_timeout_handler, NULL);
 	}
 
-	z_clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ?
+	sys_clock_announce(IS_ENABLED(CONFIG_TICKLESS_KERNEL) ?
 						dticks : (dticks > 0));
 }
 
@@ -299,7 +299,7 @@ void z_nrf_rtc_timer_chan_free(uint32_t chan)
 	atomic_or(&alloc_mask, BIT(chan));
 }
 
-int z_clock_driver_init(const struct device *device)
+int sys_clock_driver_init(const struct device *device)
 {
 	ARG_UNUSED(device);
 	static const enum nrf_lfclk_start_mode mode =
@@ -339,7 +339,7 @@ int z_clock_driver_init(const struct device *device)
 	return 0;
 }
 
-void z_clock_set_timeout(int32_t ticks, bool idle)
+void sys_clock_set_timeout(int32_t ticks, bool idle)
 {
 	ARG_UNUSED(idle);
 	uint32_t cyc;
@@ -380,7 +380,7 @@ void z_clock_set_timeout(int32_t ticks, bool idle)
 	compare_set(0, cyc, sys_clock_timeout_handler, NULL);
 }
 
-uint32_t z_clock_elapsed(void)
+uint32_t sys_clock_elapsed(void)
 {
 	if (!IS_ENABLED(CONFIG_TICKLESS_KERNEL)) {
 		return 0;

kernel/sched.c
@@ -351,7 +351,7 @@ void z_reset_time_slice(void)
 	 * FUTURE z_time_slice() call.
 	 */
 	if (slice_time != 0) {
-		_current_cpu->slice_ticks = slice_time + z_clock_elapsed();
+		_current_cpu->slice_ticks = slice_time + sys_clock_elapsed();
 		z_set_timeout_expiry(slice_time, false);
 	}
 }