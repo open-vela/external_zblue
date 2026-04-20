Balaklaka/zephyr

Tests: Bluetooth: Mesh: BLOB cli no response
- Tests that BLOB Client suspends if no target responds to Block get.
- Tests that BLOB Client transfer ends if no target responds to Xfer
  get.

Signed-off-by: Anders Storrø <anders.storro@nordicsemi.no>

tests/bluetooth/bsim_bt/bsim_test_mesh/tests_scripts/blob_mdls/blob_cli_no_rsp.sh
@@ -1,0 +1,15 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+RunTest mesh_blob \
+	blob_cli_fail_on_no_rsp \
+	blob_srv_fail_on_block_get \
+	blob_srv_fail_on_block_get -- -argstest msg-fail-type=0
+
+RunTest mesh_blob \
+	blob_cli_fail_on_no_rsp \
+	blob_srv_fail_on_xfer_get \
+	blob_srv_fail_on_xfer_get -- -argstest msg-fail-type=1

open-vela/external_zblue

tests/bluetooth/bsim/mesh/tests_scripts/blob_mdls/blob_cli_no_rsp.sh
@@ -1,0 +1,17 @@
+#!/usr/bin/env bash
+# Copyright 2022 Nordic Semiconductor
+# SPDX-License-Identifier: Apache-2.0
+
+source $(dirname "${BASH_SOURCE[0]}")/../../_mesh_test.sh
+
+conf=prj_mesh1d1_conf
+RunTest blob_no_rsp \
+	blob_cli_fail_on_no_rsp \
+	blob_srv_fail_on_block_get \
+	blob_srv_fail_on_block_get -- -argstest msg-fail-type=0
+
+conf=prj_mesh1d1_conf
+RunTest blob_no_rsp \
+	blob_cli_fail_on_no_rsp \
+	blob_srv_fail_on_xfer_get \
+	blob_srv_fail_on_xfer_get -- -argstest msg-fail-type=1