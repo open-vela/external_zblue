mniestroj/zephyr

boards: arm: add Seeed XIAO BLE board
The Seeed XIAO BLE is a tiny (21 mm x 17.5 mm) Nordic Semiconductor
nRF52840 ARM Cortex-M4F development board with onboard LEDs, USB port,
QSPI flash, battery charger, and range of I/O broken out into 14 pins.

Signed-off-by: Marcin Niestroj <m.niestroj@emb.dev>

tests/drivers/adc/adc_api/src/test_adc.c
@@ -65,7 +65,8 @@
 	defined(CONFIG_BOARD_UBX_EVKNINAB4_NRF52833) || \
 	defined(CONFIG_BOARD_WE_PROTEUS2EV_NRF52832) || \
 	defined(CONFIG_BOARD_WE_PROTEUS3EV_NRF52840) || \
-	defined(CONFIG_BOARD_BT610)
+	defined(CONFIG_BOARD_BT610) || \
+	defined(CONFIG_BOARD_XIAO_BLE)
 
 #include <hal/nrf_saadc.h>
 #define ADC_DEVICE_NAME		DT_LABEL(DT_INST(0, nordic_nrf_saadc))

open-vela/external_zblue

Co-authored-by: Peter Johanson <peter@peterjohanson.com>

tests/drivers/adc/adc_api/src/test_adc.c
@@ -69,7 +69,8 @@
 	defined(CONFIG_BOARD_PAN1780_EVB) || \
 	defined(CONFIG_BOARD_PAN1781_EVB) || \
 	defined(CONFIG_BOARD_PAN1782_EVB) || \
-	defined(CONFIG_BOARD_PAN1770_EVB)
+	defined(CONFIG_BOARD_PAN1770_EVB) || \
+	defined(CONFIG_BOARD_XIAO_BLE)
 
 #include <hal/nrf_saadc.h>
 #define ADC_DEVICE_NODE		DT_INST(0, nordic_nrf_saadc)