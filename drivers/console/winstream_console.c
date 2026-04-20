nashif/zephyr

xtensa: move to use system cache API support for coherency
Remove custom implementation and use system cache interface instead.

Signed-off-by: Anas Nashif <anas.nashif@intel.com>

drivers/console/winstream_console.c
@@ -8,10 +8,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/winstream.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/cache.h>
 
 #include <adsp_memory.h>
 #include <mem_window.h>
-#include <soc.h>
 
 struct k_spinlock trace_lock;
 
@@ -55,7 +55,7 @@ static int winstream_console_init(void)
 	}
 	const struct mem_win_config *config = dev->config;
 	void *buf =
-		arch_xtensa_uncached_ptr((__sparse_force void __sparse_cache *)config->mem_base);
+		sys_cache_uncached_ptr((__sparse_force void __sparse_cache *)config->mem_base);
 
 	winstream = sys_winstream_init(buf, config->size);
 

drivers/mm/mm_drv_intel_adsp_mtl_tlb.c
@@ -16,7 +16,7 @@
  * Note that all passed in addresses should be in cached range
  * (aka cached addresses). Due to the need to calculate TLB
  * indexes, virtual addresses will be converted internally to
- * cached one via z_soc_cached_ptr(). However, physical addresses
+ * cached one via sys_cache_cached_ptr(). However, physical addresses
  * are untouched.
  */
 
@@ -183,8 +183,8 @@ int sys_mm_drv_map_page(void *virt, uintptr_t phys, uint32_t flags)
 	 * the cached physical address is needed to perform
 	 * bound check.
 	 */
-	uintptr_t pa = POINTER_TO_UINT(z_soc_cached_ptr(UINT_TO_POINTER(phys)));
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t pa = POINTER_TO_UINT(sys_cache_cached_ptr(UINT_TO_POINTER(phys)));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr(virt));
 
 	ARG_UNUSED(flags);
 
@@ -215,7 +215,7 @@ int sys_mm_drv_map_page(void *virt, uintptr_t phys, uint32_t flags)
 				 "unable to assign free phys page %d\n", ret);
 			goto out;
 		}
-		pa = POINTER_TO_UINT(z_soc_cached_ptr(phys_block_ptr));
+		pa = POINTER_TO_UINT(sys_cache_cached_ptr(phys_block_ptr));
 	}
 
 	/* Check bounds of physical address space */
@@ -296,7 +296,7 @@ int sys_mm_drv_map_region(void *virt, uintptr_t phys,
 		goto out;
 	}
 
-	va = (__sparse_force uint8_t *)z_soc_cached_ptr(virt);
+	va = (__sparse_force uint8_t *)sys_cache_cached_ptr(virt);
 	pa = phys;
 
 	key = k_spin_lock(&sys_mm_drv_common_lock);
@@ -324,7 +324,7 @@ out:
 int sys_mm_drv_map_array(void *virt, uintptr_t *phys,
 			 size_t cnt, uint32_t flags)
 {
-	void *va = (__sparse_force void *)z_soc_cached_ptr(virt);
+	void *va = (__sparse_force void *)sys_cache_cached_ptr(virt);
 
 	return sys_mm_drv_simple_map_array(va, phys, cnt, flags);
 }
@@ -339,7 +339,7 @@ int sys_mm_drv_unmap_page(void *virt)
 	int ret = 0;
 
 	/* Use cached virtual address */
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr(virt));
 
 	/* Check bounds of virtual address space */
 	CHECKIF((va < UNUSED_L2_START_ALIGNED) ||
@@ -396,7 +396,7 @@ out:
 
 int sys_mm_drv_unmap_region(void *virt, size_t size)
 {
-	void *va = (__sparse_force void *)z_soc_cached_ptr(virt);
+	void *va = (__sparse_force void *)sys_cache_cached_ptr(virt);
 
 	return sys_mm_drv_simple_unmap_region(va, size);
 }
@@ -408,7 +408,7 @@ int sys_mm_drv_page_phys_get(void *virt, uintptr_t *phys)
 	int ret = 0;
 
 	/* Use cached address */
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr(virt));
 
 	CHECKIF(!sys_mm_drv_is_addr_aligned(va)) {
 		ret = -EINVAL;
@@ -449,7 +449,7 @@ int sys_mm_drv_page_flag_get(void *virt, uint32_t *flags)
 	uint16_t ent;
 
 	/* Use cached address */
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr(virt));
 
 	CHECKIF(!sys_mm_drv_is_addr_aligned(va)) {
 		ret = -EINVAL;
@@ -487,8 +487,8 @@ out:
 int sys_mm_drv_remap_region(void *virt_old, size_t size,
 			    void *virt_new)
 {
-	void *va_new = (__sparse_force void *)z_soc_cached_ptr(virt_new);
-	void *va_old = (__sparse_force void *)z_soc_cached_ptr(virt_old);
+	void *va_new = (__sparse_force void *)sys_cache_cached_ptr(virt_new);
+	void *va_old = (__sparse_force void *)sys_cache_cached_ptr(virt_old);
 
 	return sys_mm_drv_simple_remap_region(va_old, size, va_new);
 }
@@ -500,8 +500,8 @@ int sys_mm_drv_move_region(void *virt_old, size_t size, void *virt_new,
 	size_t offset;
 	int ret = 0;
 
-	virt_new = (__sparse_force void *)z_soc_cached_ptr(virt_new);
-	virt_old = (__sparse_force void *)z_soc_cached_ptr(virt_old);
+	virt_new = (__sparse_force void *)sys_cache_cached_ptr(virt_new);
+	virt_old = (__sparse_force void *)sys_cache_cached_ptr(virt_old);
 
 	CHECKIF(!sys_mm_drv_is_virt_addr_aligned(virt_old) ||
 		!sys_mm_drv_is_virt_addr_aligned(virt_new) ||
@@ -598,8 +598,8 @@ int sys_mm_drv_move_array(void *virt_old, size_t size, void *virt_new,
 {
 	int ret;
 
-	void *va_new = (__sparse_force void *)z_soc_cached_ptr(virt_new);
-	void *va_old = (__sparse_force void *)z_soc_cached_ptr(virt_old);
+	void *va_new = (__sparse_force void *)sys_cache_cached_ptr(virt_new);
+	void *va_old = (__sparse_force void *)sys_cache_cached_ptr(virt_old);
 
 	ret = sys_mm_drv_simple_move_array(va_old, size, va_new,
 					    phys_new, phys_cnt);
@@ -783,7 +783,7 @@ __imr void adsp_mm_restore_context(void *storage_buffer)
 
 	while (phys_addr != 0) {
 		uint32_t phys_addr_uncached =
-				POINTER_TO_UINT(z_soc_uncached_ptr(
+				POINTER_TO_UINT(sys_cache_uncached_ptr(
 					(void __sparse_cache *)UINT_TO_POINTER(phys_addr)));
 		uint32_t phys_offset = phys_addr - L2_SRAM_BASE;
 		uint32_t bank_idx = (phys_offset / SRAM_BANK_SIZE);

include/zephyr/arch/xtensa/cache.h
@@ -193,6 +193,119 @@ static ALWAYS_INLINE void arch_icache_disable(void)
 #endif /* CONFIG_ICACHE */
 
 
+/**
+ * @brief Test if a pointer is in cached region.
+ *
+ * Some hardware may map the same physical memory twice
+ * so that it can be seen in both (incoherent) cached mappings
+ * and a coherent "shared" area. This tests if a particular
+ * pointer is within the cached, coherent area.
+ *
+ * @param ptr Pointer
+ *
+ * @retval True if pointer is in cached region.
+ * @retval False if pointer is not in cached region.
+ */
+static inline bool arch_is_ptr_cached(void *ptr)
+{
+	size_t addr = (size_t) ptr;
+
+	return (addr >> 29) == CONFIG_XTENSA_CACHED_REGION;
+}
+
+/**
+ * @brief Test if a pointer is in un-cached region.
+ *
+ * Some hardware may map the same physical memory twice
+ * so that it can be seen in both (incoherent) cached mappings
+ * and a coherent "shared" area. This tests if a particular
+ * pointer is within the un-cached, incoherent area.
+ *
+ * @param ptr Pointer
+ *
+ * @retval True if pointer is not in cached region.
+ * @retval False if pointer is in cached region.
+ */
+static inline bool arch_is_ptr_uncached(void *ptr)
+{
+	size_t addr = (size_t) ptr;
+
+	return (addr >> 29) == CONFIG_XTENSA_UNCACHED_REGION;
+}
+
+static ALWAYS_INLINE uint32_t z_xtrpoflip(uint32_t addr, uint32_t rto, uint32_t rfrom)
+{
+	/* The math here is all compile-time: when the two regions
+	 * differ by a power of two, we can convert between them by
+	 * setting or clearing just one bit.  Otherwise it needs two
+	 * operations.
+	 */
+	uint32_t rxor = (rto ^ rfrom) << 29;
+
+	rto <<= 29;
+	if (Z_IS_POW2(rxor)) {
+		if ((rxor & rto) == 0) {
+			return addr & ~rxor;
+		} else {
+			return addr | rxor;
+		}
+	} else {
+		return (addr & ~(7U << 29)) | rto;
+	}
+}
+
+/**
+ * @brief Return cached pointer to a RAM address
+ *
+ * The Xtensa coherence architecture maps addressable RAM twice, in
+ * two different 512MB regions whose L1 cache settings can be
+ * controlled independently.  So for any given pointer, it is possible
+ * to convert it to and from a cached version.
+ *
+ * This function takes a pointer to any addressable object (either in
+ * cacheable memory or not) and returns a pointer that can be used to
+ * refer to the same memory through the L1 data cache.  Data read
+ * through the resulting pointer will reflect locally cached values on
+ * the current CPU if they exist, and writes will go first into the
+ * cache and be written back later.
+ *
+ * @see arch_uncached_ptr()
+ *
+ * @param ptr A pointer to a valid C object
+ * @return A pointer to the same object via the L1 dcache
+ */
+static inline void __sparse_cache *arch_cached_ptr(void *ptr)
+{
+	return (__sparse_force void __sparse_cache *)z_xtrpoflip((uint32_t) ptr,
+						CONFIG_XTENSA_CACHED_REGION,
+						CONFIG_XTENSA_UNCACHED_REGION);
+}
+
+/**
+ * @brief Return uncached pointer to a RAM address
+ *
+ * The Xtensa coherence architecture maps addressable RAM twice, in
+ * two different 512MB regions whose L1 cache settings can be
+ * controlled independently.  So for any given pointer, it is possible
+ * to convert it to and from a cached version.
+ *
+ * This function takes a pointer to any addressable object (either in
+ * cacheable memory or not) and returns a pointer that can be used to
+ * refer to the same memory while bypassing the L1 data cache.  Data
+ * in the L1 cache will not be inspected nor modified by the access.
+ *
+ * @see arch_cached_ptr()
+ *
+ * @param ptr A pointer to a valid C object
+ * @return A pointer to the same object bypassing the L1 dcache
+ */
+static inline void *arch_uncached_ptr(void __sparse_cache *ptr)
+{
+	return (void *)z_xtrpoflip((__sparse_force uint32_t)ptr,
+				   CONFIG_XTENSA_UNCACHED_REGION,
+				   CONFIG_XTENSA_CACHED_REGION);
+}
+
 
 #ifdef __cplusplus
 } /* extern "C" */

soc/xtensa/intel_adsp/cavs/power.c
@@ -20,7 +20,6 @@
 #include <adsp_clk.h>
 #include <adsp_imr_layout.h>
 #include <cavs-idc.h>
-#include "soc.h"
 
 #ifdef CONFIG_DYNAMIC_INTERRUPTS
 #include <zephyr/sw_isr_table.h>
@@ -150,7 +149,7 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 				.imr_restore_vector = rom_entry,
 			};
 			struct imr_layout *imr_layout =
-				z_soc_uncached_ptr((__sparse_force void __sparse_cache *)
+				sys_cache_uncached_ptr((__sparse_force void __sparse_cache *)
 						   L3_MEM_BASE_ADDR);
 
 			imr_layout->imr_state.header = hdr;

tests/boards/intel_adsp/smoke/src/cpus.c
@@ -84,19 +84,19 @@ static void core_smoke(void *arg)
 	zassert_equal(cpu, arch_curr_cpu()->id, "wrong cpu");
 
 	/* Un/cached regions should be configured and distinct */
-	zassert_equal(&tag, arch_xtensa_cached_ptr((void *)&tag),
+	zassert_equal(&tag, sys_cache_cached_ptr((void *)&tag),
 		      "stack memory not cached");
-	zassert_not_equal(&tag, arch_xtensa_uncached_ptr((void *)&tag),
+	zassert_not_equal(&tag, sys_cache_uncached_ptr((void *)&tag),
 			  "stack memory not cached");
-	zassert_not_equal(&static_tag, arch_xtensa_cached_ptr((void *)&static_tag),
+	zassert_not_equal(&static_tag, sys_cache_cached_ptr((void *)&static_tag),
 		      "stack memory not cached");
-	zassert_equal(&static_tag, arch_xtensa_uncached_ptr((void *)&static_tag),
+	zassert_equal(&static_tag, sys_cache_uncached_ptr((void *)&static_tag),
 			  "stack memory not cached");
 
 	/* Un/cached regions should be working */
 	printk(" Cache behavior check\n");
-	volatile int *ctag = (volatile int *)arch_xtensa_cached_ptr((void *)&tag);
-	volatile int *utag = (volatile int *)arch_xtensa_uncached_ptr((void *)&tag);
+	volatile int *ctag = (volatile int *)sys_cache_cached_ptr((void *)&tag);
+	volatile int *utag = (volatile int *)sys_cache_uncached_ptr((void *)&tag);
 
 	tag = 99;
 	zassert_true(*ctag == 99, "variable is cached");

open-vela/external_zblue

drivers/console/winstream_console.c
@@ -8,10 +8,10 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/winstream.h>
 #include <zephyr/devicetree.h>
+#include <zephyr/cache.h>
 
 #include <adsp_memory.h>
 #include <mem_window.h>
-#include <soc.h>
 
 struct k_spinlock trace_lock;
 
@@ -78,7 +78,7 @@ static int winstream_console_init(void)
 	}
 	const struct mem_win_config *config = dev->config;
 	void *buf =
-		arch_xtensa_uncached_ptr((__sparse_force void __sparse_cache *)config->mem_base);
+		sys_cache_uncached_ptr_get((__sparse_force void __sparse_cache *)config->mem_base);
 
 	winstream = sys_winstream_init(buf, config->size);
 	winstream_console_hook_install();

drivers/mm/mm_drv_intel_adsp_mtl_tlb.c
@@ -16,7 +16,7 @@
  * Note that all passed in addresses should be in cached range
  * (aka cached addresses). Due to the need to calculate TLB
  * indexes, virtual addresses will be converted internally to
- * cached one via z_soc_cached_ptr(). However, physical addresses
+ * cached one via sys_cache_cached_ptr_get(). However, physical addresses
  * are untouched.
  */
 
@@ -183,8 +183,8 @@ int sys_mm_drv_map_page(void *virt, uintptr_t phys, uint32_t flags)
 	 * the cached physical address is needed to perform
 	 * bound check.
 	 */
-	uintptr_t pa = POINTER_TO_UINT(z_soc_cached_ptr(UINT_TO_POINTER(phys)));
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t pa = POINTER_TO_UINT(sys_cache_cached_ptr_get(UINT_TO_POINTER(phys)));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr_get(virt));
 
 	ARG_UNUSED(flags);
 
@@ -215,7 +215,7 @@ int sys_mm_drv_map_page(void *virt, uintptr_t phys, uint32_t flags)
 				 "unable to assign free phys page %d\n", ret);
 			goto out;
 		}
-		pa = POINTER_TO_UINT(z_soc_cached_ptr(phys_block_ptr));
+		pa = POINTER_TO_UINT(sys_cache_cached_ptr_get(phys_block_ptr));
 	}
 
 	/* Check bounds of physical address space */
@@ -296,7 +296,7 @@ int sys_mm_drv_map_region(void *virt, uintptr_t phys,
 		goto out;
 	}
 
-	va = (__sparse_force uint8_t *)z_soc_cached_ptr(virt);
+	va = (__sparse_force uint8_t *)sys_cache_cached_ptr_get(virt);
 	pa = phys;
 
 	key = k_spin_lock(&sys_mm_drv_common_lock);
@@ -324,7 +324,7 @@ out:
 int sys_mm_drv_map_array(void *virt, uintptr_t *phys,
 			 size_t cnt, uint32_t flags)
 {
-	void *va = (__sparse_force void *)z_soc_cached_ptr(virt);
+	void *va = (__sparse_force void *)sys_cache_cached_ptr_get(virt);
 
 	return sys_mm_drv_simple_map_array(va, phys, cnt, flags);
 }
@@ -339,7 +339,7 @@ int sys_mm_drv_unmap_page(void *virt)
 	int ret = 0;
 
 	/* Use cached virtual address */
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr_get(virt));
 
 	/* Check bounds of virtual address space */
 	CHECKIF((va < UNUSED_L2_START_ALIGNED) ||
@@ -396,7 +396,7 @@ out:
 
 int sys_mm_drv_unmap_region(void *virt, size_t size)
 {
-	void *va = (__sparse_force void *)z_soc_cached_ptr(virt);
+	void *va = (__sparse_force void *)sys_cache_cached_ptr_get(virt);
 
 	return sys_mm_drv_simple_unmap_region(va, size);
 }
@@ -408,7 +408,7 @@ int sys_mm_drv_page_phys_get(void *virt, uintptr_t *phys)
 	int ret = 0;
 
 	/* Use cached address */
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr_get(virt));
 
 	CHECKIF(!sys_mm_drv_is_addr_aligned(va)) {
 		ret = -EINVAL;
@@ -449,7 +449,7 @@ int sys_mm_drv_page_flag_get(void *virt, uint32_t *flags)
 	uint16_t ent;
 
 	/* Use cached address */
-	uintptr_t va = POINTER_TO_UINT(z_soc_cached_ptr(virt));
+	uintptr_t va = POINTER_TO_UINT(sys_cache_cached_ptr_get(virt));
 
 	CHECKIF(!sys_mm_drv_is_addr_aligned(va)) {
 		ret = -EINVAL;
@@ -487,8 +487,8 @@ out:
 int sys_mm_drv_remap_region(void *virt_old, size_t size,
 			    void *virt_new)
 {
-	void *va_new = (__sparse_force void *)z_soc_cached_ptr(virt_new);
-	void *va_old = (__sparse_force void *)z_soc_cached_ptr(virt_old);
+	void *va_new = (__sparse_force void *)sys_cache_cached_ptr_get(virt_new);
+	void *va_old = (__sparse_force void *)sys_cache_cached_ptr_get(virt_old);
 
 	return sys_mm_drv_simple_remap_region(va_old, size, va_new);
 }
@@ -500,8 +500,8 @@ int sys_mm_drv_move_region(void *virt_old, size_t size, void *virt_new,
 	size_t offset;
 	int ret = 0;
 
-	virt_new = (__sparse_force void *)z_soc_cached_ptr(virt_new);
-	virt_old = (__sparse_force void *)z_soc_cached_ptr(virt_old);
+	virt_new = (__sparse_force void *)sys_cache_cached_ptr_get(virt_new);
+	virt_old = (__sparse_force void *)sys_cache_cached_ptr_get(virt_old);
 
 	CHECKIF(!sys_mm_drv_is_virt_addr_aligned(virt_old) ||
 		!sys_mm_drv_is_virt_addr_aligned(virt_new) ||
@@ -598,8 +598,8 @@ int sys_mm_drv_move_array(void *virt_old, size_t size, void *virt_new,
 {
 	int ret;
 
-	void *va_new = (__sparse_force void *)z_soc_cached_ptr(virt_new);
-	void *va_old = (__sparse_force void *)z_soc_cached_ptr(virt_old);
+	void *va_new = (__sparse_force void *)sys_cache_cached_ptr_get(virt_new);
+	void *va_old = (__sparse_force void *)sys_cache_cached_ptr_get(virt_old);
 
 	ret = sys_mm_drv_simple_move_array(va_old, size, va_new,
 					    phys_new, phys_cnt);
@@ -783,7 +783,7 @@ __imr void adsp_mm_restore_context(void *storage_buffer)
 
 	while (phys_addr != 0) {
 		uint32_t phys_addr_uncached =
-				POINTER_TO_UINT(z_soc_uncached_ptr(
+				POINTER_TO_UINT(sys_cache_uncached_ptr_get(
 					(void __sparse_cache *)UINT_TO_POINTER(phys_addr)));
 		uint32_t phys_offset = phys_addr - L2_SRAM_BASE;
 		uint32_t bank_idx = (phys_offset / SRAM_BANK_SIZE);

include/zephyr/arch/xtensa/cache.h
@@ -192,6 +192,144 @@ static ALWAYS_INLINE void arch_icache_disable(void)
 
 #endif /* CONFIG_ICACHE */
 
+#if defined(CONFIG_CACHE_DOUBLEMAP)
+/**
+ * @brief Test if a pointer is in cached region.
+ *
+ * Some hardware may map the same physical memory twice
+ * so that it can be seen in both (incoherent) cached mappings
+ * and a coherent "shared" area. This tests if a particular
+ * pointer is within the cached, coherent area.
+ *
+ * @param ptr Pointer
+ *
+ * @retval True if pointer is in cached region.
+ * @retval False if pointer is not in cached region.
+ */
+static inline bool arch_cache_is_ptr_cached(void *ptr)
+{
+	size_t addr = (size_t) ptr;
+
+	return (addr >> 29) == CONFIG_XTENSA_CACHED_REGION;
+}
+
+/**
+ * @brief Test if a pointer is in un-cached region.
+ *
+ * Some hardware may map the same physical memory twice
+ * so that it can be seen in both (incoherent) cached mappings
+ * and a coherent "shared" area. This tests if a particular
+ * pointer is within the un-cached, incoherent area.
+ *
+ * @param ptr Pointer
+ *
+ * @retval True if pointer is not in cached region.
+ * @retval False if pointer is in cached region.
+ */
+static inline bool arch_cache_is_ptr_uncached(void *ptr)
+{
+	size_t addr = (size_t) ptr;
+
+	return (addr >> 29) == CONFIG_XTENSA_UNCACHED_REGION;
+}
+
+static ALWAYS_INLINE uint32_t z_xtrpoflip(uint32_t addr, uint32_t rto, uint32_t rfrom)
+{
+	/* The math here is all compile-time: when the two regions
+	 * differ by a power of two, we can convert between them by
+	 * setting or clearing just one bit.  Otherwise it needs two
+	 * operations.
+	 */
+	uint32_t rxor = (rto ^ rfrom) << 29;
+
+	rto <<= 29;
+	if (Z_IS_POW2(rxor)) {
+		if ((rxor & rto) == 0) {
+			return addr & ~rxor;
+		} else {
+			return addr | rxor;
+		}
+	} else {
+		return (addr & ~(7U << 29)) | rto;
+	}
+}
+
+/**
+ * @brief Return cached pointer to a RAM address
+ *
+ * The Xtensa coherence architecture maps addressable RAM twice, in
+ * two different 512MB regions whose L1 cache settings can be
+ * controlled independently.  So for any given pointer, it is possible
+ * to convert it to and from a cached version.
+ *
+ * This function takes a pointer to any addressable object (either in
+ * cacheable memory or not) and returns a pointer that can be used to
+ * refer to the same memory through the L1 data cache.  Data read
+ * through the resulting pointer will reflect locally cached values on
+ * the current CPU if they exist, and writes will go first into the
+ * cache and be written back later.
+ *
+ * @see arch_uncached_ptr()
+ *
+ * @param ptr A pointer to a valid C object
+ * @return A pointer to the same object via the L1 dcache
+ */
+static inline void __sparse_cache *arch_cache_cached_ptr_get(void *ptr)
+{
+	return (__sparse_force void __sparse_cache *)z_xtrpoflip((uint32_t) ptr,
+						CONFIG_XTENSA_CACHED_REGION,
+						CONFIG_XTENSA_UNCACHED_REGION);
+}
+
+/**
+ * @brief Return uncached pointer to a RAM address
+ *
+ * The Xtensa coherence architecture maps addressable RAM twice, in
+ * two different 512MB regions whose L1 cache settings can be
+ * controlled independently.  So for any given pointer, it is possible
+ * to convert it to and from a cached version.
+ *
+ * This function takes a pointer to any addressable object (either in
+ * cacheable memory or not) and returns a pointer that can be used to
+ * refer to the same memory while bypassing the L1 data cache.  Data
+ * in the L1 cache will not be inspected nor modified by the access.
+ *
+ * @see arch_cached_ptr()
+ *
+ * @param ptr A pointer to a valid C object
+ * @return A pointer to the same object bypassing the L1 dcache
+ */
+static inline void *arch_cache_uncached_ptr_get(void __sparse_cache *ptr)
+{
+	return (void *)z_xtrpoflip((__sparse_force uint32_t)ptr,
+				   CONFIG_XTENSA_UNCACHED_REGION,
+				   CONFIG_XTENSA_CACHED_REGION);
+}
+#else
+static inline bool arch_cache_is_ptr_cached(void *ptr)
+{
+	ARG_UNUSED(ptr);
+
+	return false;
+}
+
+static inline bool arch_cache_is_ptr_uncached(void *ptr)
+{
+	ARG_UNUSED(ptr);
+
+	return false;
+}
+
+static inline void *arch_cache_cached_ptr_get(void *ptr)
+{
+	return ptr;
+}
+
+static inline void *arch_cache_uncached_ptr_get(void *ptr)
+{
+	return ptr;
+}
+#endif
 
 
 #ifdef __cplusplus

soc/xtensa/intel_adsp/cavs/power.c
@@ -20,7 +20,6 @@
 #include <adsp_clk.h>
 #include <adsp_imr_layout.h>
 #include <cavs-idc.h>
-#include "soc.h"
 
 #ifdef CONFIG_DYNAMIC_INTERRUPTS
 #include <zephyr/sw_isr_table.h>
@@ -150,7 +149,7 @@ void pm_state_set(enum pm_state state, uint8_t substate_id)
 				.imr_restore_vector = rom_entry,
 			};
 			struct imr_layout *imr_layout =
-				z_soc_uncached_ptr((__sparse_force void __sparse_cache *)
+				sys_cache_uncached_ptr_get((__sparse_force void __sparse_cache *)
 						   L3_MEM_BASE_ADDR);
 
 			imr_layout->imr_state.header = hdr;

tests/boards/intel_adsp/smoke/src/cpus.c
@@ -84,19 +84,19 @@ static void core_smoke(void *arg)
 	zassert_equal(cpu, arch_curr_cpu()->id, "wrong cpu");
 
 	/* Un/cached regions should be configured and distinct */
-	zassert_equal(&tag, arch_xtensa_cached_ptr((void *)&tag),
+	zassert_equal(&tag, sys_cache_cached_ptr_get((void *)&tag),
 		      "stack memory not cached");
-	zassert_not_equal(&tag, arch_xtensa_uncached_ptr((void *)&tag),
+	zassert_not_equal(&tag, sys_cache_uncached_ptr_get((void *)&tag),
 			  "stack memory not cached");
-	zassert_not_equal(&static_tag, arch_xtensa_cached_ptr((void *)&static_tag),
+	zassert_not_equal(&static_tag, sys_cache_cached_ptr_get((void *)&static_tag),
 		      "stack memory not cached");
-	zassert_equal(&static_tag, arch_xtensa_uncached_ptr((void *)&static_tag),
+	zassert_equal(&static_tag, sys_cache_uncached_ptr_get((void *)&static_tag),
 			  "stack memory not cached");
 
 	/* Un/cached regions should be working */
 	printk(" Cache behavior check\n");
-	volatile int *ctag = (volatile int *)arch_xtensa_cached_ptr((void *)&tag);
-	volatile int *utag = (volatile int *)arch_xtensa_uncached_ptr((void *)&tag);
+	volatile int *ctag = (volatile int *)sys_cache_cached_ptr_get((void *)&tag);
+	volatile int *utag = (volatile int *)sys_cache_uncached_ptr_get((void *)&tag);
 
 	tag = 99;
 	zassert_true(*ctag == 99, "variable is cached");