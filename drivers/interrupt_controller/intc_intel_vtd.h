zephyrproject-rtos/zephyr

drivers/interrupt_controller: Generate proper MSI address on VT-D
SHV bit depends on the number of vectors allocated.
If it's facing a multi-vector MSI array, it will set the bit.
If not the bit must be 0.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

drivers/interrupt_controller/intc_intel_vtd.h
@@ -12,8 +12,8 @@
 /* We don't care about int_idx[15], since the size is fixed to 256,
  * it's always 0
  */
-#define VTD_MSI_MAP(int_idx) \
-	((0x0FEE << 20) | (int_idx << 5) | VTD_INT_SHV | VTD_INT_FORMAT)
+#define VTD_MSI_MAP(int_idx, shv)					\
+	((0xFEE00000U) | (int_idx << 5) | shv | VTD_INT_FORMAT)
 
 /* Interrupt Remapping Table Entry (IRTE) for Remapped Interrupts */
 union vtd_irte {

open-vela/external_zblue

drivers/interrupt_controller/intc_intel_vtd.h
@@ -12,8 +12,8 @@
 /* We don't care about int_idx[15], since the size is fixed to 256,
  * it's always 0
  */
-#define VTD_MSI_MAP(int_idx) \
-	((0x0FEE << 20) | (int_idx << 5) | VTD_INT_SHV | VTD_INT_FORMAT)
+#define VTD_MSI_MAP(int_idx, shv)					\
+	((0x0FEE00000U) | (int_idx << 5) | shv | VTD_INT_FORMAT)
 
 /* Interrupt Remapping Table Entry (IRTE) for Remapped Interrupts */
 struct vtd_irte {