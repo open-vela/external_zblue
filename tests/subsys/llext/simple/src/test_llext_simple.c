zephyrproject-rtos/zephyr

llext: add a relocatable extension test to Xtensa
LLEXT on Xtensa now supports both shared and relocatable (partially
linked) extensions. This commit adds a copy of the LLEXT test for the
relocatable case.

Signed-off-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>

tests/subsys/llext/simple/src/test_llext_simple.c
@@ -174,10 +174,12 @@ static LLEXT_CONST uint8_t object_ext[] __aligned(4) = {
 };
 LLEXT_LOAD_UNLOAD(object, true)
 
+#ifndef CONFIG_LLEXT_RELOCATABLE
 static LLEXT_CONST uint8_t syscalls_ext[] __aligned(4) = {
 	#include "syscalls.inc"
 };
 LLEXT_LOAD_UNLOAD(syscalls, true)
+#endif
 #endif /* ! LOADER_BUILD_ONLY */
 
 /*

open-vela/external_zblue

Signed-off-by: Luca Burelli <l.burelli@arduino.cc>

tests/subsys/llext/simple/src/test_llext_simple.c
@@ -219,6 +219,7 @@ static LLEXT_CONST uint8_t object_ext[] __aligned(4) = {
 };
 LLEXT_LOAD_UNLOAD(object, true, NULL)
 
+#ifndef CONFIG_LLEXT_TYPE_ELF_RELOCATABLE
 static LLEXT_CONST uint8_t syscalls_ext[] __aligned(4) = {
 	#include "syscalls.inc"
 };
@@ -228,6 +229,7 @@ static LLEXT_CONST uint8_t threads_kernel_objects_ext[] __aligned(4) = {
 	#include "threads_kernel_objects.inc"
 };
 LLEXT_LOAD_UNLOAD(threads_kernel_objects, true, threads_objects_perm_setup)
+#endif
 #endif /* ! LOADER_BUILD_ONLY */
 
 