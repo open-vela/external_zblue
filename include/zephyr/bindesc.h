dztang/zephyr

bindesc: Add support for the build version values
This patch adds bindesc support for the build version values for the
kernel and application - BUILD_VERSION and APP_BUILD_VERSION.

The kernel's BUILD_VERSION can be overridden at build time.

Signed-off-by: Attie Grande <attie.grande@argentum-systems.co.uk>

include/zephyr/bindesc.h
@@ -48,6 +48,9 @@ extern "C" {
 /** The app version number such as 0x10203 */
 #define BINDESC_ID_APP_VERSION_NUMBER 0x804
 
+/** The app git reference such as "v3.3.0-18-g2c85d9224fca" */
+#define BINDESC_ID_APP_BUILD_VERSION 0x805
+
 /** The kernel version string such as "3.4.0" */
 #define BINDESC_ID_KERNEL_VERSION_STRING 0x900
 
@@ -62,6 +65,9 @@ extern "C" {
 
 /** The kernel version number such as 0x30400 */
 #define BINDESC_ID_KERNEL_VERSION_NUMBER 0x904
+
+/** The kernel git reference such as "v3.3.0-18-g2c85d9224fca" */
+#define BINDESC_ID_KERNEL_BUILD_VERSION 0x905
 
 /** The year the image was compiled in */
 #define BINDESC_ID_BUILD_TIME_YEAR 0xa00
@@ -309,6 +315,10 @@ extern const struct bindesc_entry BINDESC_NAME(kernel_version_patchlevel);
 extern const struct bindesc_entry BINDESC_NAME(kernel_version_number);
 #endif /* IS_ENABLED(CONFIG_BINDESC_KERNEL_VERSION_NUMBER) */
 
+#if IS_ENABLED(CONFIG_BINDESC_KERNEL_BUILD_VERSION)
+extern const struct bindesc_entry BINDESC_NAME(kernel_build_version);
+#endif /* IS_ENABLED(CONFIG_BINDESC_KERNEL_BUILD_VERSION) */
+
 #if IS_ENABLED(CONFIG_BINDESC_APP_VERSION_STRING)
 extern const struct bindesc_entry BINDESC_NAME(app_version_string);
 #endif /* IS_ENABLED(CONFIG_BINDESC_APP_VERSION_STRING) */
@@ -328,6 +338,10 @@ extern const struct bindesc_entry BINDESC_NAME(app_version_patchlevel);
 #if IS_ENABLED(CONFIG_BINDESC_APP_VERSION_NUMBER)
 extern const struct bindesc_entry BINDESC_NAME(app_version_number);
 #endif /* IS_ENABLED(CONFIG_BINDESC_APP_VERSION_NUMBER) */
+
+#if IS_ENABLED(CONFIG_BINDESC_APP_BUILD_VERSION)
+extern const struct bindesc_entry BINDESC_NAME(app_build_version);
+#endif /* IS_ENABLED(CONFIG_BINDESC_APP_BUILD_VERSION) */
 
 #if IS_ENABLED(CONFIG_BINDESC_BUILD_TIME_YEAR)
 extern const struct bindesc_entry BINDESC_NAME(build_time_year);

subsys/bindesc/bindesc_version.c
@@ -33,6 +33,11 @@ BINDESC_UINT_DEFINE(kernel_version_number, BINDESC_ID_KERNEL_VERSION_NUMBER,
 			KERNEL_VERSION_NUMBER);
 #endif /* IS_ENABLED(CONFIG_BINDESC_KERNEL_VERSION_NUMBER) */
 
+#if IS_ENABLED(CONFIG_BINDESC_KERNEL_BUILD_VERSION)
+BINDESC_STR_DEFINE(kernel_build_version, BINDESC_ID_KERNEL_BUILD_VERSION,
+			STRINGIFY(BUILD_VERSION));
+#endif /* CONFIG_BINDESC_KERNEL_BUILD_VERSION */
+
 #if IS_ENABLED(HAS_APP_VERSION)
 #include <app_version.h>
 
@@ -60,5 +65,10 @@ BINDESC_UINT_DEFINE(app_version_patchlevel, BINDESC_ID_APP_VERSION_PATCHLEVEL,
 BINDESC_UINT_DEFINE(app_version_number, BINDESC_ID_APP_VERSION_NUMBER,
 			APP_VERSION_NUMBER);
 #endif /* IS_ENABLED(CONFIG_BINDESC_APP_VERSION_NUMBER) */
+
+#if IS_ENABLED(CONFIG_BINDESC_APP_BUILD_VERSION)
+BINDESC_STR_DEFINE(app_build_version, BINDESC_ID_APP_BUILD_VERSION,
+			STRINGIFY(APP_BUILD_VERSION));
+#endif /* CONFIG_BINDESC_APP_BUILD_VERSION */
 
 #endif /* IS_ENABLED(HAS_APP_VERSION) */

open-vela/external_zblue

include/zephyr/bindesc.h
@@ -51,6 +51,9 @@ extern "C" {
 /** The app version number such as 0x10203 */
 #define BINDESC_ID_APP_VERSION_NUMBER 0x804
 
+/** The app git reference such as "v3.3.0-18-g2c85d9224fca" */
+#define BINDESC_ID_APP_BUILD_VERSION 0x805
+
 /** The kernel version string such as "3.4.0" */
 #define BINDESC_ID_KERNEL_VERSION_STRING 0x900
 
@@ -65,6 +68,9 @@ extern "C" {
 
 /** The kernel version number such as 0x30400 */
 #define BINDESC_ID_KERNEL_VERSION_NUMBER 0x904
+
+/** The kernel git reference such as "v3.3.0-18-g2c85d9224fca" */
+#define BINDESC_ID_KERNEL_BUILD_VERSION 0x905
 
 /** The year the image was compiled in */
 #define BINDESC_ID_BUILD_TIME_YEAR 0xa00
@@ -308,6 +314,10 @@ extern const struct bindesc_entry BINDESC_NAME(kernel_version_patchlevel);
 extern const struct bindesc_entry BINDESC_NAME(kernel_version_number);
 #endif /* defined(CONFIG_BINDESC_KERNEL_VERSION_NUMBER) */
 
+#if defined(CONFIG_BINDESC_KERNEL_BUILD_VERSION)
+extern const struct bindesc_entry BINDESC_NAME(kernel_build_version);
+#endif /* defined(CONFIG_BINDESC_KERNEL_BUILD_VERSION) */
+
 #if defined(CONFIG_BINDESC_APP_VERSION_STRING)
 extern const struct bindesc_entry BINDESC_NAME(app_version_string);
 #endif /* defined(CONFIG_BINDESC_APP_VERSION_STRING) */
@@ -327,6 +337,10 @@ extern const struct bindesc_entry BINDESC_NAME(app_version_patchlevel);
 #if defined(CONFIG_BINDESC_APP_VERSION_NUMBER)
 extern const struct bindesc_entry BINDESC_NAME(app_version_number);
 #endif /* defined(CONFIG_BINDESC_APP_VERSION_NUMBER) */
+
+#if defined(CONFIG_BINDESC_APP_BUILD_VERSION)
+extern const struct bindesc_entry BINDESC_NAME(app_build_version);
+#endif /* defined(CONFIG_BINDESC_APP_BUILD_VERSION) */
 
 #if defined(CONFIG_BINDESC_BUILD_TIME_YEAR)
 extern const struct bindesc_entry BINDESC_NAME(build_time_year);

subsys/bindesc/bindesc_version.c
@@ -33,6 +33,11 @@ BINDESC_UINT_DEFINE(kernel_version_number, BINDESC_ID_KERNEL_VERSION_NUMBER,
 			KERNEL_VERSION_NUMBER);
 #endif /* defined(CONFIG_BINDESC_KERNEL_VERSION_NUMBER) */
 
+#if defined(CONFIG_BINDESC_KERNEL_BUILD_VERSION)
+BINDESC_STR_DEFINE(kernel_build_version, BINDESC_ID_KERNEL_BUILD_VERSION,
+			STRINGIFY(BUILD_VERSION));
+#endif /* CONFIG_BINDESC_KERNEL_BUILD_VERSION */
+
 #if defined(HAS_APP_VERSION)
 #include <zephyr/app_version.h>
 
@@ -60,5 +65,10 @@ BINDESC_UINT_DEFINE(app_version_patchlevel, BINDESC_ID_APP_VERSION_PATCHLEVEL,
 BINDESC_UINT_DEFINE(app_version_number, BINDESC_ID_APP_VERSION_NUMBER,
 			APP_VERSION_NUMBER);
 #endif /* defined(CONFIG_BINDESC_APP_VERSION_NUMBER) */
+
+#if defined(CONFIG_BINDESC_APP_BUILD_VERSION)
+BINDESC_STR_DEFINE(app_build_version, BINDESC_ID_APP_BUILD_VERSION,
+			STRINGIFY(APP_BUILD_VERSION));
+#endif /* CONFIG_BINDESC_APP_BUILD_VERSION */
 
 #endif /* defined(HAS_APP_VERSION) */