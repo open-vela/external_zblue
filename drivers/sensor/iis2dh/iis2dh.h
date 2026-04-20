avisconti/zephyr

drivers/sensors: Add iis2dh accelerometer sensor support
Add support to STM IIS2DH the ultra-low-power highperformance
three-axis linear accelerometer.

Signed-off-by: Armando Visconti <armando.visconti@st.com>

drivers/sensor/iis2dh/iis2dh.h
@@ -1,0 +1,96 @@
+/* ST Microelectronics IIS2DH 3-axis accelerometer driver
+ *
+ * Copyright (c) 2020 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Datasheet:
+ * https://www.st.com/resource/en/datasheet/iis2dh.pdf
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_IIS2DH_IIS2DH_H_
+#define ZEPHYR_DRIVERS_SENSOR_IIS2DH_IIS2DH_H_
+
+#include <drivers/spi.h>
+#include <drivers/gpio.h>
+#include <sys/util.h>
+#include <drivers/sensor.h>
+#include "iis2dh_reg.h"
+
+union axis3bit16_t {
+	s16_t i16bit[3];
+	u8_t u8bit[6];
+};
+
+/*
+ * Return ODR reg value based on data rate set
+ */
+#define IIS2DH_ODR_TO_REG_HR(_lp, _odr) \
+	((_odr == 0) ? IIS2DH_POWER_DOWN : \
+	((_odr < 10) ? IIS2DH_ODR_1Hz : \
+	((_odr < 25) ? IIS2DH_ODR_10Hz : \
+	((_lp == IIS2DH_LP_8bit) && (_odr >= 5376) ? IIS2DH_ODR_5kHz376_LP_1kHz344_NM_HP : \
+	((_lp != IIS2DH_LP_8bit) && (_odr >= 1344) ? IIS2DH_ODR_5kHz376_LP_1kHz344_NM_HP : \
+	((_lp == IIS2DH_LP_8bit) && (_odr >= 1600) ? IIS2DH_ODR_1kHz620_LP : \
+	((_lp != IIS2DH_LP_8bit) && (_odr >= 800) ? IIS2DH_ODR_400Hz : \
+	((31 - __builtin_clz(_odr / 25))) + 3)))))))
+
+/* FS reg value from Full Scale */
+#define IIS2DH_FS_TO_REG(_fs)	(30 - __builtin_clz(_fs))
+
+/**
+ * struct iis2dh_device_config - iis2dh hw configuration
+ * @bus_name: Pointer to bus master identifier.
+ * @pm: Power mode (lis2dh_powermode).
+ * @int_gpio_port: Pointer to GPIO PORT identifier.
+ * @int_gpio_pin: GPIO pin number connecter to sensor int pin.
+ * @int_pin: Sensor int pin (int1/int2).
+ */
+struct iis2dh_device_config {
+	const char *bus_name;
+	u8_t pm;
+#ifdef CONFIG_IIS2DH_TRIGGER
+	const char *int_gpio_port;
+	u8_t int_gpio_pin;
+	u8_t int_gpio_flags;
+	u8_t int_pin;
+#endif /* CONFIG_IIS2DH_TRIGGER */
+};
+
+/* sensor data */
+struct iis2dh_data {
+	struct device *bus;
+	s16_t acc[3];
+	u32_t gain;
+
+	stmdev_ctx_t *ctx;
+#ifdef CONFIG_IIS2DH_TRIGGER
+	struct device *gpio;
+	u8_t gpio_pin;
+	struct gpio_callback gpio_cb;
+	sensor_trigger_handler_t drdy_handler;
+#if defined(CONFIG_IIS2DH_TRIGGER_OWN_THREAD)
+	K_THREAD_STACK_MEMBER(thread_stack, CONFIG_IIS2DH_THREAD_STACK_SIZE);
+	struct k_thread thread;
+	struct k_sem gpio_sem;
+#elif defined(CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD)
+	struct k_work work;
+	struct device *dev;
+#endif /* CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD */
+#endif /* CONFIG_IIS2DH_TRIGGER */
+#if defined(DT_INST_0_ST_IIS2DH_CS_GPIOS_CONTROLLER)
+	struct spi_cs_control cs_ctrl;
+#endif
+};
+
+int iis2dh_i2c_init(struct device *dev);
+int iis2dh_spi_init(struct device *dev);
+
+#ifdef CONFIG_IIS2DH_TRIGGER
+int iis2dh_init_interrupt(struct device *dev);
+int iis2dh_trigger_set(struct device *dev,
+			  const struct sensor_trigger *trig,
+			  sensor_trigger_handler_t handler);
+#endif /* CONFIG_IIS2DH_TRIGGER */
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_IIS2DH_IIS2DH_H_ */

drivers/sensor/iis2dh/iis2dh_trigger.c
@@ -1,0 +1,191 @@
+/* ST Microelectronics IIS2DH 3-axis accelerometer driver
+ *
+ * Copyright (c) 2020 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Datasheet:
+ * https://www.st.com/resource/en/datasheet/iis2dh.pdf
+ */
+
+#include <kernel.h>
+#include <drivers/sensor.h>
+#include <drivers/gpio.h>
+#include <logging/log.h>
+
+#include "iis2dh.h"
+
+LOG_MODULE_DECLARE(IIS2DH, CONFIG_SENSOR_LOG_LEVEL);
+
+/**
+ * iis2dh_enable_int - enable selected int pin to generate interrupt
+ */
+static int iis2dh_enable_drdy(struct device *dev,
+			      enum sensor_trigger_type type, int enable)
+{
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	iis2dh_ctrl_reg3_t reg3;
+
+	/* set interrupt for pin INT1 */
+	iis2dh_pin_int1_config_get(iis2dh->ctx, &reg3);
+
+	reg3.i1_drdy1 = enable;
+
+	return iis2dh_pin_int1_config_set(iis2dh->ctx, &reg3);
+}
+
+/**
+ * iis2dh_trigger_set - link external trigger to event data ready
+ */
+int iis2dh_trigger_set(struct device *dev,
+		       const struct sensor_trigger *trig,
+		       sensor_trigger_handler_t handler)
+{
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	union axis3bit16_t raw;
+	int state = (handler != NULL) ? PROPERTY_ENABLE : PROPERTY_DISABLE;
+
+	switch (trig->type) {
+	case SENSOR_TRIG_DATA_READY:
+		iis2dh->drdy_handler = handler;
+		if (state) {
+			/* dummy read: re-trigger interrupt */
+			iis2dh_acceleration_raw_get(iis2dh->ctx, raw.u8bit);
+		}
+		return iis2dh_enable_drdy(dev, SENSOR_TRIG_DATA_READY, state);
+	default:
+		LOG_ERR("Unsupported sensor trigger");
+		return -ENOTSUP;
+	}
+}
+
+static int iis2dh_handle_drdy_int(struct device *dev)
+{
+	struct iis2dh_data *data = dev->driver_data;
+
+	struct sensor_trigger drdy_trig = {
+		.type = SENSOR_TRIG_DATA_READY,
+		.chan = SENSOR_CHAN_ALL,
+	};
+
+	if (data->drdy_handler) {
+		data->drdy_handler(dev, &drdy_trig);
+	}
+
+	return 0;
+}
+
+/**
+ * iis2dh_handle_interrupt - handle the drdy event
+ * read data and call handler if registered any
+ */
+static void iis2dh_handle_interrupt(void *arg)
+{
+	struct device *dev = (struct device *)arg;
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	const struct iis2dh_device_config *cfg = dev->config->config_info;
+
+	iis2dh_handle_drdy_int(dev);
+
+	gpio_pin_interrupt_configure(iis2dh->gpio, cfg->int_gpio_pin,
+				     GPIO_INT_EDGE_TO_ACTIVE);
+}
+
+static void iis2dh_gpio_callback(struct device *dev,
+				 struct gpio_callback *cb, u32_t pins)
+{
+	struct iis2dh_data *iis2dh =
+		CONTAINER_OF(cb, struct iis2dh_data, gpio_cb);
+
+	if ((pins & BIT(iis2dh->gpio_pin)) == 0U) {
+		return;
+	}
+
+	gpio_pin_interrupt_configure(dev, iis2dh->gpio_pin,
+				     GPIO_INT_DISABLE);
+
+#if defined(CONFIG_IIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_give(&iis2dh->gpio_sem);
+#elif defined(CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD)
+	k_work_submit(&iis2dh->work);
+#endif /* CONFIG_IIS2DH_TRIGGER_OWN_THREAD */
+}
+
+#ifdef CONFIG_IIS2DH_TRIGGER_OWN_THREAD
+static void iis2dh_thread(int dev_ptr, int unused)
+{
+	struct device *dev = INT_TO_POINTER(dev_ptr);
+	struct iis2dh_data *iis2dh = dev->driver_data;
+
+	ARG_UNUSED(unused);
+
+	while (1) {
+		k_sem_take(&iis2dh->gpio_sem, K_FOREVER);
+		iis2dh_handle_interrupt(dev);
+	}
+}
+#endif /* CONFIG_IIS2DH_TRIGGER_OWN_THREAD */
+
+#ifdef CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD
+static void iis2dh_work_cb(struct k_work *work)
+{
+	struct iis2dh_data *iis2dh =
+		CONTAINER_OF(work, struct iis2dh_data, work);
+
+	iis2dh_handle_interrupt(iis2dh->dev);
+}
+#endif /* CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD */
+
+int iis2dh_init_interrupt(struct device *dev)
+{
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	const struct iis2dh_device_config *cfg = dev->config->config_info;
+	int ret;
+
+	/* setup data ready gpio interrupt */
+	iis2dh->gpio = device_get_binding(cfg->int_gpio_port);
+	if (iis2dh->gpio == NULL) {
+		LOG_DBG("Cannot get pointer to %s device",
+			    cfg->int_gpio_port);
+		return -EINVAL;
+	}
+
+#if defined(CONFIG_IIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_init(&iis2dh->gpio_sem, 0, UINT_MAX);
+
+	k_thread_create(&iis2dh->thread, iis2dh->thread_stack,
+		       CONFIG_IIS2DH_THREAD_STACK_SIZE,
+		       (k_thread_entry_t)iis2dh_thread, dev,
+		       0, NULL, K_PRIO_COOP(CONFIG_IIS2DH_THREAD_PRIORITY),
+		       0, K_NO_WAIT);
+#elif defined(CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD)
+	iis2dh->work.handler = iis2dh_work_cb;
+	iis2dh->dev = dev;
+#endif /* CONFIG_IIS2DH_TRIGGER_OWN_THREAD */
+
+	iis2dh->gpio_pin = cfg->int_gpio_pin;
+
+	ret = gpio_pin_configure(iis2dh->gpio, cfg->int_gpio_pin,
+				 GPIO_INPUT | cfg->int_gpio_flags);
+	if (ret < 0) {
+		LOG_DBG("Could not configure gpio");
+		return ret;
+	}
+
+	gpio_init_callback(&iis2dh->gpio_cb,
+			   iis2dh_gpio_callback,
+			   BIT(cfg->int_gpio_pin));
+
+	if (gpio_add_callback(iis2dh->gpio, &iis2dh->gpio_cb) < 0) {
+		LOG_DBG("Could not set gpio callback");
+		return -EIO;
+	}
+
+	/* enable drdy on int1 in pulse mode */
+	if (iis2dh_int1_pin_notification_mode_set(iis2dh->ctx, IIS2DH_INT1_PULSED)) {
+		return -EIO;
+	}
+
+	return gpio_pin_interrupt_configure(iis2dh->gpio, cfg->int_gpio_pin,
+					    GPIO_INT_EDGE_TO_ACTIVE);
+}

open-vela/external_zblue

drivers/sensor/iis2dh/iis2dh.h
@@ -1,0 +1,95 @@
+/* ST Microelectronics IIS2DH 3-axis accelerometer driver
+ *
+ * Copyright (c) 2020 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Datasheet:
+ * https://www.st.com/resource/en/datasheet/iis2dh.pdf
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_IIS2DH_IIS2DH_H_
+#define ZEPHYR_DRIVERS_SENSOR_IIS2DH_IIS2DH_H_
+
+#include <drivers/spi.h>
+#include <drivers/gpio.h>
+#include <sys/util.h>
+#include <drivers/sensor.h>
+#include "iis2dh_reg.h"
+
+union axis3bit16_t {
+	int16_t i16bit[3];
+	uint8_t u8bit[6];
+};
+
+/*
+ * Return ODR reg value based on data rate set
+ */
+#define IIS2DH_ODR_TO_REG_HR(_lp, _odr) \
+	((_odr == 0) ? IIS2DH_POWER_DOWN : \
+	((_odr < 10) ? IIS2DH_ODR_1Hz : \
+	((_odr < 25) ? IIS2DH_ODR_10Hz : \
+	((_lp == IIS2DH_LP_8bit) && (_odr >= 5376) ? IIS2DH_ODR_5kHz376_LP_1kHz344_NM_HP : \
+	((_lp != IIS2DH_LP_8bit) && (_odr >= 1344) ? IIS2DH_ODR_5kHz376_LP_1kHz344_NM_HP : \
+	((_lp == IIS2DH_LP_8bit) && (_odr >= 1600) ? IIS2DH_ODR_1kHz620_LP : \
+	((_lp != IIS2DH_LP_8bit) && (_odr >= 800) ? IIS2DH_ODR_400Hz : \
+	((31 - __builtin_clz(_odr / 25))) + 3)))))))
+
+/* FS reg value from Full Scale */
+#define IIS2DH_FS_TO_REG(_fs)	(30 - __builtin_clz(_fs))
+
+/**
+ * struct iis2dh_device_config - iis2dh hw configuration
+ * @bus_name: Pointer to bus master identifier.
+ * @pm: Power mode (lis2dh_powermode).
+ * @int_gpio_port: Pointer to GPIO PORT identifier.
+ * @int_gpio_pin: GPIO pin number connecter to sensor int pin.
+ */
+struct iis2dh_device_config {
+	const char *bus_name;
+	uint8_t pm;
+#ifdef CONFIG_IIS2DH_TRIGGER
+	const char *int_gpio_port;
+	uint8_t int_gpio_pin;
+	uint8_t int_gpio_flags;
+	uint8_t int_pin;
+#endif /* CONFIG_IIS2DH_TRIGGER */
+};
+
+/* sensor data */
+struct iis2dh_data {
+	struct device *bus;
+	int16_t acc[3];
+	uint32_t gain;
+
+	stmdev_ctx_t *ctx;
+#ifdef CONFIG_IIS2DH_TRIGGER
+	struct device *gpio;
+	uint8_t gpio_pin;
+	struct gpio_callback gpio_cb;
+	sensor_trigger_handler_t drdy_handler;
+#if defined(CONFIG_IIS2DH_TRIGGER_OWN_THREAD)
+	K_THREAD_STACK_MEMBER(thread_stack, CONFIG_IIS2DH_THREAD_STACK_SIZE);
+	struct k_thread thread;
+	struct k_sem gpio_sem;
+#elif defined(CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD)
+	struct k_work work;
+	struct device *dev;
+#endif /* CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD */
+#endif /* CONFIG_IIS2DH_TRIGGER */
+#if DT_INST_SPI_DEV_HAS_CS_GPIOS(0)
+	struct spi_cs_control cs_ctrl;
+#endif
+};
+
+int iis2dh_i2c_init(struct device *dev);
+int iis2dh_spi_init(struct device *dev);
+
+#ifdef CONFIG_IIS2DH_TRIGGER
+int iis2dh_init_interrupt(struct device *dev);
+int iis2dh_trigger_set(struct device *dev,
+			  const struct sensor_trigger *trig,
+			  sensor_trigger_handler_t handler);
+#endif /* CONFIG_IIS2DH_TRIGGER */
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_IIS2DH_IIS2DH_H_ */

drivers/sensor/iis2dh/iis2dh_trigger.c
@@ -1,0 +1,191 @@
+/* ST Microelectronics IIS2DH 3-axis accelerometer driver
+ *
+ * Copyright (c) 2020 STMicroelectronics
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Datasheet:
+ * https://www.st.com/resource/en/datasheet/iis2dh.pdf
+ */
+
+#include <kernel.h>
+#include <drivers/sensor.h>
+#include <drivers/gpio.h>
+#include <logging/log.h>
+
+#include "iis2dh.h"
+
+LOG_MODULE_DECLARE(IIS2DH, CONFIG_SENSOR_LOG_LEVEL);
+
+/**
+ * iis2dh_enable_int - enable selected int pin to generate interrupt
+ */
+static int iis2dh_enable_drdy(struct device *dev,
+			      enum sensor_trigger_type type, int enable)
+{
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	iis2dh_ctrl_reg3_t reg3;
+
+	/* set interrupt for pin INT1 */
+	iis2dh_pin_int1_config_get(iis2dh->ctx, &reg3);
+
+	reg3.i1_drdy1 = enable;
+
+	return iis2dh_pin_int1_config_set(iis2dh->ctx, &reg3);
+}
+
+/**
+ * iis2dh_trigger_set - link external trigger to event data ready
+ */
+int iis2dh_trigger_set(struct device *dev,
+		       const struct sensor_trigger *trig,
+		       sensor_trigger_handler_t handler)
+{
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	union axis3bit16_t raw;
+	int state = (handler != NULL) ? PROPERTY_ENABLE : PROPERTY_DISABLE;
+
+	switch (trig->type) {
+	case SENSOR_TRIG_DATA_READY:
+		iis2dh->drdy_handler = handler;
+		if (state) {
+			/* dummy read: re-trigger interrupt */
+			iis2dh_acceleration_raw_get(iis2dh->ctx, raw.u8bit);
+		}
+		return iis2dh_enable_drdy(dev, SENSOR_TRIG_DATA_READY, state);
+	default:
+		LOG_ERR("Unsupported sensor trigger");
+		return -ENOTSUP;
+	}
+}
+
+static int iis2dh_handle_drdy_int(struct device *dev)
+{
+	struct iis2dh_data *data = dev->driver_data;
+
+	struct sensor_trigger drdy_trig = {
+		.type = SENSOR_TRIG_DATA_READY,
+		.chan = SENSOR_CHAN_ALL,
+	};
+
+	if (data->drdy_handler) {
+		data->drdy_handler(dev, &drdy_trig);
+	}
+
+	return 0;
+}
+
+/**
+ * iis2dh_handle_interrupt - handle the drdy event
+ * read data and call handler if registered any
+ */
+static void iis2dh_handle_interrupt(void *arg)
+{
+	struct device *dev = (struct device *)arg;
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	const struct iis2dh_device_config *cfg = dev->config_info;
+
+	iis2dh_handle_drdy_int(dev);
+
+	gpio_pin_interrupt_configure(iis2dh->gpio, cfg->int_gpio_pin,
+				     GPIO_INT_EDGE_TO_ACTIVE);
+}
+
+static void iis2dh_gpio_callback(struct device *dev,
+				 struct gpio_callback *cb, uint32_t pins)
+{
+	struct iis2dh_data *iis2dh =
+		CONTAINER_OF(cb, struct iis2dh_data, gpio_cb);
+
+	if ((pins & BIT(iis2dh->gpio_pin)) == 0U) {
+		return;
+	}
+
+	gpio_pin_interrupt_configure(dev, iis2dh->gpio_pin,
+				     GPIO_INT_DISABLE);
+
+#if defined(CONFIG_IIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_give(&iis2dh->gpio_sem);
+#elif defined(CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD)
+	k_work_submit(&iis2dh->work);
+#endif /* CONFIG_IIS2DH_TRIGGER_OWN_THREAD */
+}
+
+#ifdef CONFIG_IIS2DH_TRIGGER_OWN_THREAD
+static void iis2dh_thread(int dev_ptr, int unused)
+{
+	struct device *dev = INT_TO_POINTER(dev_ptr);
+	struct iis2dh_data *iis2dh = dev->driver_data;
+
+	ARG_UNUSED(unused);
+
+	while (1) {
+		k_sem_take(&iis2dh->gpio_sem, K_FOREVER);
+		iis2dh_handle_interrupt(dev);
+	}
+}
+#endif /* CONFIG_IIS2DH_TRIGGER_OWN_THREAD */
+
+#ifdef CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD
+static void iis2dh_work_cb(struct k_work *work)
+{
+	struct iis2dh_data *iis2dh =
+		CONTAINER_OF(work, struct iis2dh_data, work);
+
+	iis2dh_handle_interrupt(iis2dh->dev);
+}
+#endif /* CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD */
+
+int iis2dh_init_interrupt(struct device *dev)
+{
+	struct iis2dh_data *iis2dh = dev->driver_data;
+	const struct iis2dh_device_config *cfg = dev->config_info;
+	int ret;
+
+	/* setup data ready gpio interrupt */
+	iis2dh->gpio = device_get_binding(cfg->int_gpio_port);
+	if (iis2dh->gpio == NULL) {
+		LOG_DBG("Cannot get pointer to %s device",
+			    cfg->int_gpio_port);
+		return -EINVAL;
+	}
+
+#if defined(CONFIG_IIS2DH_TRIGGER_OWN_THREAD)
+	k_sem_init(&iis2dh->gpio_sem, 0, UINT_MAX);
+
+	k_thread_create(&iis2dh->thread, iis2dh->thread_stack,
+		       CONFIG_IIS2DH_THREAD_STACK_SIZE,
+		       (k_thread_entry_t)iis2dh_thread, dev,
+		       0, NULL, K_PRIO_COOP(CONFIG_IIS2DH_THREAD_PRIORITY),
+		       0, K_NO_WAIT);
+#elif defined(CONFIG_IIS2DH_TRIGGER_GLOBAL_THREAD)
+	iis2dh->work.handler = iis2dh_work_cb;
+	iis2dh->dev = dev;
+#endif /* CONFIG_IIS2DH_TRIGGER_OWN_THREAD */
+
+	iis2dh->gpio_pin = cfg->int_gpio_pin;
+
+	ret = gpio_pin_configure(iis2dh->gpio, cfg->int_gpio_pin,
+				 GPIO_INPUT | cfg->int_gpio_flags);
+	if (ret < 0) {
+		LOG_DBG("Could not configure gpio");
+		return ret;
+	}
+
+	gpio_init_callback(&iis2dh->gpio_cb,
+			   iis2dh_gpio_callback,
+			   BIT(cfg->int_gpio_pin));
+
+	if (gpio_add_callback(iis2dh->gpio, &iis2dh->gpio_cb) < 0) {
+		LOG_DBG("Could not set gpio callback");
+		return -EIO;
+	}
+
+	/* enable drdy on int1 in pulse mode */
+	if (iis2dh_int1_pin_notification_mode_set(iis2dh->ctx, IIS2DH_INT1_PULSED)) {
+		return -EIO;
+	}
+
+	return gpio_pin_interrupt_configure(iis2dh->gpio, cfg->int_gpio_pin,
+					    GPIO_INT_EDGE_TO_ACTIVE);
+}