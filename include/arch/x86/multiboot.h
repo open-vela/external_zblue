zephyrproject-rtos/zephyr

arch/x86: Expose multiboot init function even when disabled
Just a dummy function will do.

When enabled, the code does not need the #ifdef as cmake is handling
this properly already. This was also the wrong CONFIG_ used there
anyway.

Signed-off-by: Tomasz Bursztyka <tomasz.bursztyka@linux.intel.com>

include/arch/x86/multiboot.h
@@ -36,7 +36,18 @@ struct multiboot_info {
 
 extern struct multiboot_info multiboot_info;
 
-extern void z_multiboot_init(struct multiboot_info *);
+#ifdef CONFIG_MULTIBOOT_INFO
+
+void z_multiboot_init(struct multiboot_info *info);
+
+#else
+
+inline void z_multiboot_init(struct multiboot_info *info)
+{
+	ARG_UNUSED(info);
+}
+
+#endif /* CONFIG_MULTIBOOT_INFO */
 
 /*
  * the mmap_addr field points to a series of entries of the following form.

open-vela/external_zblue

include/arch/x86/multiboot.h
@@ -36,7 +36,18 @@ struct multiboot_info {
 
 extern struct multiboot_info multiboot_info;
 
+#ifdef CONFIG_MULTIBOOT_INFO
+
 extern void z_multiboot_init(struct multiboot_info *info_pa);
+
+#else
+
+inline void z_multiboot_init(struct multiboot_info *info_pa)
+{
+	ARG_UNUSED(info_pa);
+}
+
+#endif /* CONFIG_MULTIBOOT_INFO */
 
 /*
  * the mmap_addr field points to a series of entries of the following form.