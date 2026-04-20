zephyrproject-rtos/zephyr

arch: arm64: Refine v8R AArch64 MPU regions switch
The current mechanism of the MPU region switching configures and
reprograms the regions (including inserting, splitting the dynamic
region, and flushing the regions to the registers) every time during the
context switch. This, not only causes a large usage of the kernel stack
but also a lower performance.

To improve it, move the configuration operations ahead to make sure the
context swtich only flushes the current thread regions to the registers
and does not configure the regions anymore. To achieve this, configure
the regions during any operations related to partitions (partition
add/remove, and domain add/remove thread), flush the sys_dyn_regions if
the current thread is the privileged thread, and flush the thread's own
regions if it's a user thread.

Signed-off-by: Jaxson Han <jaxson.han@arm.com>

include/zephyr/arch/arm64/thread.h
@@ -21,6 +21,7 @@
 
 #ifndef _ASMLANGUAGE
 #include <zephyr/types.h>
+#include <zephyr/arch/arm64/mm.h>
 
 struct _callee_saved {
 	uint64_t x19;
@@ -50,8 +51,14 @@ struct z_arm64_fp_context {
 };
 
 struct _thread_arch {
-#if defined(CONFIG_USERSPACE) && defined(CONFIG_ARM_MMU)
+#if defined(CONFIG_USERSPACE)
+#if defined(CONFIG_ARM_MMU)
 	struct arm_mmu_ptables *ptables;
+#endif
+#if defined(CONFIG_ARM_MPU)
+	struct dynamic_region_info regions[CONFIG_ARM_MPU_MAX_DYNAMIC_REGIONS];
+	uint8_t region_num;
+#endif
 #endif
 #ifdef CONFIG_ARM64_SAFE_EXCEPTION_STACK
 	uint64_t stack_limit;

open-vela/external_zblue

include/zephyr/arch/arm64/thread.h
@@ -21,6 +21,7 @@
 
 #ifndef _ASMLANGUAGE
 #include <zephyr/types.h>
+#include <zephyr/arch/arm64/mm.h>
 
 struct _callee_saved {
 	uint64_t x19;
@@ -50,8 +51,15 @@ struct z_arm64_fp_context {
 };
 
 struct _thread_arch {
-#if defined(CONFIG_USERSPACE) && defined(CONFIG_ARM_MMU)
+#if defined(CONFIG_USERSPACE)
+#if defined(CONFIG_ARM_MMU)
 	struct arm_mmu_ptables *ptables;
+#endif
+#if defined(CONFIG_ARM_MPU)
+	struct dynamic_region_info regions[ARM64_MPU_MAX_DYNAMIC_REGIONS];
+	uint8_t region_num;
+	atomic_t flushing;
+#endif
 #endif
 #ifdef CONFIG_ARM64_SAFE_EXCEPTION_STACK
 	uint64_t stack_limit;